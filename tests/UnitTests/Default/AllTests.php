07cad4d0ee50ca13482f81ff1ed8a7516739f259
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-12 19:39:51 +0000

    Crescas in mille millia

---
+++ tests/UnitTests/Default/AllTests.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Test suite for the default module
+ *
+ * LICENSE: Licensed under the terms of the GNU Publice License
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Default_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'Controllers/BaseTest.php';
+
+/**
+ * Test suite for the default module
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
+class Default_AllTests
+{
+    /**
+     * Runs the test suite
+     *
+     */
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    /**
+     * Builds the test suite containing all
+     * tests of this module and returns the suite
+     *
+     * @return PHPUnit_Framework_TestSuite
+     */
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Default Controller');
+
+        $suite->addTestSuite('BaseTest');
+        // ...
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Default_AllTests::main') {
+    Framework_AllTests::main();
+}
