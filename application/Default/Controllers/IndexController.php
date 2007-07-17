c6bdd435b4fb01940a39666fb74fbf30a0b34750
Gustavo Solt <gustavo.solt@mayflower.de>
2007-07-17 14:08:47 +0000

    Default controllers

--- application/Default/Controllers/IndexController.php
+++ application/Default/Controllers/IndexController.php
@@ -3,7 +3,7 @@
  * Default Controller for PHProjekt 6.0
  *
  * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
- * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @license    see http://phprojekt.com/licence
  * @version    CVS: $Id$
  * @author     David Soria Parra <soria_parra@mayflower.de>
  * @package    PHProjekt
@@ -26,6 +26,15 @@ require_once 'Zend/Controller/Action.php';
  */
 class IndexController extends Zend_Controller_Action
 {
+	/**
+	 * Return true if not have access 
+     *
+	 */
+	public function accessDenied()
+	{
+        return false;
+	}
+
     /**
      * Standard action
      *
@@ -37,5 +46,24 @@ class IndexController extends Zend_Controller_Action
         /* @var $renderer Zend_View_Abstract */
         $renderer       = Zend_Registry::get('view');
         $renderer->name = "David";
+    }
+
+    /**
+     * If the Action don´t exists, call indexAction
+     *
+     * @param string method - Action method
+     * @param array  args   - Arguments for the Action
+     * @return Zend_Exception
+     *
+     */
+    public function __call($method, $args)
+    {
+        if ('Action' == substr($method, -6)) {
+            // If the action method was not found,
+            // forward to the index action            
+            return $this->_forward('index');  
+        }
+        // all other methods throw an exception
+        throw new Exception('Invalid method "' . $method . '" called');
     }
 }
