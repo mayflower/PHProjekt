9fc10bd09bdc0ea6d4660d80f2485802ccbc2de3
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-18 17:13:32 +0000

    ActiveRecord exception

---
+++ phprojekt/application/Phprojekt/ActiveRecord/Exception.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Exception class for ActiveRecord pattern
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ * @since      File available since Release 1.0
+ */
+
+/**
+ * Exception class for ActiveRecord pattern
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
+class Phprojekt_ActiveRecord_Exception extends Exception
+{
+    /**
+     * New instance
+     *
+     * @param string $message Fault string
+     * @param int    $code    Fault code
+     */
+    function __construct($message, $code = null)
+    {
+        parent::__construct($message, $code);
+    }
+}
