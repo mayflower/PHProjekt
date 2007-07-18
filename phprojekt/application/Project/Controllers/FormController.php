b13386a4adc2c6d371a089a89c3ab8b7d20539a0
Gustavo Solt <gustavo.solt@mayflower.de>
2007-07-18 07:09:56 +0000

    Module project

---
+++ phprojekt/application/Project/Controllers/FormController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Form Project Module Controller for PHProjekt 6.0
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
+ * @version    CVS: $Id: 
+ * @author     Gustavo Solt <solt@mayflower.de>
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+
+require_once (PHPR_CORE_PATH . '/Default/Controllers/FormController.php');
+
+/**
+ * Form Project Module Controller for PHProjekt 6.0
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Gustavo Solt <solt@mayflower.de>
+ */
+class Project_FormController extends FormController
+{
+}
