b82dff38e135f84b7739aeef02069caca2f868fa
Gustavo Solt <gustavo.solt@mayflower.de>
2007-07-18 07:05:37 +0000

    Controllers and Views extend for modules

--- phprojekt/application/Default/Controllers/IndexController.php
+++ phprojekt/application/Default/Controllers/IndexController.php
@@ -3,7 +3,8 @@
  * Default Controller for PHProjekt 6.0
  *
  * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
- * @license    see http://phprojekt.com/licence
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
  * @version    CVS: $Id$
  * @author     David Soria Parra <soria_parra@mayflower.de>
  * @package    PHProjekt
@@ -18,7 +19,8 @@ require_once 'Zend/Controller/Action.php';
  *
  * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
  * @version    Release: @package_version@
- * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *             GNU Public License 2.0
  * @package    PHProjekt
  * @link       http://www.phprojekt.com
  * @since      File available since Release 1.0
@@ -26,6 +28,128 @@ require_once 'Zend/Controller/Action.php';
  */
 class IndexController extends Zend_Controller_Action
 {
+
+    /**
+     * Smarty object
+     * @var Smarty
+     */
+    protected $_smarty;
+
+    /**
+     * Tree View output
+     *
+     * @var string
+     */
+    public $treeView = '';
+
+    /**
+     * List View output
+     *
+     * @var string
+     */
+    public $listView = '';
+
+    /**
+     * Detail View output
+     *
+     * @var string
+     */
+    public $detailView = '';
+
+    /**
+     * Init function
+     * Get the Smarty instance and asign the tree, list and detail views
+     *
+     * @param void
+     *
+     * @return void
+     */
+    public function init() {
+        /* Get the smarty object */
+        $this->_smarty = Zend_Registry::get('view');
+
+        /* Set treeview */
+        $this->setTreeView($this->_helper->viewRenderer->view->render('tree.tpl'));
+
+        /* Set listview */
+        $this->setListView($this->_helper->viewRenderer->view->render('list.tpl'));
+
+        /* Set detailview */
+        $this->setDetailView($this->_helper->viewRenderer->view->render('detail.tpl'));
+    }
+
+    /**
+     * Assign an output to the tree view
+     *
+     * @param string $output The output to show
+     *
+     * @return void
+     */
+    public function setTreeView($output)
+    {
+        $this->_smarty->treeView = $output;
+    }
+
+    /**
+     * Assign an output to the list view
+     *
+     * @param string $output The output to show
+     *
+     * @return void
+     */
+    public function setListView($output)
+    {
+        $this->_smarty->listView = $output;
+    }
+
+    /**
+     * Assign an output to the detail view
+     *
+     * @param string $output The output to show
+     *
+     * @return void
+     */
+    public function setDetailView($output)
+    {
+        $this->_smarty->detailView = $output;
+    }
+
+    /**
+     * Get the tree output
+     *
+     * @param void 
+     *
+     * @return string The output to show
+     */
+    public function getTreeView()
+    {
+        return $this->_smarty->treeView;
+    }
+
+    /**
+     * Get the list output
+     *
+     * @param void 
+     *
+     * @return string The output to show
+     */
+    public function getListView()
+    {
+        return $this->_smarty->listView;
+    }
+
+    /**
+     * Get the detail output
+     *
+     * @param void 
+     *
+     * @return string The output to show
+     */
+    public function getDetailView($output)
+    {
+        return $this->_smarty->detailView;
+    }
+
 	/**
 	 * Return true if not have access 
      *
@@ -43,9 +167,6 @@ class IndexController extends Zend_Controller_Action
      */
     public function indexAction()
     {
-        /* @var $renderer Zend_View_Abstract */
-        $renderer       = Zend_Registry::get('view');
-        $renderer->name = "David";
     }
 
     /**
