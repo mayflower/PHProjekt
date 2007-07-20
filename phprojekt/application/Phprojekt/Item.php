9aea179aa68dac2aac56caf1bc83372916fc5092
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-20 17:24:47 +0000

    Added

---
+++ phprojekt/application/Phprojekt/Item.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * A item, with database manager support
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ * @since      File available since Release 1.0
+ */
+
+/**
+ * A item, with database manager support
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
+class Phprojekt_Item extends Phprojekt_ActiveRecord
+{
+
+}
