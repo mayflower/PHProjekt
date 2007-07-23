08b48c535a9540ba684a42445d25e082ae9b183f
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-23 20:38:34 +0000

    Improve logging mechanism and add logging to activerecord

--- application/Phprojekt/Log.php
+++ application/Phprojekt/Log.php
@@ -30,93 +30,34 @@ require_once 'Zend/Log.php';
  */
 class Phprojekt_Log extends Zend_Log
 {
-    const EMERG   = 0;  // Emergency: system is unusable
-    const ALERT   = 1;  // Alert: action must be taken immediately
-    const CRIT    = 2;  // Critical: critical conditions
-    const ERR     = 3;  // Error: error conditions
-    const WARN    = 4;  // Warning: warning conditions
-    const NOTICE  = 5;  // Notice: normal but significant condition
-    const INFO    = 6;  // Informational: informational messages
-    const DEBUG   = 7;  // Debug: debug messages
-
-    /**
-     * @var array of Log objects
-     */
-    private $_loguers = array();
-
-    /**
-     * @var array of priorities where the keys are the
-     * priority numbers and the values are the priority names
-     */
-    private $_priorities = array();
+    protected $_loggers = array();
 
     /**
      * Get the constants for use later
      *
      * @param  void
      */
-    public function __construct()
+    public function __construct(Zend_Config $config)
     {
-        $r = new ReflectionClass($this);
-        $this->_priorities = array_flip($r->getConstants());
-    }
+        parent::__construct();
 
-    /**
-     * Add a new log object
-     *
-     * @param  string  $priority  priority name
-     * @return void
-     */
-    public function addLog($priority = 'debug')
-    {
-        $config = Zend_Registry::get('config');
+        foreach ($config->log as $key => $val) {
+            $constant = "self::".strtoupper($key);
+            if (defined($constant)) {
 
-        if (isset($config->log->$priority->filename)) {
-            $oWriter = new Zend_Log_Writer_Stream($config->log->$priority->filename);
-        } else {
-            $oWriter = new Zend_Log_Writer_Null;
-        }
-
-        $priority = strtoupper($priority);
-        $this->_loguers[$priority] = new Zend_Log($oWriter);
-    }
-
-    /**
-     * Undefined method handler allows a shortcut:
-     *   $log->priorityName('message')
-     *     instead of
-     *   $log->log('message', Zend_Log::PRIORITY_NAME)
-     *
-     * @param  string  $method  priority name
-     * @param  string  $params  message to log
-     * @return void
-     * @throws Zend_Log_Exception
-     */
-    public function __call($method, $params)
-    {
-        $priority = strtoupper($method);
-
-        /* Add the new log if is not started */
-        if (!isset($this->_loguers[$priority])) {
-            $this->addLog($method);
-        }
-
-        if (($iPriority = array_search($priority, $this->_priorities)) !== false) {
-            $this->_loguers[$priority]->log(array_shift($params), $iPriority);
-        } else {
-            throw new Zend_Log_Exception('Bad log priority');
+                $priority = constant($constant);
+                $logger   = new Zend_Log(new Zend_Log_Writer_Stream($val->filename));
+                $logger->addFilter(new Zend_Log_Filter_Priority($priority));
+                $this->_loggers[] = $logger;
+            }
         }
     }
 
-    /**
-     * Add a writer per each log object
-     *
-     * @param  Zend_Log_Writer_Abstract $writer
-     * @return void
-     */
-    public function addWriter($writer,$priority)
+    public function log($message, $priority)
     {
-        $priority = strtoupper($priority);
-        $_loguers[$priority]->_writers[] = $writer;
+        /* @var Zend_Log $logger */
+        foreach ($this->_loggers as $logger) {
+            $logger->log($message, $priority);
+        }
     }
 }
