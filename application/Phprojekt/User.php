c033f5f5e5aa80bb149e48b9928c40d71da867f7
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2007-07-24 07:19:54 +0000

    CS

--- application/Phprojekt/User.php
+++ application/Phprojekt/User.php
@@ -2,27 +2,26 @@
 /**
  * User class for PHProjekt 6.0
  *
- * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
- * @license    http://phprojekt.com/license
- * @version    CVS: $Id$
- * @author     Eduardo Polidor <polidor@mayflower.de>
- * @package    PHProjekt
- * @link       http://www.phprojekt.com
- * @since      File available since Release 1.0
- * @TODO       Check with phpcs ASAP
- * @todo       EVERYTHING, THIS IS A MOCKUP.
+ * @copyright 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license   http://www.phprojekt.com/license PHProjekt6 License
+ * @version   CVS: $Id$
+ * @author    Eduardo Polidor <polidor@mayflower.de>
+ * @package   PHProjekt
+ * @link      http://www.phprojekt.com
+ * @since     File available since Release 1.0
  */
 
 /* Phprojekt_DatabaseManagerObject */
-require_once ('Phprojekt_DatabaseManagerObject.php');
+require_once 'Phprojekt_DatabaseManagerObject.php';
 
 /**
  * Phprojekt_User for PHProjekt 6.0
  *
  * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
  * @version    Release: @package_version@
- * @license    http://phprojekt.com/license
+ * @license   http://www.phprojekt.com/license PHProjekt6 License
  * @author     Eduardo Polidor <polidor@mayflower.de>
+ * @package    PHProjekt 
  */
 
 class Phprojekt_User extends Phprojekt_Item
@@ -57,18 +56,28 @@ class Phprojekt_User extends Phprojekt_Item
      */
     public $language;
 
+    /**
+     * Create object of type PHProjekt_User
+     *
+     */
     function __construct()
     {
     }
 
+    /**
+     * Destruct object 
+     *
+     */
     function __destruct()
     {
     }
 
     /**
      * Get a setting value of the user
-     * @param string name of the setting to be get
-     * @return mixed value of the setting
+     * 
+     * @param string $name of the setting to be get
+     * 
+     * @return mixed $value of the setting
      */
     public function getSetting(mixed $name)
     {
@@ -77,6 +86,7 @@ class Phprojekt_User extends Phprojekt_Item
 
     /**
      * Checks if user is active
+     * 
      * @return boolean id user is active or not
      */
     public function isActive()
@@ -86,6 +96,7 @@ class Phprojekt_User extends Phprojekt_Item
 
     /**
      * Makes the login process
+     * 
      * @return boolean true if login process was sucessful
      */
     public function login()
@@ -95,6 +106,7 @@ class Phprojekt_User extends Phprojekt_Item
 
     /**
      * Makes the logout process
+     * 
      *@return boolean true if logout process was sucessful
      */
     public function logout()
@@ -104,8 +116,10 @@ class Phprojekt_User extends Phprojekt_Item
 
     /**
      * Sets any setting of the user
-     * @param string name of the setting to be changed
-     * @param string value new value
+     * 
+     * @param string $name  of the setting to be changed
+     * @param string $value new value
+     * 
      * @return mixed The value set on the setting
      */
     public function setSetting(string $name, string $value)
