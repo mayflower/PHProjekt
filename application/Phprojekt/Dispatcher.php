4acdf629724a60c908994f1cb7a0d192d70a09ca
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-25 14:46:06 +0000

    Change back, as it doenst working as expected at the moment

--- application/Phprojekt/Dispatcher.php
+++ application/Phprojekt/Dispatcher.php
@@ -1,5 +1,32 @@
 <?php
+/**
+ * Phprojekt own dispatcher to handle customized controller names
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id:
+ * @author     Gustavo Solt <solt@mayflower.de>
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
 
+/**
+ * Phprojekt own dispatcher to handle customized controller names.
+ * Customized controllers have the name {MODULE}_Customized_{FOO}Controller
+ *
+ * @todo not supported yet
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Gustavo Solt <solt@mayflower.de>
+ */
 class Phprojekt_Dispatcher extends Zend_Controller_Dispatcher_Standard
 {
 
@@ -11,21 +38,10 @@ class Phprojekt_Dispatcher extends Zend_Controller_Dispatcher_Standard
      */
     public function formatControllerName($unformatted)
     {
-        $logger = Zend_Registry::get('log');
-        $logger->debug("Dispatch unformated {$unformatted}");
-
-
         $module = $this->getFrontController()->getRequest()->getModuleName();
         $module = self::formatModuleName($module);
 
         $controller = parent::formatControllerName($unformatted);
-
-/*        if ($module !== self::getDefaultModule()) {
-            $controller = Phprojekt_Loader::getController($module, $controller);
-        }
-*/
-        $logger->debug("Dispatch formated {$controller}");
-
         return $controller;
     }
 }
