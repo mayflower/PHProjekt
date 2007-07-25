ac94afe0abbdd61f64289ae2cc0fa98f4fd42528
David Soria Parra <david.soria_parra@mayflower.de>
2007-07-25 17:34:49 +0000

    Added

---
+++ tests/UnitTests/Phprojekt/LoaderTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Unit test
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/Extensions/ExceptionTestCase.php';
+
+/**
+ * Tests for active records
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
+class Phprojekt_LoaderTest extends PHPUnit_Extensions_ExceptionTestCase
+{
+    /**
+     * setUp method for PHPUnit. We use a shared db connection
+     *
+     */
+    public function setUp()
+    {
+        $this->sharedFixture = Zend_Db::factory('PDO_MYSQL', array(
+                                          'username' => 'phprojekt',
+                                          'password' => 'phprojekt',
+                                          'dbname'   => 'phprojekt-mvc-test',
+                                          'host'     => 'localhost'));
+    }
+
+    /**
+     * Test the getModel functionality
+     *
+     */
+    public function testGetModel()
+    {
+        $name = Phprojekt_Loader::getModel('Project', 'Project');
+        $this->assertEquals('Project_Models_Project', $name);
+
+        $instance = new $name(array('db'=>$this->sharedFixture));
+        $this->assertNotNull($instance);
+
+        $instance = Phprojekt_Loader::getModelFactory('Project', 'Project',
+                        array('db'=>$this->sharedFixture));
+        $this->assertNotNull($instance);
+    }
+}
