6ff3d774a85c8b75c9dc97b4048ef8306604941c
Gustavo Solt <gustavo.solt@mayflower.de>
2007-08-01 13:01:45 +0000

    PHPunit

---
+++ tests/UnitTests/Phprojekt/DatabaseManagerTest.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Unit test
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+require_once 'PHPUnit/Framework.php';
+require_once 'PHPUnit/Extensions/ExceptionTestCase.php';
+
+/**
+ * Tests for database manager
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Gustavo Solt <solt@mayflower.de>
+ */
+class Phprojekt_DatabaseManagerTest extends PHPUnit_Extensions_ExceptionTestCase
+{
+    /**
+     * setUp method for PHPUnit. We use a shared db connection
+     *
+     */
+    public function setUp()
+    {
+        $config = new Zend_Config_Ini('./configuration.ini', 'testing');
+        $this->sharedFixture = Zend_Db::factory($config->database->type, array(
+                                          'username' => $config->database->username,
+                                          'password' => $config->database->password,
+                                          'dbname'   => $config->database->name,
+                                          'host'     => $config->database->host));
+
+        $this->_emptyResult = array();
+
+        $this->_formResult = array(
+                        'title' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'notes'     => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'priority'  => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => '')
+                            );
+
+        $this->_listResult = array(
+                        'title' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'priority'  => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'notes'     => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => '')
+                            );
+    }
+
+    /**
+     * Test getFieldsForList
+     *
+     */
+    public function testGetFieldsForList()
+    {
+        $db     = new Phprojekt_DatabaseManager(array('db' => $this->sharedFixture));
+        $fields = $db->getFieldsForList('project','listPosition');
+        $this->assertEquals(array_keys($this->_listResult), array_keys($fields));
+
+        /* Second call */
+        $fields_second = $db->getFieldsForList('project','listPosition');
+        $this->assertEquals($fields, $fields_second);
+
+        $db     = new Phprojekt_DatabaseManager(array('db' => $this->sharedFixture));
+        $fields = $db->getFieldsForList('nothing','nothing');
+        $this->assertEquals($this->_emptyResult, $fields);
+    }
+
+    /**
+     * Test getFieldsForForm
+     *
+     */
+    public function testGetFieldsForForm()
+    {
+        $db     = new Phprojekt_DatabaseManager(array('db' => $this->sharedFixture));
+        $fields = $db->getFieldsForForm('project','formPosition');
+        $this->assertEquals(array_keys($this->_formResult), array_keys($fields));
+
+        /* Second call */
+        $fields_second = $db->getFieldsForForm('project','formPosition');
+        $this->assertEquals($fields, $fields_second);
+
+        $db     = new Phprojekt_DatabaseManager(array('db' => $this->sharedFixture));
+        $fields = $db->getFieldsForForm('nothing','nothing');
+        $this->assertEquals($this->_emptyResult, $fields);
+    }
+}
