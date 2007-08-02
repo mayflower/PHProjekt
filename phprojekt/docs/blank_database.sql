02708caa1b3ed2f9397cfab3ca5c7959832aa05d
David Soria Parra <david.soria_parra@mayflower.de>
2007-08-02 16:03:03 +0000

    Move to camelcaps

--- phprojekt/docs/blank_database.sql
+++ phprojekt/docs/blank_database.sql
@@ -1,147 +1,151 @@
-DROP TABLE IF EXISTS module_instance
-;
-DROP TABLE IF EXISTS user_module_setting
-;
-DROP TABLE IF EXISTS project
-;
-DROP TABLE IF EXISTS project_user_role_rel
-;
-DROP TABLE IF EXISTS history
-;
-DROP TABLE IF EXISTS user
-;
-DROP TABLE IF EXISTS role
-;
-DROP TABLE IF EXISTS database_manager
-;
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: phprojekt-mvc-test
+-- ------------------------------------------------------
+-- Server version	5.0.38-Ubuntu_0ubuntu1-log
 
+BEGIN;
 
+--
+-- Table structure for table `DatabaseManager`
+--
+DROP TABLE IF EXISTS `DatabaseManager`;
+CREATE TABLE `DatabaseManager` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tableName` varchar(50) default NULL,
+  `tableField` varchar(60) default NULL,
+  `formTab` int default NULL,
+  `formLabel` varchar(255) default NULL,
+  `formTooltip` varchar(255) default NULL,
+  `formType` varchar(50) default NULL,
+  `formPosition` int default NULL,
+  `formColumns` int default NULL,
+  `formRegexp` varchar(255) default NULL,
+  `formRange` text,
+  `defaultValue` varchar(255) default NULL,
+  `listPosition` int default NULL,
+  `listAlign` varchar(20) default NULL,
+  `listUseFilter` smallint default NULL,
+  `altPosition` int default NULL,
+  `status` varchar(20) default NULL,
+  `isInteger` smallint default NULL,
+  `isRequired` smallint default NULL,
+  `isUnique` int default NULL,
+  PRIMARY KEY  (`id`)
+);
 
-CREATE TABLE module_instance
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	projectId MEDIUMINT,
-	module VARCHAR(250) NOT NULL,
-	name VARCHAR(250) NOT NULL,
-	PRIMARY KEY (id),
-	KEY (projectId)
-)
-;
+--
+-- Table structure for table `History`
+--
 
+DROP TABLE IF EXISTS `History`;
+CREATE TABLE `History` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `dataobjectId` int NOT NULL,
+  `module` varchar(50) NOT NULL,
+  `oldValue` varchar(100) NOT NULL,
+  `newValue` varchar(250) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
 
-CREATE TABLE user_module_setting
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	userId MEDIUMINT NOT NULL,
-	keyValue VARCHAR(250) NOT NULL,
-	value VARCHAR(250) NOT NULL,
-	module VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id),
-	KEY (userId)
-)
-;
+CREATE INDEX `History_userId` ON `History`(`userId`);
 
+--
+-- Table structure for table `ModuleInstance`
+--
 
-CREATE TABLE project
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	parent MEDIUMINT,
-	path VARCHAR(25) NOT NULL DEFAULT "\\",
-	title VARCHAR(250) NOT NULL,
-	notes TEXT NOT NULL,
-	ownerId MEDIUMINT,
-	startDate DATETIME,
-	endDate DATETIME,
-	priority INTEGER,
-	currentStatus VARCHAR(50) NOT NULL DEFAULT 'working',
-	completePercent FLOAT(0) DEFAULT 0,
-	hourlyWageRate FLOAT(0),
-	budget FLOAT(0),
-	PRIMARY KEY (id),
-	KEY (ownerId)
-)
-;
+DROP TABLE IF EXISTS `ModuleInstance`;
+CREATE TABLE `ModuleInstance` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `projectId` int default NULL,
+  `module` varchar(250) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
 
+CREATE INDEX `ModuleInstance_userId` ON `ModuleInstance`(`projectId`);
+--
+-- Table structure for table `Project`
+--
 
-CREATE TABLE project_user_role_rel
-(
-	projectId MEDIUMINT NOT NULL,
-	userId MEDIUMINT NOT NULL,
-	roleId MEDIUMINT NOT NULL,
-	KEY (projectId),
-	KEY (roleId),
-	KEY (userId)
-)
-;
+DROP TABLE IF EXISTS `Project`;
+CREATE TABLE `Project` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `parent` int default NULL,
+  `path` varchar(25) NOT NULL default '\\',
+  `title` varchar(250) NOT NULL,
+  `notes` text NOT NULL,
+  `ownerId` int default NULL,
+  `startDate` datetime default NULL,
+  `endDate` datetime default NULL,
+  `priority` int default NULL,
+  `currentStatus` varchar(50) NOT NULL default 'working',
+  `completePercent` float default '0',
+  `hourlyWageRate` float default NULL,
+  `budget` float default NULL,
+  PRIMARY KEY(`id`)
+);
+CREATE INDEX `Project_ownerId` ON `Project`(`ownerId`);
 
+--
+-- Table structure for table `ProjectUserRoleRelation`
+--
 
-CREATE TABLE history
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	userId MEDIUMINT NOT NULL,
-	dataobjectId MEDIUMINT NOT NULL,
-	module VARCHAR(50) NOT NULL,
-	oldValue VARCHAR(100) NOT NULL,
-	newValue VARCHAR(250) NOT NULL,
-	action VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id),
-	KEY (userId)
-)
-;
+DROP TABLE IF EXISTS `ProjectUserRoleRelation`;
+CREATE TABLE `ProjectUserRoleRelation` (
+  `projectId` int NOT NULL,
+  `userId` int NOT NULL,
+  `roleId` int NOT NULL
+);
 
+CREATE INDEX `ProjectUserRoleRelation_projectId` ON `ProjectUserRoleRelation`(`projectId`);
+CREATE INDEX `ProjectUserRoleRelation_userId` ON `ProjectUserRoleRelation`(`userId`);
+CREATE INDEX `ProjectUserRoleRelation_roleId` ON `ProjectUserRoleRelation`(`roleId`);
 
-CREATE TABLE user
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(250) NOT NULL,
-	password VARCHAR(32) NOT NULL,
-	firstname VARCHAR(250),
-	lastname VARCHAR(250),
-	language VARCHAR(5) NOT NULL,
-	PRIMARY KEY (id),
-	UNIQUE (username)
-)
-;
+--
+-- Table structure for table `Role`
+--
 
+DROP TABLE IF EXISTS `Role`;
+CREATE TABLE `Role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `module` varchar(250) NOT NULL,
+  `permission` varchar(50) NOT NULL,
+  PRIMARY KEY(`id`)
+);
 
-CREATE TABLE role
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	module VARCHAR(250) NOT NULL,
-	permission VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id)
-)
-;
+--
+-- Table structure for table `User`
+--
 
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `firstname` varchar(250) default NULL,
+  `lastname` varchar(250) default NULL,
+  `language` varchar(5) NOT NULL,
+  PRIMARY KEY(`id`),
+  UNIQUE(`username`)
+);
 
-CREATE TABLE database_manager
-(
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-	tableName VARCHAR(50),
-	tableField VARCHAR(60),
-	formTab INTEGER,
-	formLabel VARCHAR(255),
-	formTooltip VARCHAR(255),
-	formType VARCHAR(50),
-	formPosition INTEGER,
-	formColumns INTEGER,
-	formRegexp VARCHAR(255),
-	formRange TEXT,
-	defaultValue VARCHAR(255),
-	listPosition INTEGER,
-	listAlign VARCHAR(20),
-	listUseFilter TINYINT,
-	altPosition INTEGER,
-	status VARCHAR(20),
-	isInteger TINYINT,
-	isRequired TINYINT,
-	isUnique INTEGER,
-	PRIMARY KEY (id)
-)
-;
+--
+-- Table structure for table `UserModuleSetting`
+--
+DROP TABLE IF EXISTS `UserModuleSetting`;
+CREATE TABLE `UserModuleSetting` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `keyValue` varchar(250) NOT NULL,
+  `value` varchar(250) NOT NULL,
+  `module` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
+CREATE INDEX `UserModuleSetting_userId` ON `UserModuleSetting`(`userId`);
 
-## FOR TEST ##
-INSERT INTO `database_manager` VALUES (1, 'project', 'title', 1, 'title', 'title', 'text', 1, 1, '', NULL, '', 1, 'left', 1, 1, '', 0, 1, 0);
-INSERT INTO `database_manager` VALUES (2, 'project', 'notes', 1, 'notes', 'notes', 'textarea', 2, 2, '', NULL, '', 3, 'left', 1, 2, '1', 0, 1, 0);
-INSERT INTO `database_manager` VALUES (3, 'project', 'priority', 1, 'priority', 'priority', 'text', 3, 1, NULL, NULL, '5', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+COMMIT;
