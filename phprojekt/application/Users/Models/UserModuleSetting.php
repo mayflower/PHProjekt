51864a674c610747ba96d4480f561a148686a9eb
David Soria Parra <david.soria_parra@mayflower.de>
2007-08-08 02:28:30 +0000

    Move to the right location

---
+++ phprojekt/application/Users/Models/UserModuleSetting.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User model
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id:
+ * @package    PHProjekt
+ * @subpackage Core
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <david.soria_parra@mayflower.de>
+ */
+
+/**
+ * Settings on a per user base
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @package    PHProjekt
+ * @subpackage Core
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <david.soria_parra@mayflower.de>
+ */
+class Users_Models_UserModuleSetting extends Phprojekt_ActiveRecord_Abstract
+{
+}
