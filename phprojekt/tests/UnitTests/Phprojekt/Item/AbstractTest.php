12d179876eb8dad9a880ef8a7d1da577938adf00
Gustavo Solt <gustavo.solt@mayflower.de>
2007-08-10 19:08:53 +0000

    Add test functions for the death code and add a feld startDate on the test database for make date testings.

--- phprojekt/tests/UnitTests/Phprojekt/Item/AbstractTest.php
+++ phprojekt/tests/UnitTests/Phprojekt/Item/AbstractTest.php
@@ -26,6 +26,189 @@ require_once 'PHPUnit/Extensions/ExceptionTestCase.php';
 class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
 {
     /**
+     * setUp method for PHPUnit. We use a shared db connection
+     *
+     */
+    public function setUp()
+    {
+        $this->_emptyResult = array();
+
+        $this->_formResult = array(
+                        'title' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'notes'     => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'priority'  => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'startDate' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => '')
+                            );
+
+        $this->_listResult = array(
+                        'title' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'priority'  => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'notes'     => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => ''),
+                        'startDate' => array(
+                                'id'                => '',
+                                'tableName'         => '',
+                                'tablefield'        => '',
+                                'formTab'           => '',
+                                'formLabel'         => '',
+                                'formTooltip'       => '',
+                                'formType'          => '',
+                                'formPosition'      => '',
+                                'formColumns'       => '',
+                                'formRegexp'        => '',
+                                'formRange'         => '',
+                                'defaultValue'      => '',
+                                'listPosition'      => '',
+                                'listAlign'         => '',
+                                'listUseFilter'     => '',
+                                'altPosition'       => '',
+                                'status'            => '',
+                                'isInteger'         => '',
+                                'isRequired'        => '',
+                                'isUnique'          => '')
+                            );
+    }
+
+    /**
      * Test set
      * Should throw an exception
      *
@@ -33,7 +216,7 @@ class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
      */
     public function testWrongSet()
     {
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $this->setExpectedException('Exception');
         $item->wrongAttribute = 'Hello World';
     }
@@ -46,9 +229,10 @@ class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
      */
     public function testRequiredFieldSet()
     {
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $item->title = '';
         $item->notes = 'TEST';
+        $item->startDate = '1981-05-12';
         $result = array(
             array('field'    => 'title',
                   'message'  => 'Is a required field')
@@ -64,11 +248,11 @@ class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
      */
     public function testIntegerFieldSet()
     {
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $item->priority = 'AA';
         $this->assertEquals(0, $item->priority);
 
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $item->priority = '7';
         $this->assertEquals(7, $item->priority);
     }
@@ -79,16 +263,18 @@ class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
      */
     public function testGetError()
     {
-        $result = array(
-            array('field'    => 'title',
-                  'message'  => 'Is a required field')
-                  );
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $result= array();
+        $result[] = array('field'    => 'title',
+                          'message'  => 'Is a required field');
+        $result[] = array('field'    => 'startDate',
+                          'message'  => 'Invalid format for date');
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $item->getError();
         $this->assertEquals(array(), $item->getError());
 
         $item->title = '';
         $item->notes = 'TEST';
+        $item->startDate = '20-';
         $item->recordValidate();
         $this->assertEquals($result, $item->getError());
     }
@@ -99,12 +285,74 @@ class Phprojekt_Item_AbstractTest extends PHPUnit_Extensions_ExceptionTestCase
      */
     public function testRecordValidate()
     {
-        $item = new PHprojekt_Project(array('db' => $this->sharedFixture));
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
         $item->title = '';
         $this->assertFalse($item->recordValidate());
 
         $item->title = 'TEST';
         $item->notes = 'TEST';
+        $item->startDate = '1981-05-12';
         $this->assertTrue($item->recordValidate());
+    }
+
+    /**
+     * Test getFieldsForList
+     *
+     */
+    public function testGetFieldsForList()
+    {
+        $item   = new Project_Models_Project(array('db' => $this->sharedFixture));
+        $fields = $item->getFieldsForList('project');
+        $this->assertEquals(array_keys($this->_listResult), array_keys($fields));
+
+        /* Second call */
+        $fields_second = $item->getFieldsForList('project');
+        $this->assertEquals($fields, $fields_second);
+    }
+
+    /**
+     * Test getFieldsForForm
+     *
+     */
+    public function testGetFieldsForForm()
+    {
+        $item   = new Project_Models_Project(array('db' => $this->sharedFixture));
+        $fields = $item->getFieldsForForm('project');
+        $this->assertEquals(array_keys($this->_formResult), array_keys($fields));
+
+        /* Second call */
+        $fields_second = $item->getFieldsForForm('project');
+        $this->assertEquals($fields, $fields_second);
+    }
+
+    public function testDate()
+    {
+        $item = new Project_Models_Project(array('db' => $this->sharedFixture));
+        $item->title = 'TEST';
+        $item->notes = 'TEST';
+        $item->startDate = 'aaaaaaaaaa';
+        $result = array(array(
+            'field'   => 'startDate',
+            'message' => 'Invalid format for date'));
+        $item->recordValidate();
+        $this->assertEquals($result, $item->getError());
+
+        $item->startDate = '1981-05-12';
+        //$this->assertEquals(array(), $item->getError());
+    }
+
+    /**
+     * Test float values
+     *
+     */
+    public function testFloat()
+    {
+        $item   = new Project_Models_Project(array('db' => $this->sharedFixture));
+        $item->budget = '1,3';
+        $this->assertEquals('1,30', $item->budget);
+
+        $item   = new Project_Models_Project(array('db' => $this->sharedFixture));
+        $item->budget = '1.3';
+        $this->assertEquals('13,00', $item->budget);
     }
 }
