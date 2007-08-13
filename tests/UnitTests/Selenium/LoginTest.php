f041386a30595f66b939997f0d0d0a6dc5a1e9e8
Cruise Control <noreply@mayflower.de>
2007-08-13 19:19:43 +0000

    Oops, finally the right place

---
+++ tests/UnitTests/Selenium/LoginTest.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * PHProjekt Selenium Test
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @author     Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+
+/**
+ * Selenium Test for PHProjekt
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
+ */
+class Selenium_LoginTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+    /**
+     * Handle for the current configuration
+     *
+     * @var Zend_Config_Ini
+     */
+    private $_config;
+     
+    /**
+     * setup the unit test. Use firefox as a browser and the document 
+     * root from the configuration file 
+     * 
+     * @return void 
+     */
+    function setUp()
+    {
+        $this->_config = new Zend_Config_Ini(DEFAULT_CONFIG_FILE, DEFAULT_CONFIG_SECTION);
+        $this->setBrowser('*firefox');
+        $this->verificationErrors = array();
+        $this->setBrowserUrl($this->_config->webpath);
+    }
+
+    /**
+     * Test the login method with wrong and correct data 
+     *
+     * @return void
+     */
+    function testLogin()
+    {
+        // Open phprojekt document root and get redirected
+        $this->open($this->_config->webpath);
+
+        $this->assertEquals("Login", $this->getTitle());
+    }
+
+    function testEmptyUsername()
+    {
+        // Open phprojekt document root and get redirected
+        $this->open($this->_config->webpath);
+
+        // Check for empty username
+        $this->type("password", "nothing");
+        $this->click("//input[@value='Send']");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertTrue($this->isTextPresent("Invalid user or password"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+    }
+
+    function testEmptyPassword()
+    {
+        // Open phprojekt document root and get redirected
+        $this->open($this->_config->webpath);
+
+        // Check for empty password
+        $this->type("username", "nothing");
+        $this->click("//input[@value='Send']");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertTrue($this->isTextPresent("Invalid user or password"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+    }
+
+    function testWrongCombination()
+    {
+        // Open phprojekt document root and get redirected
+        $this->open($this->_config->webpath);
+        $this->type("username", "david");
+        $this->type("password", "wrong");
+
+        // Check for wrong combination
+        $this->click("//input[@value='Send']");
+        $this->waitForPageToLoad("30000");
+        try {
+            $this->assertTrue($this->isTextPresent("Invalid user or password"));
+        } catch (PHPUnit_Framework_AssertionFailedError $e) {
+            array_push($this->verificationErrors, $e->toString());
+        }
+    }
+
+    function testCorrectCombination()
+    {
+        // Open phprojekt document root and get redirected
+        $this->open($this->_config->webpath);
+        $this->type("username", "david");
+
+        // Actual login with correct data
+        $this->type("password", "test");
+        $this->click("//input[@value='Send']");
+        $this->waitForPageToLoad("30000");
+
+        $this->assertEquals($this->_config->webpath . "index.php", $this->getLocation());
+
+    }
+}
