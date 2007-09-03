c1075ede47903752bb62fba38edd914d056e0daa
Gustavo Solt <gustavo.solt@mayflower.de>
2007-09-03 17:56:02 +0000

    Update testdatabase for have the values like the current database

--- phprojekt/tests/test_database.sql
+++ phprojekt/tests/test_database.sql
@@ -50,9 +50,13 @@ CREATE TABLE `DatabaseManager` (
 
 LOCK TABLES `DatabaseManager` WRITE;
 /*!40000 ALTER TABLE `DatabaseManager` DISABLE KEYS */;
-INSERT INTO `DatabaseManager` VALUES (1,'Project','title',1,'title','title','text',1,1,'',NULL,'',1,'left',1,1,'',0,1,0),(2,'Project','notes',1,'notes','notes','textarea',2,2,'',NULL,'',3,'left',1,2,'1',0,1,0),(3,'Project','priority',1,'priority','priority','text',3,1,NULL,NULL,'5',2,NULL,NULL,NULL,NULL,1,NULL,NULL);
-INSERT INTO `DatabaseManager` VALUES (10, 'Todo', 'title', 1, 'title', 'title', 'text', 2, 1, NULL, NULL, '', 2, 'left', 1, 2, '1', 0, 1, 0), (11, 'Todo', 'notes', 1, 'notes', 'notes', 'textarea', 3, 2, NULL, NULL, '', 0, NULL, 1, 0, '1', 0, 0, 0), (12, 'Todo', 'startDate', 1, 'startDate', 'startDate', 'date', 4, 1, NULL, NULL, '', 3, 'center', 1, 3, '1', 0, 1, 0), (13, 'Todo', 'endDate', 1, 'endDate', 'endDate', 'date', 5, 1, NULL, NULL, '', 4, 'center', 1, 4, '1', 0, 1, 0), (14, 'Todo', 'priority', 1, 'priority', 'priority', 'selectValues', 6, 1, NULL, '1#1|2#2|3#3|4#4|5#5|6#6|7#7|8#8|9#9|10#10', '5', 5, 'center', 1, 5, '1', 1, 1, 0), (15, 'Todo', 'currentStatus', 1, 'currentStatus', 'currentStatus', 'selectValues', 7, 1, NULL, '1#Accepted|2#Working|4#Ended|5#Stopped|7#Waiting', '1', 6, 'center', 1, 6, '1', 0, 0, 0), (16, 'Todo', 'projectId', 1, 'Project', 'Project', 'tree', 1, 1, NULL, 'Project_Models_Project', '', 1, 'center', 1, 1, '1', 1, 0, 0);
-INSERT INTO `DatabaseManager` VALUES (17,'project','startDate',1,'startDate','startDate','date',4,1,NULL,NULL,'',3,'center',1,3,'1',0,1,0);
+INSERT INTO `DatabaseManager` VALUES (1, 'Project', 'parent', 1, 'parent', 'parent', 'tree', 1, 1, NULL, 'Project_Models_Project', '1', 2, 'left', 1, 1, '1', 1, 0, 0), (2, 'Project', 'title', 1, 'title', 'title', 'text', 2, 1, NULL, NULL, '', 1, 'left', 1, 2, '1', 0, 1, 0), (3, 'Project', 'notes', 1, 'notes', 'notes', 'textarea', 3, 2, NULL, NULL, '', 0, NULL, 1, 0, '1', 0, 0, 0), (4, 'Project', 'startDate', 1, 'startDate', 'startDate', 'date', 4, 1, NULL, NULL, '', 3, 'center', 1, 3, '1', 0, 1, 0), (5, 'Project', 'endDate', 1, 'endDate', 'endDate', 'date', 5, 1, NULL, NULL, '', 4, 'center', 1, 4, '1', 0, 1, 0), (6, 'Project', 'priority', 1, 'priority', 'priority', 'selectValues', 6, 1, NULL, '1#1|2#2|3#3|4#4|5#5|6#6|7#7|8#8|9#9|10#10', '5', 5, 'center', 1, 5, '1', 1, 1, 0), (7, 'Project', 'currentStatus', 1, 'currentStatus', 'currentStatus', 'selectValues', 7, 1, NULL, '1#Offered|2#Ordered|3#Working|4#Ended|5#Stopped|6#Re-Opened|7#Waiting', '1', 6, 'center', 1, 6, '1', 0, 0, 0), (8, 'Project', 'completePercent', 1, 'completePercent', 'completePercent', 'text', 8, 1, NULL, NULL, '', 7, 'center', 1, 7, '1', 0, 0, 0), (9, 'Project', 'budget', 1, 'budget', 'budget', 'text', 9, 1, NULL, NULL, '', 0, NULL, 1, 8, '1', 0, 0, 0);
+
+INSERT INTO `DatabaseManager` VALUES (10, 'Todo', 'title', 1, 'title', 'title', 'text', 2, 1, NULL, NULL, '', 1, 'left', 1, 2, '1', 0, 1, 0), (11, 'Todo', 'notes', 1, 'notes', 'notes', 'textarea', 3, 2, NULL, NULL, '', 0, NULL, 1, 0, '1', 0, 0, 0), (12, 'Todo', 'startDate', 1, 'startDate', 'startDate', 'date', 4, 1, NULL, NULL, '', 3, 'center', 1, 3, '1', 0, 1, 0), (13, 'Todo', 'endDate', 1, 'endDate', 'endDate', 'date', 5, 1, NULL, NULL, '', 4, 'center', 1, 4, '1', 0, 1, 0), (14, 'Todo', 'priority', 1, 'priority', 'priority', 'selectValues', 6, 1, NULL, '1#1|2#2|3#3|4#4|5#5|6#6|7#7|8#8|9#9|10#10', '5', 5, 'center', 1, 5, '1', 1, 1, 0), (15, 'Todo', 'currentStatus', 1, 'currentStatus', 'currentStatus', 'selectValues', 7, 1, NULL, '1#Accepted|2#Working|4#Ended|5#Stopped|7#Waiting', '1', 6, 'center', 1, 6, '1', 0, 0, 0), (16, 'Todo', 'projectId', 1, 'project', 'project', 'tree', 1, 1, NULL, 'Project_Models_Project', '', 2, 'center', 1, 1, '1', 1, 0, 0);
+
+INSERT INTO `DatabaseManager` VALUES(17,'History','userId',1,'UserId','UserId','userId','1','1',null,null,0,1,'left',1,1,1,1,1,0), (18,'History','dataobjectId',1,'DataobjectId','DataobjectId','text','2','1',null,null,0,2,'center',1,2,1,1,1,0), (19,'History','module',1,'Module','Module','text','3','1',null,null,'',3,'left',1,3,1,0,1,0), (20,'History','field',1,'Field','Field','text','4','1',null,null,'',4,'left',1,4,1,0,1,0), (21,'History','oldValue',1,'OldValue','OldValue','text','5','1',null,null,'',0,'',0,0,1,0,1,0), (22,'History','newValue',1,'NewValue','NewValue','text','6','1',null,null,'',0,'',0,0,1,0,1,0), (23,'History','action',1,'Action','Action','text','7','1',null,null,'',7,'left',1,7,1,0,1,0), (24,'History','datetime',1,'Datetime','Datetime','datetime','8','1',null,null,'',8,'center',1,8,1,0,1,0);
+
+COMMIT;
 
 /*!40000 ALTER TABLE `DatabaseManager` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -76,16 +80,6 @@ CREATE TABLE `History` (
   KEY `userId` (`userId`),
   KEY `History_userId` (`userId`)
 );
-
-INSERT INTO DatabaseManager (id,tableName,tableField,formTab,formLabel,formTooltip,formType,formPosition,formColumns,formRegexp,formRange,defaultValue,listPosition,listAlign,listUseFilter,altPosition,status,isInteger,isRequired,isUnique)
-VALUES(0,'History','userId',1,'UserId','UserId','userId','1','1',null,null,0,1,'left',1,1,1,1,1,0),
-(18,'History','dataobjectId',1,'DataobjectId','DataobjectId','text','2','1',null,null,0,2,'center',1,2,1,1,1,0),
-(19,'History','module',1,'Module','Module','text','3','1',null,null,'',3,'left',1,3,1,0,1,0),
-(20,'History','field',1,'Field','Field','text','4','1',null,null,'',4,'left',1,4,1,0,1,0),
-(21,'History','oldValue',1,'OldValue','OldValue','text','5','1',null,null,'',0,'',0,0,1,0,1,0),
-(22,'History','newValue',1,'NewValue','NewValue','text','6','1',null,null,'',0,'',0,0,1,0,1,0),
-(23,'History','action',1,'Action','Action','text','7','1',null,null,'',7,'left',1,7,1,0,1,0),
-(24,'History','datetime',1,'Datetime','Datetime','datetime','8','1',null,null,'',8,'center',1,8,1,0,1,0);
 
 --
 -- Table structure for table `ModuleInstance`
@@ -140,7 +134,8 @@ CREATE TABLE `Project` (
 
 LOCK TABLES `Project` WRITE;
 /*!40000 ALTER TABLE `Project` DISABLE KEYS */;
-INSERT INTO `Project` VALUES (1,NULL,'/','Test Project','Test note',NULL,NULL,NULL,NULL,'ordered',0,NULL,NULL);
+INSERT INTO `Project` (`id`, `parent`, `path`, `title`, `notes`, `ownerId`, `startDate`, `endDate`, `priority`, `currentStatus`, `completePercent`, `hourlyWageRate`, `budget`) VALUES (1,NULL,'/','Invisible Root','',NULL,NULL,NULL,NULL,'working',0,NULL,NULL),(2,1,'/1/','Project 1','',NULL,NULL,NULL,NULL,'working',0,NULL,NULL),(3,1,'/1/','Project 2','',NULL,NULL,NULL,NULL,'working',0,NULL,NULL),(4,2,'/1/2/','Sub Project','',NULL,NULL,NULL,NULL,'working',0,NULL,NULL);
+INSERT INTO `Project` VALUES (5,2,'/1/2/','Test Project','Test note',NULL,NULL,NULL,NULL,'ordered',0,NULL,NULL);
 /*!40000 ALTER TABLE `Project` ENABLE KEYS */;
 UNLOCK TABLES;
 
