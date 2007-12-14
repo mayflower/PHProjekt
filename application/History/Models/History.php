f7b4b1572463729c8abf21a4c372552e4aca88ba
David Soria Parra <david.soria_parra@mayflower.de>
2007-12-14 19:15:55 +0000

    getListView() is not longer needed

--- application/History/Models/History.php
+++ application/History/Models/History.php
@@ -12,7 +12,6 @@
  * @author     Gustavo Solt <solt@mayflower.de>
  * @since      File available since Release 1.0
  */
-
 /**
  * History model class
  *
@@ -25,61 +24,4 @@
  * @author     Gustavo Solt <solt@mayflower.de>
  */
 class History_Models_History extends Phprojekt_Item_Abstract
-{
-    /**
-     * Get all the projects for list from the db
-     * The return array have all the rows that are ActiveRecords itself
-     * and the number of rows without the limits
-     *
-     * The pagination is stored in the session name "0 + Module".
-     *
-     * @return array Array with the rows for render and the total rows number
-     */
-    public function getListData()
-    {
-        $listData  = array();
-        $where     = null;
-        $projectId = 0;
-
-        /* Limit the query for paging */
-        $session = new Zend_Session_Namespace($projectId . $this->_name);
-        if (true === isset($session->currentPage)) {
-            $currentPage = $session->currentPage;
-        } else {
-            $currentPage = 0;
-        }
-
-        $config = Zend_Registry::get('config');
-        $count  = $config->itemsPerPage;
-
-        $order = 'datetime DESC';
-
-        $datetime     = null;
-        $numberOfRows = 0;
-        $howManyRows  = 0;
-        $index        = 0;
-        /* Get only the different datetime */
-        foreach ($this->fetchAll($where, $order) as $row) {
-            if ($datetime != $row->datetime) {
-                $datetime = $row->datetime;
-                /* Ommit the items before the actual page */
-                if ($index < $currentPage) {
-                    $howManyRows++;
-                } else {
-                    if (($numberOfRows < $count)) {
-                        $numberOfRows++;
-                        $listData[] = $row;
-                    } else {
-                        $howManyRows++;
-                    }
-                }
-                $index++;
-            }
-        }
-
-        /* Add items found to the total items */
-        $howManyRows += $numberOfRows;
-
-        return array($listData, $howManyRows);
-    }
-}
+{ }
