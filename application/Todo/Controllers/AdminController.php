6ec15b769f5fd659716d8989510d4eb217f9cc7e
David Soria Parra <david.soria_parra@mayflower.de>
2008-01-11 14:14:28 +0000

    Basic administration infrastructure

---
+++ application/Todo/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<<?php
+/**
+ * Administration for the todo module
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007, 2008 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
+
+/**
+ * Administration for the todo module
+ * 
+ * @copyright  2007, 2008 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
+class Todo_AdminController extends AdminController
+{
+	public static $name = 'Todo Administration';
+	public static $_configuration = array();
+}
