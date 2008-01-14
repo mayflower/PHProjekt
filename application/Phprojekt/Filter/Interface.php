617a013f001a384d094aaf9fda6fb39e3c6d2dd7
Gustavo Solt <gustavo.solt@mayflower.de>
2008-01-14 15:40:42 +0000

    Add Interfaces

---
+++ application/Phprojekt/Filter/Interface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * A generic interface to interact with filters.
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://www.phprojekt.com/license PHProjekt6 License
+ * @version    CVS: $Id:
+ * @author     Gustavo Solt <solt@mayflower.de>
+ * @package    PHProjekt
+ * @subpackage Core
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
+
+/**
+ * A interface that must be implemented
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://www.phprojekt.com/license PHProjekt6 License
+ * @author     Gustavo Solt <solt@mayflower.de>
+ * @package    PHProjekt
+ * @subpackage Core
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
+interface Phprojekt_Filter_Interface extends Iterator
+{
+}
