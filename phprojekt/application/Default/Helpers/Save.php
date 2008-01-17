cf59eaa5a739ee55ab53df310d21af28abca0825
David Soria Parra <david.soria_parra@mayflower.de>
2008-01-17 11:00:05 +0000

    PHPDoc Header

--- phprojekt/application/Default/Helpers/Save.php
+++ phprojekt/application/Default/Helpers/Save.php
@@ -1,5 +1,29 @@
 <?php
+/**
+ * Helper to save tree nodes and models
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id$
+ * @link       http://www.phprojekt.com
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ * @since      File available since Release 1.0
+ */
 
+/**
+ * Helper to save tree nodes and models
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @package    PHProjekt
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     David Soria Parra <soria_parra@mayflower.de>
+ */
 final class Default_Helpers_Save 
 {
     /**
