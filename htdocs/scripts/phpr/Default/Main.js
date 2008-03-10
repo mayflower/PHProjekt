0ae3a234fe5fd65992ed25f10113a42649a382e5
Nina Schmitt <nina.schmitt@mayflower.de>
2008-03-10 15:23:47 +0000

    Start to integrate changes from server and Module communication. part 1 of 2

--- htdocs/scripts/phpr/Default/Main.js
+++ htdocs/scripts/phpr/Default/Main.js
@@ -21,13 +21,17 @@ dojo.require("phpr.Default.Form");
 
 dojo.declare("phpr.Default.Main", phpr.Component, {
     
-    tree:    null,
-    grid:    null,
-	module:  null,
-	webpath: '',
+    tree:null,
+    grid:null,
+	module:null,
+	superMain:null,
+	webpath:'',
+	availableModules:null,
     
-    constructor: function(webpath, availmodules) {
+    constructor:function(webpath, availableModules, main) {
+		this.superMain  = main;
 		this.webpath = webpath;
+		this.availableModules = availableModules;
     },
     
 	openForm: function(id,module){
@@ -52,18 +56,35 @@ dojo.declare("phpr.Default.Main", phpr.Component, {
 		this.tree     = new phpr.Default.Tree(this,'Project');
 		this.grid     = new phpr.Default.Grid(updateUrl,this,parent,module);
 	},
-	
-	load: function() {
-		this.render(["phpr.Default.template", "main.html"], 
-		            dojo.body(), 
-		            {webpath: this.webpath});
-
+	load:function(){
+		this.render(["phpr.Default.template", "main.html"], dojo.body(),{webpath:this.webpath, currentModule:this.module});
 		dojo.addOnLoad(dojo.hitch(this, function() {
        			// Load the components, tree, list and details.
+				this.setSubmoduleNavigation();
 				var updateUrl = this.webpath + 'index.php/'+this.module+'/index/save/id/';
         		this.tree     = new phpr.Default.Tree(this, this.module);
         		this.grid     = new phpr.Default.Grid(updateUrl, this, null, this.module);
          	})
         );
+	},
+	reload:function(){
+		this.setSubmoduleNavigation();
+		var updateUrl = this.webpath + 'index.php/'+this.module+'/index/save/id/';
+        this.tree     = new phpr.Default.Tree(this, this.module);
+        this.grid     = new phpr.Default.Grid(updateUrl, this, null, this.module);
+		// destroy form if exists
+		if (dijit.byId("detailsBox")) {
+			phpr.destroyWidgets("detailsBox");
+		}		
+	},
+	setSubmoduleNavigation: function(){
+		var navigation ="";
+		for(i in this.availableModules){
+			var moduleName  = this.availableModules[i]["name"];
+			var moduleLabel = this.availableModules[i]["label"];
+			navigation += this.render(["phpr.Default.template", "navigation.html"], null,{moduleName:moduleName, moduleLabel:moduleLabel});
+			i++;
+		}
+		dojo.byId("subModuleNavigation").innerHTML = navigation;
 	}
 });
