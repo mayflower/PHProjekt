4f1a9eb3b5b2a455c2fa80534457239543991c20
David Soria Parra <david.soria_parra@mayflower.de>
2008-03-14 09:14:59 +0000

    Add the translation to the Test Zend_Registry as it is needed by some unit tests.

--- tests/UnitTests/AllTests.php
+++ tests/UnitTests/AllTests.php
@@ -92,6 +92,10 @@ class AllTests extends PHPUnit_Framework_TestSuite
         $log = new Phprojekt_Log($config);
         Zend_Registry::set('log', $log);
 
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+        Zend_Registry::set('translate', $translate);
+
         $db = Zend_Db::factory($config->database->type, array(
         'username' => $config->database->username,
         'password' => $config->database->password,
