59c8bfc3b5bf8bec3a84fe86724390dca7780630
Nina Schmitt <nina.schmitt@mayflower.de>
2008-03-17 12:49:03 +0000

    added it own components for each module

---
+++ htdocs/scripts/phpr/Project/Grid.js
@@ -0,0 +1,9 @@
+dojo.provide("phpr.Project.Grid");
+
+dojo.require("phpr.Default.Grid");
+
+dojo.declare("phpr.Project.Grid", phpr.Default.Grid, {
+	 constructor: function(webpath){
+	 },
+
+});
---
+++ htdocs/scripts/phpr/Todo/Tree.js
@@ -0,0 +1,9 @@
+dojo.provide("phpr.Todo.Tree");
+
+dojo.require("phpr.Default.Tree");
+
+dojo.declare("phpr.Todo.Tree", phpr.Default.Tree, {
+	 constructor: function(webpath){
+	 },
+
+});
