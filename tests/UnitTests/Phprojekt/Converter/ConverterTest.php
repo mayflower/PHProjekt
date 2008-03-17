3d53d58fbd8288c936fe5528c431c8ce6cebc1b1
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-03-17 12:34:20 +0000

    Converter to json string tests.

---
+++ tests/UnitTests/Phprojekt/Converter/ConverterTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Unit test
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id:
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * Tests Converter class
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Eduardo Polidor <polidor@mayflower.de>
+ */
+class Phprojekt_ConverterTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test json converter
+     *
+     */
+    public function testConvert()
+    {
+        $converted = substr('/* {"metadata":[{"key":"title","label":"Title","type":',0,23);
+        
+        $authNamespace = new Zend_Session_Namespace('PHProjekt_Auth');
+        $authNamespace->userId = 1;
+        
+        $object = Phprojekt_Loader::getModel('Project', 'Project'); 
+        
+        $records = $object->fetchAll(null, null, 20, 0);
+       
+        $data = Phprojekt_Converter_Json::convert($records);
+        
+        $this->assertEquals($converted, substr($data,0,23));
+    }
+    
+    /**
+     * Test json convert tree
+     *
+     */
+    public function testConvertTree()
+    {
+        $converted = substr('/* {"metadata":[{"key":"title","label":"Title","type":',0,23);
+        
+        $authNamespace = new Zend_Session_Namespace('PHProjekt_Auth');
+        $authNamespace->userId = 1;
+        
+        $object = Phprojekt_Loader::getModel('Project', 'Project'); 
+        
+        $tree = new Phprojekt_Tree_Node_Database($object, 1);
+        $tree->setup();
+
+       
+        $data = Phprojekt_Converter_Json::convertTree($tree);
+        print("|1|$data|2|");
+        $this->assertEquals($converted, substr($data,0,23));
+    }
+
+}
