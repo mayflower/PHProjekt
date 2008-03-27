7515585af9863cd2a2bdf6e4f7f34bf1d05ce8ef
Wolfram Kriesing <wolfram@uxebu.com>
2008-03-27 22:20:29 +0000

    + add caching for templates + add publish() method

--- htdocs/scripts/phpr/Component.js
+++ htdocs/scripts/phpr/Component.js
@@ -1,17 +1,37 @@
 dojo.provide("phpr.Component");
 
-dojo.declare("phpr.Component", null, {
-    main:   null,
-    
-    render: function(template, node, content) {
-        var context = new dojox.dtl.Context(content);
-        var tpl     = new dojox.dtl.Template(dojo._getText(dojo.moduleUrl(template[0], template[1])+'?'+Math.random()));
+// A global cache
+var __phpr_templateCache = {};
 
-        if(node) {
-            node.innerHTML = tpl.render(context);
-            phpr.initWidgets(node);
-        } else {
-            return tpl.render(context);
-        }
-    }   
+dojo.declare("phpr.Component", null, {
+	main:null,
+	module:"",
+	render: function(template, node, content) {
+		var context = new dojox.dtl.Context(content);
+		// Cache the templates if loaded once.
+		var tplContent = __phpr_templateCache[template[0]+"."+template[1]];
+		if (!tplContent) {
+			tplContent = dojo._getText(dojo.moduleUrl(template[0], template[1])+'?'+Math.random());
+			__phpr_templateCache[template[0]+"."+template[1]] = content;
+		}
+		var tpl = new dojox.dtl.Template(tplContent);
+		if(node) {
+			node.innerHTML = tpl.render(context);
+			phpr.initWidgets(node);
+		} else {
+			return tpl.render(context);
+		}
+	},
+	
+	publish:function(/*String*/ name){
+		// summary:
+		//		Publish the topic for the current module, its always prefixed with the module.
+		// description:
+		//		I.e. if the module name is "project" this.publish("open)
+		//		will then publish the topic "project.open".
+		// name: String
+		//		The topic of this module that shall be published.
+		dojo.publish(this.module+"."+name);
+	}
+	
 });
