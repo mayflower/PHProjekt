caaea293488acd710b4da7830fbd2a530ca548a3
David Soria Parra <david.soria_parra@mayflower.de>
2008-04-10 12:12:30 +0000

    Add a small test for ParseTree
    I know that this test fails, but that's on purpose so that we
    don't forget to implement escaping of special characters like
    string separators (', ")

--- phprojekt/tests/UnitTests/Phprojekt/AllTests.php
+++ phprojekt/tests/UnitTests/Phprojekt/AllTests.php
@@ -53,6 +53,7 @@ require_once 'Converter/ConverterTest.php';
 require_once 'ModelInformation/DefaultTest.php';
 require_once 'LoaderTest.php';
 require_once 'Tags/DefaultTest.php';
+require_once 'Filter/ParseTreeTest.php';
 
 /**
  * Test suite for the default module
@@ -106,6 +107,7 @@ class Phprojekt_AllTests
         $suite->addTestSuite('Phprojekt_ConverterTest');
         $suite->addTestSuite('Phprojekt_ModelInformation_DefaultTest');
         $suite->addTestSuite('Phprojekt_Tags_DefaultTest');
+        $suite->addTestSuite('Phprojekt_Filter_ParseTreeTest');
 
         return $suite;
     }
