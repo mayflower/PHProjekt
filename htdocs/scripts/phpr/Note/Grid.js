818841c5b97e4378ce844733d800e2630f198e09
Nina Schmitt <nina.schmitt@mayflower.de>
2008-04-21 13:11:38 +0000

    added Note module to client

---
+++ htdocs/scripts/phpr/Note/Grid.js
@@ -0,0 +1,14 @@
+dojo.provide("phpr.Note.Grid");
+
+dojo.require("phpr.Default.Grid");
+
+dojo.declare("phpr.Note.Grid", phpr.Default.Grid, {
+    // summary: 
+    //    This class is responsible for rendering the Grid of a Note module
+    // description: 
+    //    The Grid for the Note module is rendered -  at the moment it is exactly
+    //    the same as in the Default module
+    constructor: function(){
+        
+    }
+});
