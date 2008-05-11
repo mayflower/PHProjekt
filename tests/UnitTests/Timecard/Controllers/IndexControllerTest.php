a4c4ce178c51f5fd55a317c70d96504ca8d612f7
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-05-11 15:18:02 +0000

    Added tests for timecard start and stop.

--- tests/UnitTests/Timecard/Controllers/IndexControllerTest.php
+++ tests/UnitTests/Timecard/Controllers/IndexControllerTest.php
@@ -108,4 +108,247 @@ class Timecard_IndexController_Test extends PHPUnit_Framework_TestCase
         // checking some parts of the index template
         $this->assertTrue(strpos($response, '"numRows":4} ') > 0);
     }
+    
+    public function testJsonStartAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonStart','controller'=>'index','module'=>'Timecard'));
+
+        $request->setBaseUrl($config->webpath.'index.php/Timecard/index/jsonStart');
+        $request->setPathInfo('/Timecard/index/jsonStart');
+        $request->setRequestUri('/Timecard/index/jsonStart');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos($response, 'The Item was added correctly"') > 0);
+    }
+    
+    public function testJsonStopAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonStart','controller'=>'index','module'=>'Timecard'));
+
+        $request->setBaseUrl($config->webpath.'index.php/Timecard/index/jsonStop');
+        $request->setPathInfo('/Timecard/index/jsonStop');
+        $request->setRequestUri('/Timecard/index/jsonStop');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos($response, 'The Item was saved correctly') > 0);
+    }
+    
+    public function testJsonStopActionNoRecordOpen()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonStart','controller'=>'index','module'=>'Timecard'));
+
+        $request->setBaseUrl($config->webpath.'index.php/Timecard/index/jsonStop');
+        $request->setPathInfo('/Timecard/index/jsonStop');
+        $request->setRequestUri('/Timecard/index/jsonStop');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos($response, 'The Item was not found') > 0);
+    }
 }
