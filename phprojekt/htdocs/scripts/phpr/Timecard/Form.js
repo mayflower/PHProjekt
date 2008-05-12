5d3da25c37943ccc6b96a1ab2698fcd96293985b
Nina Schmitt <nina.schmitt@mayflower.de>
2008-05-12 15:14:35 +0000

    added changes for timeproj

--- phprojekt/htdocs/scripts/phpr/Timecard/Form.js
+++ phprojekt/htdocs/scripts/phpr/Timecard/Form.js
@@ -13,7 +13,7 @@ dojo.declare("phpr.Timecard.Form",phpr.Default.Form, {
     projectm:0,
     date:null,
     
-	constructor: function(){
+	constructor: function(/*Object*/main, /*Object*/paramsIn){
          // summary:    
         //    render the form on construction
         // description: 
@@ -30,24 +30,53 @@ dojo.declare("phpr.Timecard.Form",phpr.Default.Form, {
 			options.push(range[j]);
 			j++;
 		}
-        this.render(["phpr.Timecard.template", "form.html"], dojo.byId("tcBookings"),{values:options, dateForm: dateFormatted,
-        date: this.date});
+        var params = {
+            values:options, 
+            date: this.date,
+            dateForm: dateFormatted,
+            tcProjecthValue: 0,
+            tcProjectmValue:0
+        };
+        if (dojo.isObject(paramsIn)) {
+            dojo.mixin(params, paramsIn);
+        }
+        this.render(["phpr.Timecard.template", "form.html"], dojo.byId("tcBookings"),params);
         dojo.addOnLoad(dojo.hitch(this, "onLoaded"));
     },
     onLoaded: function(){
-            dojo.connect(dijit.byId("tcSubmitButton"), "onClick", dojo.hitch(this, "submitForm"));
+             dojo.connect(dijit.byId("date"), "onChange", function(date){
+                 console.debug("date im onchange",date);
+                 dojo.publish("Timecard.changeDate",[date]);
+             });
+             dojo.connect(dijit.byId("tcSubmitButton"),        "onClick", dojo.hitch(this, "submitTcBookingForm"));
+             dojo.connect(dijit.byId("tcProjectSubmitButton"), "onClick", dojo.hitch(this, "submitTcProjectBookingForm"));
         },
-   submitForm: function(){
+   submitTcBookingForm: function(){
         // summary: 
         //    This function is responsible for submitting the formdata
         // description:
         //    This function sends the form data as json data to the server and publishes
         //    a form.Submitted topic after the data was send.
-		this.sendData = dijit.byId('bookingForm').getValues();
+		this.sendData = dijit.byId('tcBookingForm').getValues();
 		phpr.send({
 			url:       phpr.webpath + 'index.php/' + phpr.module + '/index/jsonSave/',
 			content:   this.sendData,
-            onSuccess: this.publish("reload")
+            onSuccess: this.publish("reload",[{date:this.sendData.date, dateForm:dojo.date.locale.format(this.sendData.date, {formatLength:'full',selector:'date', locale:this.lang})}])
          });  
+         
+    },
+    submitTcProjectBookingForm: function(){
+        // summary: 
+        //    This function is responsible for submitting the formdata
+        // description:
+        //    This function sends the form data as json data to the server and publishes
+        //    a form.Submitted topic after the data was send.
+		this.sendData = dijit.byId('tcBookingForm').getValues();
+		phpr.send({
+			url:       phpr.webpath + 'index.php/Timeproj/index/jsonSave/',
+			content:   this.sendData,
+            onSuccess: this.publish("reload",[{date:this.sendData.date, dateForm:dojo.date.locale.format(this.sendData.date, {formatLength:'full',selector:'date', locale:this.lang})}])
+         });  
+         
     }
 });
