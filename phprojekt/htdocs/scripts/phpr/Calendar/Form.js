4c0a8e117fb0d511e13b8979e59697fa42897019
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-06-01 15:07:36 +0000

    Added calendar module on FE and BE. Basic functionality.

---
+++ phprojekt/htdocs/scripts/phpr/Calendar/Form.js
@@ -0,0 +1,14 @@
+dojo.provide("phpr.Calendar.Form");
+
+dojo.require("phpr.Default.Form");
+
+dojo.declare("phpr.Calendar.Form", phpr.Default.Form, {
+    // summary: 
+    //    This class is responsible for rendering the Form of a Calendar module
+    // description: 
+    //    The Form for the Calendar module is rendered -  at the moment it is exactly
+    //    the same as in the Default module
+    constructor: function(){
+        
+    }
+});
---
+++ phprojekt/htdocs/scripts/phpr/Calendar/Main.js
@@ -0,0 +1,22 @@
+dojo.provide("phpr.Calendar.Main");
+
+dojo.require("phpr.Default.Main");
+// app specific files
+dojo.require("phpr.Calendar.Tree");
+dojo.require("phpr.Calendar.Grid");
+dojo.require("phpr.Calendar.Form");
+
+dojo.declare("phpr.Calendar.Main", phpr.Default.Main, {
+	 constructor: function(webpath){
+	 	this.module     = "Calendar";
+		this.gridWidget = phpr.Calendar.Grid;
+		this.formWidget = phpr.Calendar.Form;
+		this.treeWidget = phpr.Calendar.Tree;
+	 	dojo.subscribe("Calendar.load", this, "load");
+        dojo.subscribe("Calendar.changeProjekt",this, "loadSubElements"); 
+		dojo.subscribe("Calendar.reload", this, "reload");
+		dojo.subscribe("Calendar.openForm",this, "openForm");
+		dojo.subscribe("Calendar.form.Submitted",this, "submitForm");
+	 }
+
+});
