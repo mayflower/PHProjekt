34bb2312d3a828ca04399bfa8e35431c99556be4
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-06-02 15:45:39 +0000

    Calendar module

---
+++ phprojekt/application/Calendar/Controllers/IndexController.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Calendar Module Controller for PHProjekt 6.0
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id:
+ * @author     Eduardo Polidor <polidor@mayflower.de>
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ */
+
+/**
+ * Default Calendar Module Controller for PHProjekt 6.0
+ *
+ * @copyright  2007 Mayflower GmbH (http://www.mayflower.de)
+ * @version    Release: @package_version@
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @package    PHProjekt
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Eduardo Polidor <polidor@mayflower.de>
+ */
+class Calendar_IndexController extends IndexController
+{
+    /**
+     * Saves the current item
+     * Save if you are add one or edit one.
+     * Use the model module for get the data
+     *
+     * If there is an error, the save will return a Phprojekt_PublishedException
+     * If not, the return is a string with the same format than the Phprojekt_PublishedException
+     * but with success type
+     *
+     * @requestparam integer id ...
+     *
+     * @return void
+     */
+    public function jsonSaveAction()
+    {
+        $translate = Zend_Registry::get('translate');
+        $id        = (int) $this->getRequest()->getParam('id');
+
+        if (empty($id)) {
+            $model   = $this->getModelObject();
+            $message = $translate->translate('The Item was added correctly');
+        } else {
+            $model   = $this->getModelObject()->find($id);
+            $message = $translate->translate('The Item was edited correctly');
+        }
+
+        Default_Helpers_Save::save($model, $this->getRequest()->getParams());
+
+        $return    = array('type'    => 'success',
+                           'message' => $message,
+                           'code'    => 0,
+                           'id'      => $model->id);
+
+        echo Phprojekt_Converter_Json::convert($return);
+    }
+}
