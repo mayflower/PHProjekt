b7fcb742d94498a4f2dede390ef16af98a10ec3c
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-07-10 05:05:06 +0000

    Added user module setting backend.

---
+++ tests/UnitTests/User/Controllers/IndexControllerTest.php
@@ -0,0 +1,448 @@
+<?php
+/**
+ * Unit test
+ *
+ * LICENSE: Licensed under the terms of the PHProjekt 6 License
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    CVS: $Id:
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+*/
+require_once 'PHPUnit/Framework.php';
+
+/**
+ * Tests for Index Controller
+ *
+ * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @license    http://phprojekt.com/license PHProjekt 6 License
+ * @version    Release: @package_version@
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 1.0
+ * @author     Eduardo Polidor <polidor@mayflower.de>
+ */
+class User_IndexController_Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Test the user list
+     */
+    public function testGetUsersAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonList','controller'=>'index','module'=>'user'));
+
+        $request->setBaseUrl($config->webpath.'index.php/User/index/jsonGetUsers');
+        $request->setPathInfo('/User/index/jsonGetUsers');
+        $request->setRequestUri('/User/index/jsonGetUsers');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos(strtolower($response), strtolower('"numRows":1')) > 0);
+    }
+
+    /**
+     * Test user set setting
+     */
+    public function testSetSettingAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonList','controller'=>'index','module'=>'user'));
+
+        $request->setBaseUrl($config->webpath.'index.php/User/index/jsonSetSetting/name/timezone/value/3');
+        $request->setPathInfo('/User/index/jsonSetSetting/name/timezone/value/3');
+        $request->setRequestUri('/User/index/jsonSetSetting/name/timezone/value/3');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos(strtolower($response), strtolower('Value saved successful')) > 0);
+    }
+
+    /**
+     * Test user get setting
+     */
+    public function testGetSettingAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonList','controller'=>'index','module'=>'user'));
+
+        $request->setBaseUrl($config->webpath.'index.php/User/index/jsonGetSetting/name/timezone');
+        $request->setPathInfo('/User/index/jsonGetSetting/name/timezone');
+        $request->setRequestUri('/User/index/jsonGetSetting/name/timezone');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos(strtolower($response), strtolower('"3"')) > 0);
+    }
+
+    /**
+     * Test user get setting list
+     */
+    public function testGetSettingListAction()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonList','controller'=>'index','module'=>'user'));
+
+        $request->setBaseUrl($config->webpath.'index.php/User/index/jsonGetSettingList');
+        $request->setPathInfo('/User/index/jsonGetSettingList');
+        $request->setRequestUri('/User/index/jsonGetSettingList');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos(strtolower($response), strtolower('{"timezone":"3"}')) > 0);
+    }
+
+    /**
+     * Test user delete setting
+     */
+    public function testDeleteSetting()
+    {
+        $request = new Zend_Controller_Request_Http();
+        $response = new Zend_Controller_Response_Http();
+
+        $config = Zend_Registry::get('config');
+
+        $request->setParams(array('action'=>'jsonList','controller'=>'index','module'=>'user'));
+
+        $request->setBaseUrl($config->webpath.'index.php/User/index/jsonDeleteSetting/name/timezone');
+        $request->setPathInfo('/User/index/jsonDeleteSetting/name/timezone');
+        $request->setRequestUri('/User/index/jsonDeleteSetting/name/timezone');
+
+        // getting the view information
+        $request->setModuleKey('module');
+        $request->setControllerKey('controller');
+        $request->setActionKey('action');
+        $request->setDispatched(false);
+
+        $view = new Zend_View();
+        $view->addScriptPath(PHPR_CORE_PATH . '/Default/Views/scripts/');
+
+        $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer($view);
+        $viewRenderer->setViewBasePathSpec(':moduleDir/Views');
+        $viewRenderer->setViewScriptPathSpec(':action.:suffix');
+
+        Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
+
+        // Languages Set
+        Zend_Loader::loadClass('Phprojekt_Language', PHPR_CORE_PATH);
+        $translate = new Phprojekt_Language($config->language);
+
+        // Front controller stuff
+        $front = Zend_Controller_Front::getInstance();
+        $front->setDispatcher(new Phprojekt_Dispatcher());
+
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler());
+        $front->setDefaultModule('Default');
+
+        foreach (scandir(PHPR_CORE_PATH) as $module) {
+            $dir = PHPR_CORE_PATH . DIRECTORY_SEPARATOR . $module;
+
+            if (is_dir(!$dir)) {
+                continue;
+            }
+
+            if (is_dir($dir . DIRECTORY_SEPARATOR . 'Controllers')) {
+                $front->addModuleDirectory($dir);
+            }
+
+            $helperPath = $dir . DIRECTORY_SEPARATOR . 'Helpers';
+
+            if (is_dir($helperPath)) {
+                $view->addHelperPath($helperPath, $module . '_' . 'Helpers');
+                Zend_Controller_Action_HelperBroker::addPath($helperPath);
+            }
+        }
+
+        Zend_Registry::set('view', $view);
+        $view->webPath  = $config->webpath;
+        Zend_Registry::set('translate', $translate);
+
+        $front->setModuleControllerDirectoryName('Controllers');
+        $front->addModuleDirectory(PHPR_CORE_PATH);
+
+        $front->setParam('useDefaultControllerAlways', true);
+
+        $front->throwExceptions(true);
+
+        // Getting the output, otherwise the home page will be displayed
+        ob_start();
+
+        $front->dispatch($request, $response);
+        $response = ob_get_contents();
+
+        ob_end_clean();
+
+        // checking some parts of the index template
+        $this->assertTrue(strpos(strtolower($response), strtolower('Value deleted successful')) > 0);
+    }
+
+
+}
