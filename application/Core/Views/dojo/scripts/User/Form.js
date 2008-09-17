c12d76de70d66daef96628ddd2b11e9a1c16c946
Eduardo Polidor <eduardo.polidor@mayflower.de>
2008-09-17 09:35:07 +0000

    Removed user delete button

--- application/Core/Views/dojo/scripts/User/Form.js
+++ application/Core/Views/dojo/scripts/User/Form.js
@@ -1,4 +1,13 @@
 dojo.provide("phpr.User.Form");
 
 dojo.declare("phpr.User.Form", phpr.Core.Form, {
+    
+    setPermissions:function (data) {
+        this._writePermissions = true;
+        
+        // users can't be deleted
+        this._deletePermissions = false;
+        this._accessPermissions = true;
+    },
+    
 });
