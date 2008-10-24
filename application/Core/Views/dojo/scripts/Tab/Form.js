6d89e85c5707a5626f79499fed55235432e5c295
Gustavo Solt <gustavo.solt@mayflower.de>
2008-10-24 13:49:41 +0000

    Update tab cache

--- application/Core/Views/dojo/scripts/Tab/Form.js
+++ application/Core/Views/dojo/scripts/Tab/Form.js
@@ -1,4 +1,9 @@
 dojo.provide("phpr.Tab.Form");
 
 dojo.declare("phpr.Tab.Form", phpr.Core.Form, {
+    updateData: function(){
+        phpr.DataStore.deleteData({url: this._url});
+        var tabStore = new phpr.Store.Tab();
+        tabStore.update();
+    }
 });
