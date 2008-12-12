8ecfdd7c4a3830a0c0584fc6bb905d7412d82779
Gustavo Solt <gustavo.solt@mayflower.de>
2008-12-12 13:06:00 +0000

    Add Copyright (c) 2008 Change release version to 6.0

--- phprojekt/htdocs/setup/setup.inc.php
+++ phprojekt/htdocs/setup/setup.inc.php
@@ -4,12 +4,12 @@
  *
  * LICENSE: Licensed under the terms of the GNU Publice License
  *
- * @copyright  Copyright (c) 2007 Mayflower GmbH (http://www.mayflower.de)
+ * @copyright  Copyright (c) 2008 Mayflower GmbH (http://www.mayflower.de)
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
  *             GNU Public License 2.0
  * @version    CVS: $Id: setup.inc.php 828 2008-07-07 02:05:54Z gustavo $
  * @link       http://www.phprojekt.com
- * @since      File available since Release 1.0
+ * @since      File available since Release 6.0
 */
 
 function checkServer()
