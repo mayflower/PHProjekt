3c7dcc8bb85b40503214330a216f914242710a26
Gustavo Solt <gustavo.solt@mayflower.de>
2008-12-15 16:08:23 +0000

    Rename phprojekt6 to phprojekt only. Change error message

--- phprojekt/htdocs/index.php
+++ phprojekt/htdocs/index.php
@@ -46,7 +46,7 @@ try {
     Zend_Registry::set('config', $config);
 } catch (Zend_Config_Exception $error) {
     $error->getMessage();
-    echo 'You need to continue the file configuration.ini';
+    echo 'You need the file configuration.ini to continue';
     die();
 }
 
