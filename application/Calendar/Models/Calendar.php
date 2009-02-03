c7717a4e40dc03af03e572ed8aafef2b15d8e7cd
Gustavo Solt <gustavo.solt@mayflower.de>
2009-02-03 17:29:43 +0000

    Remove svn:eol-styole Add svn:eol-style: LF Add svn:keywords: Author Date Id Revision

--- application/Calendar/Models/Calendar.php
+++ application/Calendar/Models/Calendar.php
@@ -14,7 +14,7 @@
  * @copyright  Copyright (c) 2008 Mayflower GmbH (http://www.mayflower.de)
  * @package    PHProjekt
  * @license    LGPL 2.1 (See LICENSE file)
- * @version    $Id:$
+ * @version    $Id$
  * @link       http://www.phprojekt.com
  * @author     Eduardo Polidor <polidor@mayflower.de>
  * @since      File available since Release 6.0
