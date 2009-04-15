41c4eef667b60a9fa48b76b90f5d051decdba25c
Sven Rautenberg <sven.rautenberg@mayflower.de>
2009-04-15 14:50:18 +0000

    Code Style fix.

--- phprojekt/tests/UnitTests/Minutes/Controllers/IndexControllerTest.php
+++ phprojekt/tests/UnitTests/Minutes/Controllers/IndexControllerTest.php
@@ -34,36 +34,67 @@ class Minutes_IndexController_Test extends FrontInit
     /**
      * Test the empty Minutes list
      */
-    public function testJsonListActionBeforeAll()
+    public function testJsonListActionWithEmptyList()
     {
         $this->setRequestUrl('Minutes/index/jsonList/nodeId/1');
         $this->request->setParam('start', 0);
         $response = $this->getResponse();
+        
         $this->assertTrue(strpos($response, '{"metadata":[]}') > 0);
     }
-
     /**
      * Request empty form
      */
     public function testJsonDetailActionGetEmptyForm()
     {
         $this->setRequestUrl('Minutes/index/jsonDetail/id/0');
-        $this->request->setParam('start',0);
+        $this->request->setParam('start', 0);
         $response = $this->getResponse();
         
-        $this->assertTrue(strpos($response, '{"metadata":[{"key":"projectId","label":"Select","type":"hidden",') > 0, "Response was: " . $response);
-        $this->assertTrue(strpos($response, ',"data":[{"id":null,"projectId":"","rights":{"currentUser":{"moduleId":"11","itemId":null') > 0);
+        $this->assertTrue(strpos($response, '{"metadata":[{"key":"projectId","label":"Select","type":"hidden",') > 0,
+            "Response was: " . $response);
+        $this->assertTrue(strpos($response, ',"data":[{"id":null,"projectId":"","rights":' 
+            . '{"currentUser":{"moduleId":"11","itemId":null') > 0);
     }
     /**
      * Test of json save Minutes
      */
     public function testJsonSaveActionSaveFirstMinutes()
     {
-        $this->markTestIncomplete('Not yet implemented');
-        $this->setRequestUrl('Minutes/index/jsonList/nodeId/1');
-        $this->request->setParam('start', 0);
+        
+        $this->setRequestUrl('Minutes/index/jsonSave/id/0');
+        //$this->request->setParam('id', 0);
+
+/*        $this->request->setParam('checkAccessAccess[1]', 1);
+        $this->request->setParam('checkAccessAccess[1]', 1);
+        $this->request->setParam('checkAdminAccess[1]', 1);
+        $this->request->setParam('checkCopyAccess[1]', 1);
+        $this->request->setParam('checkCreateAccess[1]', 1);
+        $this->request->setParam('checkDeleteAccess[1]', 1);
+        $this->request->setParam('checkDownloadAccess[1]', 1);
+        $this->request->setParam('checkReadAccess[1]', 1);
+        $this->request->setParam('checkWriteAccess[1]', 1);
+        $this->request->setParam('dataAccessAdd', 2);
+        $this->request->setParam('dataAccess[1]', 'admin');
+        */
+        $this->request->setParam('projectId', 1);
+        $this->request->setParam('title', 'TestTitle');
+        $this->request->setParam('description', 'TestDescription');
+        $this->request->setParam('meetingDate', 'Thu Apr 09 2009 00:00:00 GMT+0200');
+        $this->request->setParam('startTime', 'Thu Jan 01 1970 03:00:00 GMT+0100');
+        $this->request->setParam('endTime', 'Thu Jan 01 1970 03:00:00 GMT+0100');
+        $this->request->setParam('place', 'TestPlace');
+        $this->request->setParam('moderator', 'TestModerator');
+        $this->request->setParam('participantsInvited[]', 0);
+        $this->request->setParam('participantsAttending[]', 0);
+        $this->request->setParam('participantsExcused[]', 0);
+        $this->request->setParam('recipients[]', 0);
+        $this->request->setParam('itemStatus', 1);
+        $this->request->setParam('string', '');
+        $this->request->setParam('requiredField1', '(*) Required Field');
+        
         $response = $this->getResponse();
-        $this->assertTrue(strpos($response, '{"metadata":[]}') > 0);
+        $this->assertTrue(strpos($response, Minutes_IndexController::ADD_TRUE_TEXT) > 0, "Response was: '$response'");
     }
 
     /**
