3bb461b3a21ae440bccefe843ae57b1b82edd46b
Markus Wolff <markus.wolff@mayflower.de>
2009-04-16 16:30:10 +0000

    + added item fields + added loading item data from server

--- phprojekt/application/Minutes/Views/dojo/scripts/Form.js
+++ phprojekt/application/Minutes/Views/dojo/scripts/Form.js
@@ -40,6 +40,8 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
         //    See Default/Form.js, method addAccessTab for
         //    a more detailed example of adding tabs.
         
+    	this.getInvitedList(); // preload invited users list to populate subform selectbox
+    	
         var itemsData = this.render(["phpr.Minutes.template", "minutesItemGrid.html"], null, {
             // no placeholders used atm.
         });
@@ -76,30 +78,30 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
     _buildGrid: function() {
         console.debug('Building grid...');
         var schema = [{
-                        name:     'Topic',
-                        field:    'topicId',
-                        styles:   "text-align: center;",
-                        width:    '30px'
+                name:     'Topic',
+                field:    'topicId',
+                styles:   "text-align: center;",
+                width:    '30px'
             },
             {
-                        name:     'Title',
-                        field:    'title',
-                        styles:   "text-align: left;",
-                        width:    '100px'
+                name:     'Title',
+                field:    'title',
+                styles:   "text-align: left;",
+                width:    '100px'
             },
             {
-                        name:     'Comment',
-                        field:    'comment',
-                        styles:   "text-align: left;",
-                        width:    '270px'
+                name:     'Comment',
+                field:    'comment',
+                styles:   "text-align: left;",
+                width:    '270px'
             },
             {
-                        name:     'Type',
-                        field:    'topicType',
-                        styles:   "text-align: center;",
-                        width:    '50px',
-                        options:    [1,2],
-                        values:     ['Topic','Comment']
+                name:     'Type',
+                field:    'topicType',
+                styles:   "text-align: center;",
+                width:    '50px',
+                options:    [1,2],
+                values:     ['Topic','Comment']
             }
         ];
         
@@ -121,34 +123,113 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
                         rows: [schema]
             }]
         }, document.createElement('div'));
+        
+        dojo.connect(grid, 'onRowDblClick', dojo.hitch(this, function(e) {
+            var data = e.grid.getItem(e.rowIndex);
+            this.getSubFormData(data.id);
+            console.debug('Row doubleclicked:');
+            console.debug(data);
+        }));
     
         dojo.byId('tabItems').appendChild(grid.domNode);
         grid.startup();
     },
     
+    
+    _itemFormData: null,
+    _invitedList:  [{id:1,name:'test1'},{id:2,name:'test2'}],
+    
+    getSubFormData: function(itemId) {
+    	var responseHandler = dojo.hitch(this, function(responseObject, ioArgs) {
+            console.debug('getSubFormData('+itemId+'): Got XHR response:');
+            console.debug(responseObject.data);
+            this._itemFormData = responseObject.data[0];
+            this.loadSubForm();
+            return responseObject;
+        });
+    	dojo.xhrGet({
+            // The following URL must match that used to test the server.
+            url: "index.php/Minutes/item/jsonDetail/minutesId/"+this.id+'/id/'+itemId,
+            handleAs: "json",
+            load: responseHandler,
+            error: function(e) {
+    			console.debug('getSubFormData has encountered an error: ' + e);
+    		}
+    	});
+    },
+    
+    getInvitedList: function() {
+    	var responseHandler = dojo.hitch(this, function(responseObject, ioArgs) {
+            console.debug('getInvitedList: Got XHR response:');
+            console.debug(responseObject.data);
+            this._invitedList = responseObject.data;
+            return responseObject;
+        });
+    	dojo.xhrGet( {
+            // The following URL must match that used to test the server.
+    		url: "index.php/Minutes/index/jsonListUser/id/"+this.id,
+            handleAs: "json",
+            load: responseHandler,
+            error: function(e) {
+    			console.debug('getInvitedList has encountered an error: ' + e);
+    		}
+    	});
+    },
+    
     loadSubForm: function() {
     	console.debug('Loading subform...');
-    	// @todo Request data from server
-    	var fakeData     = {
-    			id: 1,
-    			ownerId:    2,
-    			minutesId:    1,
-    			projectId:    1,
-    			topicId:    1,
-    			users:      [{id:1,name:'test1'},{id:2,name:'test2'}]
-    	};
+ 	
+    	if (!this._itemFormData) {
+    		// Use default empty dataset
+	    	this._itemFormData = {
+				id:         '',
+				ownerId:    '',
+				minutesId:  '',
+				projectId:  '',
+				topicId:    '',
+				topicType:  '',
+				userId:     '',
+				sortOrder:  '',
+				title:      '',
+				comment:	'',
+				topicDate:  '2009-05-01'
+	    	};
+    	}
     	// @todo Add server data to placeholder object
     	var placeholders = {
-    			lblTitle:   'Title',
-    			lblComment: 'Comment',
-    			lblOwnerId: 'Owner'
+			lblTitle:     'Title',
+			lblComment:   'Comment',
+			lblOwnerId:   'Owner',
+			lblTopicType: 'Type',
+			lblTopicDate: 'Date',
+			users:		  this._invitedList,
+			types:        [{id:1,name:'Topic'},{id:2,name:'Task'}]
     	};
-    	placeholders = dojo.mixin(placeholders, fakeData);
-    	// @todo Render the template  
+    	placeholders = dojo.mixin(placeholders, this._itemFormData);
+    	// Render the template  
     	var subForm = this.render(["phpr.Minutes.template", "minutesItemForm.html"], 
     			                  dojo.byId('minutesDetailsRight'), placeholders);
-    	// @todo Attach to minutesItemForm's onsubmit event
-    	// @todo Implement writing data back to server
-    	// @todo Attach to general save button's onclick event, submit minutesItemsForm as well when triggered
+    	
+    	// Attach to minutesItemForm's onsubmit event
+    	var formWidget = dijit.byId('minutesItemForm');
+    	dojo.connect(formWidget, 'onSubmit', dojo.hitch(this, function(e) {
+    		this.itemFormData = null; // reset form data
+    		console.debug('Submitted item form');
+    		console.debug(formWidget.getValues());
+    		// @todo Implement writing data back to server
+    	}));
+    	// Populate form
+    	console.debug('Populating form...');
+    	console.debug(this._itemFormData);
+    	for(var i in this._itemFormData) {
+    		console.debug('Setting form element "'+i+'" to: ' + this._itemFormData[i]);
+    		formWidget.attr(i, this._itemFormData[i]);
+    	}
+    	//formWidget.setValues(this._itemFormData);
+    	
+    	// @todo Attach to general save button's onclick event, 
+    	//       submit minutesItemsForm as well when triggered.
+    	//       Also reload _invitedList, in case something has
+    	//       changed.
     }
 });
