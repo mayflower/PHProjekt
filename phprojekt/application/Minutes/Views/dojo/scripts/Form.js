6d69ae408dca52e076666eb6f7e4c4c225650e0b
Markus Wolff <markus.wolff@mayflower.de>
2009-04-17 14:10:10 +0000

    * improved save function * improved layout ! grid does not yet refresh

--- phprojekt/application/Minutes/Views/dojo/scripts/Form.js
+++ phprojekt/application/Minutes/Views/dojo/scripts/Form.js
@@ -57,26 +57,52 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
         //    processed. Neccessary because the datagrid won't render
         //    unless dimensions of all surrounding elements are known.
         // I used this for testing purposes. Also renders only in gridBox
-
-        var tabs = this.form; //dijit.byId('tabItems');
+    	
+        var tabs = this.form;
         dojo.connect(tabs, "selectChild", dojo.hitch(this, function(child) {
             if (child.id == 'tabItems') {
-                dojo.byId('minutesDetailsRight').style.display = 'inline';
-                dojo.byId('itemsFormTab').style.display = 'none';
+                //dojo.byId('minutesDetailsRight').style.display = 'inline';
                 this.loadSubForm();
                 this.url  = phpr.webpath + "index.php/Minutes/item/jsonList/minutesId/" + this.id;
                 phpr.DataStore.addStore({"url": this.url});
                 phpr.DataStore.requestData({"url": this.url, processData: dojo.hitch(this, "_buildGrid")});
+            	//dojo.connect(dijit.byId('submitButton'), 'onClick', this.saveSubFormData);
             } else {
-                dojo.byId('minutesDetailsRight').style.display = 'none';
+                //dojo.byId('minutesDetailsRight').style.display = 'none';
             }
-        })); 
+        }));
+        dojo.byId('itemsFormTab').style.display = 'none';
+        var minutesBox = new dijit.layout.ContentPane({
+									region: 'center',
+									id: 'minutesBox'
+					         }, 
+					    	 dojo.doc.createElement('div'));
+        var minutesLayout = new dijit.layout.BorderContainer({
+					            	design: 'sidebar',
+					            	id: 'minutesLayout'
+					        	}, 
+					        	dojo.doc.createElement('div'));
+        var minutesGridBox = new dijit.layout.ContentPane({
+						        	region: 'center',
+						        	id: 'minutesGridBox'
+						     }, 
+    	                     dojo.doc.createElement('div'));
+        var minutesDetailsRight = new dijit.layout.ContentPane({
+						        	region: 'right',
+						        	id: 'minutesDetailsRight',
+						        	style: 'width: 50%;'
+						     }, 
+						     dojo.doc.createElement('div'));
+        minutesLayout.addChild(minutesGridBox);
+        minutesLayout.addChild(minutesDetailsRight);
+        //minutesBox.addChild(minutesLayout);
+        minutesBox.attr("content", minutesLayout.domNode);
+        dijit.byId('tabItems').attr('content', minutesBox.domNode);        
     },
     
     url: null,
     
     _buildGrid: function() {
-        console.debug('Building grid...');
         var schema = [{
                 name:     'Topic',
                 field:    'topicId',
@@ -105,13 +131,7 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
             }
         ];
         
-        var content = dojo.clone(phpr.DataStore.getData({"url": this.url}));
-        var gridData = { items: new Array() };
-        for (var i in content) {
-            gridData.items.push(content[i]);
-        }
-
-        var store = new dojo.data.ItemFileWriteStore({data: gridData});
+        var store = this._getItemGridStore();
         
         var grid = new dojox.grid.DataGrid({
             store: store,
@@ -127,28 +147,61 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
         dojo.connect(grid, 'onRowDblClick', dojo.hitch(this, function(e) {
             var data = e.grid.getItem(e.rowIndex);
             this.getSubFormData(data.id);
-            console.debug('Row doubleclicked:');
-            console.debug(data);
         }));
     
-        dojo.byId('tabItems').appendChild(grid.domNode);
+        //dojo.byId('tabItems').appendChild(grid.domNode);
+        dijit.byId('minutesGridBox').attr('content', grid.domNode);
         grid.startup();
+        this._itemGrid = grid;
     },
     
+    _getItemGridStore: function() {
+    	var content = dojo.clone(phpr.DataStore.getData({"url": this.url}));
+        var gridData = { items: new Array() };
+        for (var i in content) {
+            gridData.items.push(content[i]);
+        }
+
+        return new dojo.data.ItemFileWriteStore({data: gridData});
+    },
     
+    updateGrid: function() {
+    	if (this._itemGrid) {
+    		this._itemGrid.setStore(this._getItemGridStore());
+    	}
+    },
+    
+    _itemGrid:     null,
     _itemFormData: null,
     _invitedList:  [{id:1,name:'test1'},{id:2,name:'test2'}],
     
+    saveSubFormData: function() {
+    	var responseHandler = dojo.hitch(this, function(responseObject, ioArgs) {
+            this._itemFormData = null;
+            this.loadSubForm();
+            //this._buildGrid();
+            this.updateGrid();
+            return responseObject;
+        });
+    	dojo.xhrPost({
+            // The following URL must match that used to test the server.
+            url: "index.php/Minutes/item/jsonSave/",
+            handleAs: "text",
+            load: responseHandler,
+            error: function(e) {
+    			console.debug('saveSubFormData() has encountered an error: ' + e);
+    		},
+    		form: "minutesItemForm"
+    	});
+    },
+    
     getSubFormData: function(itemId) {
     	var responseHandler = dojo.hitch(this, function(responseObject, ioArgs) {
-            console.debug('getSubFormData('+itemId+'): Got XHR response:');
-            console.debug(responseObject.data);
             this._itemFormData = responseObject.data[0];
             this.loadSubForm();
             return responseObject;
         });
     	dojo.xhrGet({
-            // The following URL must match that used to test the server.
             url: "index.php/Minutes/item/jsonDetail/minutesId/"+this.id+'/id/'+itemId,
             handleAs: "json",
             load: responseHandler,
@@ -160,13 +213,10 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
     
     getInvitedList: function() {
     	var responseHandler = dojo.hitch(this, function(responseObject, ioArgs) {
-            console.debug('getInvitedList: Got XHR response:');
-            console.debug(responseObject.data);
             this._invitedList = responseObject.data;
             return responseObject;
         });
     	dojo.xhrGet( {
-            // The following URL must match that used to test the server.
     		url: "index.php/Minutes/index/jsonListUser/id/"+this.id,
             handleAs: "json",
             load: responseHandler,
@@ -177,19 +227,18 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
     },
     
     loadSubForm: function() {
-    	console.debug('Loading subform...');
- 	
     	if (!this._itemFormData) {
     		// Use default empty dataset
+    		// @todo FIXME
 	    	this._itemFormData = {
 				id:         '',
 				ownerId:    '',
-				minutesId:  '',
+				minutesId:  this.id,
 				projectId:  '',
 				topicId:    '',
 				topicType:  '',
 				userId:     '',
-				sortOrder:  '',
+				sortOrder:  '1',
 				title:      '',
 				comment:	'',
 				topicDate:  '2009-05-01'
@@ -199,37 +248,19 @@ dojo.declare("phpr.Minutes.Form", phpr.Default.Form, {
     	var placeholders = {
 			lblTitle:     'Title',
 			lblComment:   'Comment',
-			lblOwnerId:   'Owner',
+			lblUserId:    'Who',
 			lblTopicType: 'Type',
 			lblTopicDate: 'Date',
+			lblSubmit:    'Save',
 			users:		  this._invitedList,
 			types:        [{id:1,name:'Topic'},{id:2,name:'Task'}]
     	};
     	placeholders = dojo.mixin(placeholders, this._itemFormData);
+    	
     	// Render the template  
-    	var subForm = this.render(["phpr.Minutes.template", "minutesItemForm.html"], 
-    			                  dojo.byId('minutesDetailsRight'), placeholders);
-    	
-    	// Attach to minutesItemForm's onsubmit event
-    	var formWidget = dijit.byId('minutesItemForm');
-    	dojo.connect(formWidget, 'onSubmit', dojo.hitch(this, function(e) {
-    		this.itemFormData = null; // reset form data
-    		console.debug('Submitted item form');
-    		console.debug(formWidget.getValues());
-    		// @todo Implement writing data back to server
-    	}));
-    	// Populate form
-    	console.debug('Populating form...');
-    	console.debug(this._itemFormData);
-    	for(var i in this._itemFormData) {
-    		console.debug('Setting form element "'+i+'" to: ' + this._itemFormData[i]);
-    		formWidget.attr(i, this._itemFormData[i]);
-    	}
-    	//formWidget.setValues(this._itemFormData);
-    	
-    	// @todo Attach to general save button's onclick event, 
-    	//       submit minutesItemsForm as well when triggered.
-    	//       Also reload _invitedList, in case something has
-    	//       changed.
+    	this.render(["phpr.Minutes.template", "minutesItemForm.html"], 
+    	            dojo.byId('minutesDetailsRight'), placeholders);
+    	dojo.connect(dijit.byId('minutesItemFormSubmit'), 'onClick', 
+    			     dojo.hitch(this, this.saveSubFormData));    	
     }
 });
