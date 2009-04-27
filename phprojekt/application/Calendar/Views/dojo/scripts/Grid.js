3438857cbd36d7f43abd7e7f59f18e7528566be7
Gustavo Solt <gustavo.solt@mayflower.de>
2009-04-27 16:57:22 +0000

    Use inherited function where is possible

--- phprojekt/application/Calendar/Views/dojo/scripts/Grid.js
+++ phprojekt/application/Calendar/Views/dojo/scripts/Grid.js
@@ -21,57 +21,8 @@ dojo.provide("phpr.Calendar.Grid");
 
 dojo.declare("phpr.Calendar.Grid", phpr.Default.Grid, {
     onLoaded:function(dataContent) {
-        // summary:
-        //    This function is called when the grid is loaded
-        // description:
-        //    It takes care of setting the grid headers to the right format, displays the contextmenu
-        //    and renders the filter for the grid
-        // Layout of the grid
-        var meta = phpr.DataStore.getMetaData({url: this.url});
-
-        // Data of the grid
-        this.gridData = {
-            items: []
-        };
-        var content = dojo.clone(phpr.DataStore.getData({url: this.url}));
-        for (var i in content) {
-            this.gridData.items.push(content[i]);
-        }
-        store = new dojo.data.ItemFileWriteStore({data: this.gridData});
-
-        // Render save Button
-        this.setSaveChangesButton(meta);
-
-        // Render export Button
-        this.setExportButton(meta);
-
-        if (meta.length == 0) {
-            this._node.attr('content', phpr.drawEmptyMessage('There are no entries on this level'));
-        } else {
-            this.setGridLayout(meta);
-            this.grid = new dojox.grid.DataGrid({
-                store: store,
-                structure: [{
-                            defaultCell: {
-                                editable: true,
-                                type:     dojox.grid.cells.Input,
-                                styles:   'text-align: left;'
-                            },
-                            rows: [this.gridLayout]
-                }]
-            }, document.createElement('div'));
-
-            this.setClickEdit();
-
-            this._node.attr('content', this.grid.domNode);
-            this.grid.startup();
-
-            dojo.connect(this.grid, "onCellClick", dojo.hitch(this, "showForm"));
-            dojo.connect(this.grid, "onApplyCellEdit", dojo.hitch(this, "cellEdited"));
-            dojo.connect(this.grid, "onStartEdit", dojo.hitch(this, "checkCanEdit"));
-        }
-
-        // Because of existing many views, this avoids not to listing the real contents of the DB
+        this.inherited(arguments);
+        // Because of existing many views, don't cache the list contents
         this.updateData();
     }
 });
