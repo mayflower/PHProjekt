f1e0ead24782892ec9b9e3bb1168fde977ccca47
Sven Rautenberg <sven.rautenberg@mayflower.de>
2009-06-09 12:43:58 +0000

    Coding style: Eliminate 115 occurances of 'Variable "_Eol" is not in valid camel caps format'

--- phprojekt/htdocs/Setup/Models/Config.php
+++ phprojekt/htdocs/Setup/Models/Config.php
@@ -37,7 +37,7 @@ class Setup_Models_Config
     const OS_UNIX    = 'Unix';
 
     private $_os      = null;
-    private $_Eol     = null;
+    private $_eol     = null;
     private $_baseDir = null;
 
     public function __construct()
@@ -60,7 +60,7 @@ class Setup_Models_Config
     public function getDefaultProduction($username, $password, $dbname, $adapter, $host)
     {
         $content = $this->_getIntroduction();
-        $content .= $this->_Eol . '[production]' . $this->_Eol;
+        $content .= $this->_eol . '[production]' . $this->_eol;
         $content .= $this->_getLanguage();
         $content .= $this->_getPaths();
         $content .= $this->_getDatabase($username, $password, $dbname, $adapter, $host);
@@ -85,11 +85,11 @@ class Setup_Models_Config
             case 'WIN32':
             case 'WINNT':
                 $this->_os  = self::OS_WINDOWS;
-                $this->_Eol = "\r\n";
+                $this->_eol = "\r\n";
                 break;
             default:
                 $this->_os = self::OS_UNIX;
-                $this->_Eol = $this->_Eol;
+                $this->_eol = $this->_eol;
                 break;
         }
     }
@@ -124,17 +124,17 @@ class Setup_Models_Config
      */
     private function _getIntroduction()
     {
-        $content  = '; The semicolons \';\' are used preceding a comment line, or a line which has data' . $this->_Eol;
-        $content .= '; that is not being used.' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; This file is divided into sections,' . $this->_Eol;
-        $content .= '; Each one of them corresponds to one' . $this->_Eol;
-        $content .= '; environment, it is used only one at a time, depending on what is speficied in' . $this->_Eol;
-        $content .= '; index.php, inside folder \'htdocs\' in the line that has:' . $this->_Eol;
-        $content .= '; define(\'PHPR_CONFIG_SECTION\', \'production\');' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; You could leave that line as it is, and in configuration.ini just modify the' . $this->_Eol;
-        $content .= '; parameters inside [production] section. You can also add your own sections.' . $this->_Eol;
+        $content  = '; The semicolons \';\' are used preceding a comment line, or a line which has data' . $this->_eol;
+        $content .= '; that is not being used.' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; This file is divided into sections,' . $this->_eol;
+        $content .= '; Each one of them corresponds to one' . $this->_eol;
+        $content .= '; environment, it is used only one at a time, depending on what is speficied in' . $this->_eol;
+        $content .= '; index.php, inside folder \'htdocs\' in the line that has:' . $this->_eol;
+        $content .= '; define(\'PHPR_CONFIG_SECTION\', \'production\');' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; You could leave that line as it is, and in configuration.ini just modify the' . $this->_eol;
+        $content .= '; parameters inside [production] section. You can also add your own sections.' . $this->_eol;
 
         return $content;
     }
@@ -148,16 +148,16 @@ class Setup_Models_Config
      */
     private function _getLanguage($language = 'en')
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;;;;;' . $this->_Eol;
-        $content .= '; LANGUAGE ;' . $this->_Eol;
-        $content .= ';;;;;;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;;;;;' . $this->_eol;
+        $content .= '; LANGUAGE ;' . $this->_eol;
+        $content .= ';;;;;;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Here it is specified the default language for the system, could be "de" for' . $this->_Eol;
-        $content .= '; German, "en" for English or "es" for Spanish. Actually, the language for each' . $this->_Eol;
-        $content .= '; user is specified individually from Administration -> User' . $this->_Eol;
-        $content .= 'language = "' . $language . '"' . $this->_Eol;
+        $content .= '; Here it is specified the default language for the system, could be "de" for' . $this->_eol;
+        $content .= '; German, "en" for English or "es" for Spanish. Actually, the language for each' . $this->_eol;
+        $content .= '; user is specified individually from Administration -> User' . $this->_eol;
+        $content .= 'language = "' . $language . '"' . $this->_eol;
 
         return $content;
     }
@@ -169,19 +169,19 @@ class Setup_Models_Config
      */
     private function _getPaths()
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;;' . $this->_Eol;
-        $content .= '; PATHS ;' . $this->_Eol;
-        $content .= ';;;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;;' . $this->_eol;
+        $content .= '; PATHS ;' . $this->_eol;
+        $content .= ';;;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Where the site and the main file (index.php) are located (htdocs folder).' . $this->_Eol;
+        $content .= '; Where the site and the main file (index.php) are located (htdocs folder).' . $this->_eol;
         $webPath = "http://" . $_SERVER['HTTP_HOST'] . ereg_replace('setup.php', '', $_SERVER['SCRIPT_NAME']);
-        $content .= 'webpath = "' . $webPath . '"' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content .= 'webpath = "' . $webPath . '"' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Path where will be placed files uploaded by the user.' . $this->_Eol;
-        $content .= 'uploadpath = "' . $this->_baseDir . 'upload\"' . $this->_Eol;
+        $content .= '; Path where will be placed files uploaded by the user.' . $this->_eol;
+        $content .= 'uploadpath = "' . $this->_baseDir . 'upload\"' . $this->_eol;
 
         return $content;
     }
@@ -200,25 +200,25 @@ class Setup_Models_Config
     private function _getDatabase($username = '', $password = '', $dbname = '', $adapter = 'Pdo_Mysql',
         $host = 'localhost')
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;;;;;' . $this->_Eol;
-        $content .= '; DATABASE ;' . $this->_Eol;
-        $content .= ';;;;;;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;;;;;' . $this->_eol;
+        $content .= '; DATABASE ;' . $this->_eol;
+        $content .= ';;;;;;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; For this Developer Release, it just has been tested with pdo_mysql.' . $this->_Eol;
-        $content .= 'database.adapter = "' . $adapter . '"' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; The assigned name or IP address for the database server.' . $this->_Eol;
-        $content .= 'database.params.host = "' . $host . '"' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; Username and password with the appropriate rights for Phprojekt to access to' . $this->_Eol;
-        $content .= '; the database.' . $this->_Eol;
-        $content .= 'database.params.username = "' . $username . '"' . $this->_Eol;
-        $content .= 'database.params.password = "' . $password . '"' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; Name of the database, inside the server' . $this->_Eol;
-        $content .= 'database.params.dbname = "' . $dbname . '"' . $this->_Eol;
+        $content .= '; For this Developer Release, it just has been tested with pdo_mysql.' . $this->_eol;
+        $content .= 'database.adapter = "' . $adapter . '"' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; The assigned name or IP address for the database server.' . $this->_eol;
+        $content .= 'database.params.host = "' . $host . '"' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; Username and password with the appropriate rights for Phprojekt to access to' . $this->_eol;
+        $content .= '; the database.' . $this->_eol;
+        $content .= 'database.params.username = "' . $username . '"' . $this->_eol;
+        $content .= 'database.params.password = "' . $password . '"' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; Name of the database, inside the server' . $this->_eol;
+        $content .= 'database.params.dbname = "' . $dbname . '"' . $this->_eol;
 
         return $content;
     }
@@ -230,23 +230,23 @@ class Setup_Models_Config
      */
     private function _getLogs()
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;' . $this->_Eol;
-        $content .= '; LOG ;' . $this->_Eol;
-        $content .= ';;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;' . $this->_eol;
+        $content .= '; LOG ;' . $this->_eol;
+        $content .= ';;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Here will be logged things explicitly declared.' . $this->_Eol;
-        $content .= '; E.G.: (PHP) Phprojekt::getInstance()->getLog()->debug("String to be logged");' . $this->_Eol;
-        $content .= 'log.debug.filename = "' . $this->_baseDir . 'logs\debug.log"' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content .= '; Here will be logged things explicitly declared.' . $this->_eol;
+        $content .= '; E.G.: (PHP) Phprojekt::getInstance()->getLog()->debug("String to be logged");' . $this->_eol;
+        $content .= 'log.debug.filename = "' . $this->_baseDir . 'logs\debug.log"' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= $this->_Eol;
-        $content .= '; This is another type of logging.' . $this->_Eol;
-        $content .= '; E.G.: (PHP) Phprojekt::getInstance()->getLog()->crit("String to be logged");' . $this->_Eol;
-        $content .= '; Note for developers: there are many different type of logs defined that can be' . $this->_Eol;
-        $content .= '; added here, see the complete list in phprojekt\library\Phprojekt\Log.php' . $this->_Eol;
-        $content .= 'log.crit.filename = "' . $this->_baseDir . 'logs\crit.log"' . $this->_Eol;
+        $content .= $this->_eol;
+        $content .= '; This is another type of logging.' . $this->_eol;
+        $content .= '; E.G.: (PHP) Phprojekt::getInstance()->getLog()->crit("String to be logged");' . $this->_eol;
+        $content .= '; Note for developers: there are many different type of logs defined that can be' . $this->_eol;
+        $content .= '; added here, see the complete list in phprojekt\library\Phprojekt\Log.php' . $this->_eol;
+        $content .= 'log.crit.filename = "' . $this->_baseDir . 'logs\crit.log"' . $this->_eol;
 
         return $content;
     }
@@ -258,20 +258,20 @@ class Setup_Models_Config
      */
     private function _getModules($userDisplayFormat = 0, $itemsPerPage = 3)
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;;;;' . $this->_Eol;
-        $content .= '; MODULES ;' . $this->_Eol;
-        $content .= ';;;;;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;;;;' . $this->_eol;
+        $content .= '; MODULES ;' . $this->_eol;
+        $content .= ';;;;;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Not used at the moment, leave it as it is.' . $this->_Eol;
-        $content .= 'itemsPerPage = ' . (int) $itemsPerPage . $this->_Eol;
-        $content .= $this->_Eol;
+        $content .= '; Not used at the moment, leave it as it is.' . $this->_eol;
+        $content .= 'itemsPerPage = ' . (int) $itemsPerPage . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Users' . $this->_Eol;
-        $content .= '; How the users are displayed in the system' . $this->_Eol;
-        $content .= '; (0 = lastname, firstname  1 = username, lastname, firstname, 2 = username)' . $this->_Eol;
-        $content .= 'userDisplayFormat  = ' . (int) $userDisplayFormat . $this->_Eol;
+        $content .= '; Users' . $this->_eol;
+        $content .= '; How the users are displayed in the system' . $this->_eol;
+        $content .= '; (0 = lastname, firstname  1 = username, lastname, firstname, 2 = username)' . $this->_eol;
+        $content .= 'userDisplayFormat  = ' . (int) $userDisplayFormat . $this->_eol;
 
         return $content;
     }
@@ -283,35 +283,35 @@ class Setup_Models_Config
      */
     private function _getMail($endOfLine = 0, $server = 'localhost', $user = '', $password = '')
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;;;;;;;;;;;;;;' . $this->_Eol;
-        $content .= '; MAIL NOTIFICATION ;' . $this->_Eol;
-        $content .= ';;;;;;;;;;;;;;;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;;;;;;;;;;;;;;' . $this->_eol;
+        $content .= '; MAIL NOTIFICATION ;' . $this->_eol;
+        $content .= ';;;;;;;;;;;;;;;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Inside many modules, when adding or editing an item, there is a tab' . $this->_Eol;
-        $content .= '; "Notification" that allows the user to send an email notification to the' . $this->_Eol;
-        $content .= '; people involved in that item, telling them about the creation or modification' . $this->_Eol;
-        $content .= '; of it.' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; If the email is configured to be sent in Text mode, whether to use \r\n or \n' . $this->_Eol;
-        $content .= '; for the end of line.' . $this->_Eol;
-        $content .= '; (0 = \r\n  1 = \n)' . $this->_Eol;
-        $content .= 'mailEndOfLine = ' . (int) $endOfLine . $this->_Eol;
-        $content .= $this->_Eol;
+        $content .= '; Inside many modules, when adding or editing an item, there is a tab' . $this->_eol;
+        $content .= '; "Notification" that allows the user to send an email notification to the' . $this->_eol;
+        $content .= '; people involved in that item, telling them about the creation or modification' . $this->_eol;
+        $content .= '; of it.' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; If the email is configured to be sent in Text mode, whether to use \r\n or \n' . $this->_eol;
+        $content .= '; for the end of line.' . $this->_eol;
+        $content .= '; (0 = \r\n  1 = \n)' . $this->_eol;
+        $content .= 'mailEndOfLine = ' . (int) $endOfLine . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Name or IP address of the SMTP server to be used to send that notifications.' . $this->_Eol;
-        $content .= 'smtpServer = "' . $server . '"' . $this->_Eol;
-        $content .= $this->_Eol;
-        $content .= '; If the SMTP server requires authentication, remove the semicolons \';\' and' . $this->_Eol;
-        $content .= '; write inside the inverted commas "" the appropriate username and password.' . $this->_Eol;
+        $content .= '; Name or IP address of the SMTP server to be used to send that notifications.' . $this->_eol;
+        $content .= 'smtpServer = "' . $server . '"' . $this->_eol;
+        $content .= $this->_eol;
+        $content .= '; If the SMTP server requires authentication, remove the semicolons \';\' and' . $this->_eol;
+        $content .= '; write inside the inverted commas "" the appropriate username and password.' . $this->_eol;
 
         if (empty($user) && empty($password)) {
-            $content .= ';smtpUser     = "' . $user . '"' . $this->_Eol;
-            $content .= ';smtpPassword = "' . $password . '"' . $this->_Eol;
+            $content .= ';smtpUser     = "' . $user . '"' . $this->_eol;
+            $content .= ';smtpPassword = "' . $password . '"' . $this->_eol;
         } else {
-            $content .= 'smtpUser     = "' . $user . '"' . $this->_Eol;
-            $content .= 'smtpPassword = "' . $password . '"' . $this->_Eol;
+            $content .= 'smtpUser     = "' . $user . '"' . $this->_eol;
+            $content .= 'smtpPassword = "' . $password . '"' . $this->_eol;
         }
 
         return $content;
@@ -324,18 +324,18 @@ class Setup_Models_Config
      */
     private function _getMisc($compressedDojo = 'true', $useCacheForClasses = 'true')
     {
-        $content  = $this->_Eol;
-        $content .= ';;;;;;;;' . $this->_Eol;
-        $content .= '; MISC ;' . $this->_Eol;
-        $content .= ';;;;;;;;' . $this->_Eol;
-        $content .= $this->_Eol;
+        $content  = $this->_eol;
+        $content .= ';;;;;;;;' . $this->_eol;
+        $content .= '; MISC ;' . $this->_eol;
+        $content .= ';;;;;;;;' . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Use compressed dojo to improve the speed of loading.' . $this->_Eol;
-        $content .= 'compressedDojo = ' . $compressedDojo . $this->_Eol;
-        $content .= $this->_Eol;
+        $content .= '; Use compressed dojo to improve the speed of loading.' . $this->_eol;
+        $content .= 'compressedDojo = ' . $compressedDojo . $this->_eol;
+        $content .= $this->_eol;
 
-        $content .= '; Use Zend_Registry for cache classes in the same request' . $this->_Eol;
-        $content .= 'useCacheForClasses = ' . $useCacheForClasses . $this->_Eol;
+        $content .= '; Use Zend_Registry for cache classes in the same request' . $this->_eol;
+        $content .= 'useCacheForClasses = ' . $useCacheForClasses . $this->_eol;
 
         return $content;
     }
