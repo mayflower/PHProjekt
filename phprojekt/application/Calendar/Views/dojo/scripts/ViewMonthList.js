0ff4e316da62e0d2852967c6495efb4103c6ab40
Mariano La Penna <mariano.lapenna@mayflower.de>
2009-06-18 18:02:18 +0000

    Fixed bug 'Calendar - Month view chart'

--- phprojekt/application/Calendar/Views/dojo/scripts/ViewMonthList.js
+++ phprojekt/application/Calendar/Views/dojo/scripts/ViewMonthList.js
@@ -25,7 +25,7 @@ dojo.declare("phpr.Calendar.ViewMonthList", phpr.Calendar.DefaultView, {
     // description:
     //    This Class takes care of displaying the list information we receive from our Server in a HTML table
     _header:              Array(7),
-    _schedule:            Array(),
+    _schedule:            null,
     _scrollLastDirection: 0,
 
     COLOR_WEEKDAY:      '#FFFFFF',
@@ -103,6 +103,7 @@ dojo.declare("phpr.Calendar.ViewMonthList", phpr.Calendar.DefaultView, {
         // It includes not only the days of this month but the necessary days of the previous and next month in
         // order to fill 4 or 6 week rows, from Monday to Sunday.
 
+        this._schedule  = new Array();
         var dateTemp    = this.stringToDate();
         var daysInMonth = dojo.date.getDaysInMonth(dateTemp);
         dateTemp.setDate(1);
