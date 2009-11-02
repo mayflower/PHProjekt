b7615d91796067641ea70b187a8cebeae337bc5f
Mariano La Penna <mariano.lapenna@mayflower.de>
2009-11-02 19:46:55 +0000

    Calendar views Fixed bug previous to 'Drag & Drop & Resize' improvement: if previous to this commit you enter grid or week view, open an item in the form, make changes IN THE VIEW, save them, then make changes IN THE VIEW again and Save them, phpr.DataStore error was thrown in Firebug.

--- phprojekt/application/Calendar/Views/dojo/scripts/Main.js
+++ phprojekt/application/Calendar/Views/dojo/scripts/Main.js
@@ -668,7 +668,7 @@ dojo.declare("phpr.Calendar.Main", phpr.Default.Main, {
         }
 
         if (this.form) {
-            this.form.updateData();
+            this.form = null;
         }
     },
 
