13c8d03b1a80a00985914fe232711055caf35a5a
Gustavo Solt <gustavo.solt@mayflower.de>
2010-04-29 11:50:10 -0100

    Improve md5 for uploaded files.
    Signed-off-by: Gustavo Solt <gustavo.solt@mayflower.de>

--- phprojekt/application/Default/Controllers/FileController.php
+++ phprojekt/application/Default/Controllers/FileController.php
@@ -118,7 +118,7 @@ class FileController extends IndexController
         }
         // Fix name for save it as md5
         if (is_array($_FILES) && !empty($_FILES) && isset($_FILES['uploadedFile'])) {
-            $md5name                        = md5(uniqid(rand(), 1));
+            $md5name                        = md5(mt_srand());
             $addedValue                     = $md5name . '|' . $_FILES['uploadedFile']['name'];
             $_FILES['uploadedFile']['name'] = $md5name;
         }
