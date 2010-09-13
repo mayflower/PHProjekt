a3c59997e54c598fd64397331f8ade37e3df679a
David Soria Parra <david.soria_parra@mayflower.de>
2010-09-13 16:39:12 +0200

    PHProjekt 6.0.4
    Codename Melecta
    
    Signed-off-by: David Soria Parra <david.soria_parra@mayflower.de>

--- phprojekt/library/Phprojekt.php
+++ phprojekt/library/Phprojekt.php
@@ -65,7 +65,7 @@ class Phprojekt
     /**
      * Copyright.
      */
-    const COPYRIGHT = 'PHProjekt 6.0 - Copyright (c) 2010 Mayflower GmbH';
+    const COPYRIGHT = 'PHProjekt 6.0.4 - Copyright (c) 2010 Mayflower GmbH';
 
     /**
      * Default Max size in bytes that is allowed to be uploaded per file.
