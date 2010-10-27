fdf2b61322eadf190a89dfd58c5815b13484703d
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2010-10-27 17:24:33 +0200

    Copied Calendar2 Views from old Calendar module

---
+++ phprojekt/application/Calendar2/Views/dojo/scripts/Grid.js
@@ -0,0 +1,25 @@
+/**
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Calendar2
+ * @copyright  Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1
+ * @version    Release: @package_version@
+ * @author     Gustavo Solt <solt@mayflower.de>
+ */
+
+dojo.provide("phpr.Calendar2.Grid");
+
+dojo.declare("phpr.Calendar2.Grid", phpr.Default.Grid, {
+});
