003a0bf135632fd5d6c4598519e5d065561f92e7
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2011-01-30 20:49:35 +0100

    Added an upgrade controller to the core module.
    Currently, it only shows a button 'upgrade'.
    It's long-term aim is to inform the admin which modules need upgrading, show
    nice progress bars when doing so etc.

---
+++ phprojekt/application/Core/Controllers/UpgradeController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Upgrade Controller.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Core
+ * @copyright  Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1.0
+ * @version    Release: @package_version@
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+
+/**
+ * Upgrade Controller.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Core
+ * @copyright  Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1.0
+ * @version    Release: @package_version@
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+class Core_UpgradeController extends Core_IndexController
+{
+    public function indexAction() {
+        // The Core_IndexController has assured that the user has administrative
+        // rights at this point.
+        // TODO: This is actually bad, because the user will then be redirected
+        //       back here again. We have to show the user a 'tell your admin'
+        //       page instead.
+        $config = Phprojekt::getInstance()->getConfig();
+        $language = Phprojekt_User_User::getSetting(
+            "language",
+            $config->language
+        );
+
+        $this->view->webPath        = $config->webpath;
+        $this->view->language       = $language;
+        $this->view->compressedDojo = (bool) $config->compressedDojo;
+        $this->view->frontendMsg    = (bool) $config->frontendMessages;
+        $this->view->newVersion     = Phprojekt::getVersion();
+
+        $this->render('upgrade');
+    }
+
+    public function upgradeAction() {
+        // TODO: Quadruple-check that we are allowed to do this thing.
+        $extensions = new Phprojekt_Extensions(PHPR_CORE_PATH);
+        $migration  = new Phprojekt_Migration($extensions);
+
+        $migration->performUpgrade();
+
+        // TODO: Notify the user that we're happy
+        $config = Phprojekt::getInstance()->getConfig();
+        $this->_redirect($config->webpath . '/index.php');
+    }
+}
