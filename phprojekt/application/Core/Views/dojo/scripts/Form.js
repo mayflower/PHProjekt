39aa899ff38f3d1f4663db2bed3e900c6544f9f3
Gustavo Solt <gustavo.solt@mayflower.de>
2011-02-21 19:18:03 -0300

    Apply changes to the default Core module.
    - Guidelines:
     - All private functions and variables have an '_' before the name.
     - Sort code:
      - Sorty by public, then private.
      - Sort by use for functions and name for variables.
    - General:
     - Clean code and documentation.
     - Remove templates, use code.
    
    Signed-off-by: Gustavo Solt <gustavo.solt@mayflower.de>

--- phprojekt/application/Core/Views/dojo/scripts/Form.js
+++ phprojekt/application/Core/Views/dojo/scripts/Form.js
@@ -16,109 +16,134 @@
  * @link       http://www.phprojekt.com
  * @since      File available since Release 6.0
  * @version    Release: @package_version@
- * @author     Gustavo Solt <solt@mayflower.de>
+ * @author     Gustavo Solt <gustavo.solt@mayflower.de>
  */
 
 dojo.provide("phpr.Core.Form");
 
 dojo.declare("phpr.Core.Form", phpr.Default.Form, {
-    setUrl:function() {
+    _isSystemModule: null,
+
+    init:function(id, params, isSystemModule) {
         // Summary:
-        //    Rewritten the function for work like a system module and like a form
-        // Description:
-        //    Rewritten the function for work like a system module and like a form
-        if (this.main.isSystemModule(this.main.module)) {
-            this._url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDetail/nodeId/1/id/'
-                + this.id;
-        } else {
-            this._url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDetail/nodeId/1/'
-                + 'moduleName/' + phpr.submodule;
-        }
-    },
+        //    Init the form for a new render.
+        this._isSystemModule = isSystemModule;
 
-    initData:function() {
-    },
-
-    setPermissions:function(data) {
-        this._writePermissions  = true;
-        this._deletePermissions = false;
-        if (this.id > 0) {
-            this._deletePermissions = true;
-        }
-        this._accessPermissions = true;
-    },
-
-    addBasicFields:function() {
-    },
-
-    addModuleTabs:function(data) {
-    },
-
-    useCache:function() {
-        if (this.main.isSystemModule(this.main.module)) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-
-    submitForm:function() {
-        // Summary:
-        //    Rewritten the function for work like a system module and like a form
-        // Description:
-        //    Rewritten the function for work like a system module and like a form
-        if (!this.prepareSubmission()) {
-            return false;
-        }
-        if (this.main.isSystemModule(this.main.module)) {
-            var url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonSave/nodeId/1/id/' + this.id;
-        } else {
-            var url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonSave/nodeId/1/moduleName/'
-                + phpr.submodule;
-        }
-        phpr.send({
-            url:       url,
-            content:   this.sendData,
-            onSuccess: dojo.hitch(this, function(data) {
-                new phpr.handleResponse('serverFeedback', data);
-                if (data.type == 'success') {
-                    this.customActionOnSuccess();
-                    this.publish("updateCacheData");
-                    if (this.main.isSystemModule(this.main.module)) {
-                        this.publish("setUrlHash", [phpr.parentmodule, null, [phpr.module]]);
-                    } else {
-                        this.publish("setUrlHash", [phpr.parentmodule]);
-                    }
-                }
-            })
-        });
-    },
-
-    customActionOnSuccess:function() {
-        // Summary:
-        //    Function for be rewritten
-        // Description:
-        //    Function for be rewritten
-    },
-
-    deleteForm:function() {
-        phpr.send({
-            url:       phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDelete/id/' + this.id,
-            onSuccess: dojo.hitch(this, function(data) {
-                new phpr.handleResponse('serverFeedback', data);
-                if (data.type == 'success') {
-                    this.publish("updateCacheData");
-                    this.publish("setUrlHash", [phpr.parentmodule, null, [phpr.module]]);
-                }
-            })
-        });
+        this.inherited(arguments);
     },
 
     updateData:function() {
         phpr.DataStore.deleteData({url: this._url});
     },
 
-    setBreadCrumbItem:function(itemValue) {
+    /************* Private functions *************/
+
+    _setUrl:function() {
+        // Summary:
+        //    Set the url for get the data.
+        // Description:
+        //    Rewritten the function for work like a system module and like a form.
+        if (this._isSystemModule) {
+            this._url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDetail/nodeId/1/id/'
+                + this._id;
+        } else {
+            this._url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDetail/nodeId/1/'
+                + 'moduleName/' + phpr.submodule;
+        }
+    },
+
+    _initData:function() {
+        // Summary:
+        //    Init all the data before draw the form.
+    },
+
+    _setPermissions:function(data) {
+        // Summary:
+        //    Get the permission for the current user on the item.
+        this._writePermissions  = true;
+        this._deletePermissions = false;
+        if (this._id > 0) {
+            this._deletePermissions = true;
+        }
+        this._accessPermissions = true;
+    },
+
+    _setBreadCrumbItem:function(itemValue) {
+        // Summary:
+        //    Set the Breadcrumb with the first item value.
         phpr.BreadCrumb.setItem(itemValue);
+    },
+
+    _addBasicFields:function() {
+        // Summary:
+        //    Add some special fields.
+    },
+
+    _addModuleTabs:function(data) {
+        // Summary:
+        //    Add extra tabs.
+    },
+
+    _useCache:function() {
+        // Summary:
+        //    Return true or false if the cache is used.
+        if (this._isSystemModule) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    _submitForm:function() {
+        // Summary:
+        //    Submit the forms.
+        // Description:
+        //    Rewritten the function for work like a system module and like a form
+        if (!this._prepareSubmission()) {
+            return false;
+        }
+        if (this._isSystemModule) {
+            var url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonSave/nodeId/1/id/'
+                + this._id;
+        } else {
+            var url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonSave/nodeId/1/moduleName/'
+                + phpr.submodule;
+        }
+        phpr.send({
+            url:       url,
+            content:   this._sendData,
+            onSuccess: dojo.hitch(this, function(data) {
+                new phpr.handleResponse('serverFeedback', data);
+                if (data.type == 'success') {
+                    this._customActionOnSuccess();
+                    dojo.publish(phpr.module + '.updateCacheData');
+                    if (this._isSystemModule) {
+                        dojo.publish(phpr.module + '.setUrlHash', [phpr.parentmodule, null, [phpr.module]]);
+                    } else {
+                        dojo.publish(phpr.module + '.setUrlHash', [phpr.parentmodule]);
+                    }
+                }
+            })
+        });
+    },
+
+    _customActionOnSuccess:function() {
+        // Summary:
+        //    Function for be rewritten.
+    },
+
+    _deleteForm:function() {
+        // Summary:
+        //    Delete an item.
+        phpr.send({
+            url:       phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonDelete/id/' + this._id,
+            onSuccess: dojo.hitch(this, function(data) {
+                new phpr.handleResponse('serverFeedback', data);
+                if (data.type == 'success') {
+                    dojo.publish(phpr.module + '.updateCacheData');
+                    dojo.publish(phpr.module + '.setUrlHash', [phpr.parentmodule, null, [phpr.module]]);
+                }
+            })
+        });
     }
 });
--- phprojekt/application/Core/Views/dojo/scripts/Grid.js
+++ phprojekt/application/Core/Views/dojo/scripts/Grid.js
@@ -16,41 +16,61 @@
  * @link       http://www.phprojekt.com
  * @since      File available since Release 6.0
  * @version    Release: @package_version@
- * @author     Gustavo Solt <solt@mayflower.de>
+ * @author     Gustavo Solt <gustavo.solt@mayflower.de>
  */
 
 dojo.provide("phpr.Core.Grid");
 
 dojo.declare("phpr.Core.Grid", phpr.Default.Grid, {
-    setUrl:function() {
-        this.url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonList/nodeId/1';
+    _setGetExtraActionsUrl:function() {
+        // Summary:
+        //    Sets the url where to get the grid actions data from.
+        this._getActionsUrl = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonGetExtraActions';
     },
 
-    setGetExtraActionsUrl:function() {
-        this.getActionsUrl = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonGetExtraActions';
+    _setUpdateUrl:function() {
+        // Summary:
+        //    Sets the url for save the changes.
+        this._updateUrl = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonSaveMultiple/nodeId/1';
     },
 
-    useCheckbox:function() {
+    _setUrl:function() {
+        // Summary:
+        //    Set the url for getting the data.
+        this._url = phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/jsonList/nodeId/1';
+    },
+
+    _useCheckbox:function() {
+        // Summary:
+        //    Whether to show or not the checkbox in the grid list.
         return false;
     },
 
-    getLinkForEdit:function(id) {
-        this.main.setUrlHash(phpr.parentmodule, id, [phpr.module]);
+    _setExportButton:function(meta) {
+        // Summary:
+        //    If there is any row, render an export Button.
     },
 
-    canEdit:function(inRowIndex) {
-        return true;
+    _showTags:function() {
+        // Summary:
+        //    Draw the tags.
     },
 
-    showTags:function() {
-    },
-
-    setExportButton:function(meta) {
-    },
-
-    getDoActionUrl:function(action, idUrl, ids) {
+    _getDoActionUrl:function(action, idUrl, ids) {
         // Summary:
         //    Custom getDoActionUrl for Core
         return phpr.webpath + 'index.php/Core/' + phpr.module.toLowerCase() + '/' + action + '/' + idUrl + '/' + ids;
+    },
+
+    _getLinkForEdit:function(id) {
+        // Summary:
+        //    Return the link for open the form.
+        dojo.publish(phpr.module + '.setUrlHash', [phpr.parentmodule, id, [phpr.module]]);
+    },
+
+    _canEdit:function(inRowIndex) {
+        // Summary:
+        //    Check the user access on the item.
+        return true;
     }
 });
