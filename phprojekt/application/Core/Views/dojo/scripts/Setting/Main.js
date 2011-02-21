475dfd9ab4cc097f63aea91cbb5c3b6d8e46de1a
Gustavo Solt <gustavo.solt@mayflower.de>
2011-02-21 19:18:20 -0300

    Apply changes to the Setting module.
    - Guidelines:
     - All private functions and variables have an '_' before the name.
     - Sort code:
      - Sorty by public, then private.
      - Sort by use for functions and name for variables.
    - General:
     - Clean code and documentation.
     - Remove templates, use code.
    
    Signed-off-by: Gustavo Solt <gustavo.solt@mayflower.de>

--- phprojekt/application/Core/Views/dojo/scripts/Setting/Main.js
+++ phprojekt/application/Core/Views/dojo/scripts/Setting/Main.js
@@ -16,42 +16,56 @@
  * @link       http://www.phprojekt.com
  * @since      File available since Release 6.0
  * @version    Release: @package_version@
- * @author     Gustavo Solt <solt@mayflower.de>
+ * @author     Gustavo Solt <gustavo.solt@mayflower.de>
  */
 
 dojo.provide("phpr.Setting.Main");
 
 dojo.declare("phpr.Setting.Main", phpr.Core.Main, {
     constructor:function() {
-        this.module = "Setting";
-        this.loadFunctions(this.module);
+        // Summary:
+        //    Create a new instance of the module.
+        this._module = 'Setting';
 
-        this.gridWidget = phpr.Setting.Grid;
-        this.formWidget = phpr.Setting.Form;
+        this._loadFunctions();
+
+        this._gridWidget = phpr.Setting.Grid;
+        this._formWidget = phpr.Setting.Form;
     },
 
-    getSummary:function() {
+    processActionFromUrlHash:function(data) {
+        // Summary:
+        //     Check the action params and run the correct function.
+        //     Reload is the default, but each function can redefine it.
+        if (data[0]) {
+            dojo.publish(this._module + '.reload', [data[0]]);
+        }
+    },
+
+    /************* Private functions *************/
+
+    _getSystemModules:function() {
+        // Summary:
+        //    Return the modules that are like normal modules instead of just a form.
+        return new Array();
+    },
+
+    _defineModules:function(module) {
+        // Summary:
+        //    Set the global vars for this module.
+        phpr.module       = this._module;
+        phpr.submodule    = module;
+        phpr.parentmodule = 'Setting';
+    },
+
+    _getSummary:function() {
+        // Summary:
+        //    Returns a text info of the Parent module.
         return '<b>' + phpr.nls.get('Setting') + '</b>'
             + '<br /><br />'
             + phpr.nls.get('This module is for the user to set and change specific configuration parameters of '
             + 'his/her profile.')
             + '<br /><br />'
             + phpr.nls.get('Please choose one of the tabs of above.');
-    },
-
-    getSystemModules:function() {
-        return new Array();
-    },
-
-    defineModules:function(module) {
-        phpr.module       = this.module;
-        phpr.submodule    = module;
-        phpr.parentmodule = 'Setting';
-    },
-
-    processActionFromUrlHash:function(data) {
-        if (data[0]) {
-            dojo.publish(this.module + ".reload", [data[0]]);
-        }
     }
 });
