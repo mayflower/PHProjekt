37748e6cf020f0d3059aa5102152b8e04af8fa16
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2011-03-04 21:17:05 +0100

    Added translation for cumulative completion

--- phprojekt/application/Project/Languages/de.inc.php
+++ phprojekt/application/Project/Languages/de.inc.php
@@ -18,7 +18,7 @@ $lang["Ended"] = "Beendet";
 $lang["Stopped"] = "Angehalten";
 $lang["Re-Opened"] = "Neu eröffnete";
 $lang["Waiting"] = "Waiting";
-$lang["Complete percent"] = "Complete Prozent";
+$lang["Complete percent"] = "Fertigstellung";
 $lang["Budget"] = "Budget";
 $lang["Hourly wage rate"] = "Stundensatz";
 $lang["Contact"] = "Kontakt";
@@ -202,3 +202,4 @@ $lang["Content Help"]["Role"] = "<br />
 
 $lang["Content Help"]["Notification"] = "DEFAULT";
 $lang["Content Help"]["History"] = "DEFAULT";
+$lang["Cumulative complete percent"] = "Kumulative Fertigstellung";
