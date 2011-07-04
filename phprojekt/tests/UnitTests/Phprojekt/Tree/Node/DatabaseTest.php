bd00686d5be942e1b55257b18043d79b428ed54f
David Soria Parra <david.soria_parra@mayflower.de>
2011-07-04 13:35:49 +0200

    Make sure the sub project dates are in the parent project time interval

--- phprojekt/tests/UnitTests/Phprojekt/Tree/Node/DatabaseTest.php
+++ phprojekt/tests/UnitTests/Phprojekt/Tree/Node/DatabaseTest.php
@@ -99,7 +99,7 @@ class Phprojekt_Tree_Node_DatabaseTest extends DatabaseTest
         $this->assertEquals('....Test Project', $node->getDepthDisplay('title'));
         $node = $this->_tree->getNodeById(5);
         $this->assertEquals('........Sub Project', $node->getDepthDisplay('title'));
-        $this->assertEquals('........1987-03-22', $node->getDepthDisplay('startDate'));
+        $this->assertEquals('........2009-06-02', $node->getDepthDisplay('startDate'));
     }
 
     /**
