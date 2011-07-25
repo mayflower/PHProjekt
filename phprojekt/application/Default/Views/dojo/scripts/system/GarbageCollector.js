1e16c9f708ab737358833e7d20c168e1234af9a7
Reno Reckling <e-github@wthack.de>
2011-07-25 17:21:13 +0200

    fix checkstyle errors in GarbageCollector.js
    i fixed all checkstyle errors

--- phprojekt/application/Default/Views/dojo/scripts/system/GarbageCollector.js
+++ phprojekt/application/Default/Views/dojo/scripts/system/GarbageCollector.js
@@ -29,51 +29,51 @@ dojo.declare("phpr.Default.System.GarbageCollector", null, {
     _eventHandler: {},
 
     constructor: function() {
-        this._domNodes = new Object();
-        this._eventHandler = new Object();
+        this._domNodes = {};
+        this._eventHandler = {};
     },
 
-    destroy:function() {
-        for(var i in this._eventHandler) {
-            for(var e in this._eventHandler[i]) {
+    destroy: function() {
+        for (var i in this._eventHandler) {
+            for (var e in this._eventHandler[i]) {
                 this._eventHandler[i][e] = null;
             }
             this._eventHandler[i] = null;
         }
 
-        for(var i in this._domNodes) {
-            for(var e in this._domNodes[i]) {
+        for (var i in this._domNodes) {
+            for (var e in this._domNodes[i]) {
                 this._domNodes[i][e] = null;
             }
             this._domNodes[i] = null;
         }
     },
 
-    addNode:function(node,context) {
+    addNode: function(node, context) {
         // Summary:
         //      Adds a domNode to the garbage collection watch
         // Description:
         //      This adds a domNode or dijit widget to the GC
         //      To provide more flexibility, you can optionally provide a
         //      context.
-        if(!this._domNodes[context]) {
-            this._domNodes[context] = new Array();
+        if (!this._domNodes[context]) {
+            this._domNodes[context] = [];
         }
         this._domNodes[context].push(node);
     },
-    addEvent:function(handler,context) {
+    addEvent: function(handler, context) {
         // Summary:
         //      Adds a event to the garbage collection watch
         // Description:
         //      This adds a event to the GC
         //      To provide more flexibility, you can optionally provide a
         //      context.
-        if(!this._eventHandler[context]) {
-            this._eventHandler[context] = new Array();
+        if (!this._eventHandler[context]) {
+            this._eventHandler[context] = [];
         }
         this._eventHandler[context].push(handler);
     },
-    collect:function(context) {
+    collect: function(context) {
         // Summary:
         //      Collect all registered events and nodes from the given scope
         // Description:
@@ -81,18 +81,18 @@ dojo.declare("phpr.Default.System.GarbageCollector", null, {
         //      registered dijit widgets and dom node recursively within the
         //      given context.
 
-        if(this._eventHandler[context] && dojo.isArray(this._eventHandler[context])) {
-            while(this._eventHandler[context].length > 0) {
-                if(this._eventHandler[context][0]) {
+        if (this._eventHandler[context] && dojo.isArray(this._eventHandler[context])) {
+            while (this._eventHandler[context].length > 0) {
+                if (this._eventHandler[context][0]) {
                     dojo.disconnect(this._eventHandler[context][0]);
                 }
                 this._eventHandler[context][0] = null;
-                this._eventHandler[context].splice(0,1);
+                this._eventHandler[context].splice(0, 1);
             }
         }
 
-        if(this._domNodes[context] && dojo.isArray(this._domNodes[context])) {
-            while(this._domNodes[context].length > 0) {
+        if (this._domNodes[context] && dojo.isArray(this._domNodes[context])) {
+            while (this._domNodes[context].length > 0) {
                 var n = this._domNodes[context][0];
 
                 if (dijit.byId(n) && dijit.byId(n).destroyRecursive) { // dijit widget?
@@ -109,9 +109,10 @@ dojo.declare("phpr.Default.System.GarbageCollector", null, {
                             widget.destroy();
                         }
                     } catch (e) {
-                        dojo.forEach(dijit.findWidgets(dojo.byId(n)), function(w) {
-                            w.destroyRecursive();
-                        });
+                        dojo.forEach(dijit.findWidgets(dojo.byId(n)),
+                            function(w) {
+                                w.destroyRecursive();
+                            });
                         dojo.destroy(dojo.byId(n));
                     }
                 }
@@ -126,7 +127,7 @@ dojo.declare("phpr.Default.System.GarbageCollector", null, {
 dojo.declare("phpr.Default.System.GarbageCollected", null, {
     garbageCollector: null,
 
-    constructor:function() {
+    constructor: function() {
         this.garbageCollector = new phpr.Default.System.GarbageCollector();
     },
 
