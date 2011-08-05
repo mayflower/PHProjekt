4b477e261dbbd92aae369ffd485eb9191b827aa3
Reno Reckling <e-github@wthack.de>
2011-08-05 15:34:53 +0200

    use fillTemplate inside the this.render in component.js
    i am trying to replace this.render(), part of this path is providing a function
    to render templates into a string.
    this function can also be used by this.render() for the time beeing

--- phprojekt/application/Default/Views/dojo/scripts/system/Component.js
+++ phprojekt/application/Default/Views/dojo/scripts/system/Component.js
@@ -21,7 +21,6 @@
 
 dojo.provide("phpr.Default.System.Component");
 
-dojo.require("dojox.dtl.Inline");
 dojo.require("dojo.NodeList-traverse");
 dojo.require("phpr.Default.System.GarbageCollector");
 
@@ -29,15 +28,7 @@ dojo.declare("phpr.Default.System.Component", phpr.Default.System.GarbageCollect
     main:   null,
     module: "",
     render:function(template, node, data) {
-
-        var context = new dojox.dtl.Context(data);
-        // Use the cached template
-        var tplContent = __phpr_templateCache[template[0] + "." + template[1]];
-        var tpl        = new dojox.dtl.Template(tplContent);
-        var content    = tpl.render(context);
-        tpl = null;
-        tplContent = null;
-        context = null;
+        content = phpr.fillTemplate(template[0] + "." + template[1], data);
 
         // [a-zA-Z1-9[]:|]
         var eregId = /id=\\?["'][\w\x5b\x5d\x3a\x7c]*\\?["']/gi;
