6c900254a0ec8508befcac72eb05d7dc04339b4f
Reno Reckling <e-github@wthack.de>
2011-08-06 01:54:42 +0200

    fix phpr.Store.User name
    the class has been renamed to phpr.Default.System.Store.User

--- phprojekt/application/Calendar2/Views/dojo/scripts/Form.js
+++ phprojekt/application/Calendar2/Views/dojo/scripts/Form.js
@@ -38,7 +38,7 @@ dojo.declare("phpr.Calendar2.Form", phpr.Default.Form, {
 
     initData:function() {
         // Get all the active users
-        this.userStore = new phpr.Store.User();
+        this.userStore = new phpr.Default.System.Store.User();
         this._initData.push({'store': this.userStore});
 
         // Get the tags
