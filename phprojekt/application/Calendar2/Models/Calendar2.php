62d4e6233586eaf4845b37254b85d47519fa9501
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2011-09-17 03:27:46 +0200

    Added checks to prevent breaking on caldav read if fields are empty

--- phprojekt/application/Calendar2/Models/Calendar2.php
+++ phprojekt/application/Calendar2/Models/Calendar2.php
@@ -943,10 +943,14 @@ class Calendar2_Models_Calendar2 extends Phprojekt_Item_Abstract
     public function asVObject()
     {
         $vobject = new Sabre_VObject_Component('vevent');
-        $vobject->add('summary', $this->summary);
-        $vobject->add('description', $this->description);
-        $vobject->add('comment', $this->comments);
-        $vobject->add('location', $this->location);
+        if ($this->summary)
+            $vobject->add('summary', $this->summary);
+        if ($this->description)
+            $vobject->add('description', $this->description);
+        if ($this->comments)
+            $vobject->add('comment', $this->comments);
+        if ($this->location)
+            $vobject->add('location', $this->location);
         if ($this->recurrenceId) {
             $recurrenceId = new Datetime($this->recurrenceId);
             $vobject->add('recurrence-id', $recurrenceId->format('Ymd\THis\Z'));
