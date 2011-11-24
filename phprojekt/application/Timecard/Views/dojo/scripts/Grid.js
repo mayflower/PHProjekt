0e80432633717ea45488c28a49e83b4663f7f137
Reno Reckling <reno.reckling@mayflower.de>
2011-11-24 01:49:47 +0100

    checkstyle fixes timecard grid.js
    checkstyle fixes

--- phprojekt/application/Timecard/Views/dojo/scripts/Grid.js
+++ phprojekt/application/Timecard/Views/dojo/scripts/Grid.js
@@ -24,22 +24,22 @@ dojo.provide("phpr.Timecard.Grid");
 dojo.require("dijit.form.Button");
 
 dojo.declare("phpr.Timecard.Grid", phpr.Default.System.Component, {
-    main:          null,
-    _date:         null,
-    _node:         null,
-    _month:        null,
-    _year:         null,
+    main: null,
+    _date: null,
+    _node: null,
+    _month: null,
+    _year: null,
     _exportButton: null,
 
-    constructor:function(/*Object*/main, /*js Date object */date) {
+    constructor: function(/*Object*/main, /*js Date object */date) {
         // Summary:
         //    Render the list of dates in the month
         // Description:
         //    Render the list of dates in the month
-        this.main   = main;
-        this._date  = date;
+        this.main = main;
+        this._date = date;
         this._month = date.getMonth();
-        this._year  = date.getFullYear();
+        this._year = date.getFullYear();
 
         this.setUrl();
         this.setContainer();
@@ -51,16 +51,16 @@ dojo.declare("phpr.Timecard.Grid", phpr.Default.System.Component, {
         phpr.DataStore.requestData({url: this.url, processData: dojo.hitch(this, "onLoaded")});
     },
 
-    setUrl:function() {
+    setUrl: function() {
         // Summary:
         //    Set the url for getting the data
         // Description:
         //    Set the url for getting the data
-        this.url = phpr.webpath + 'index.php/' + phpr.module + '/index/jsonMonthList/year/' + this._year + '/month/'
-            + (this._month + 1);
+        this.url = phpr.webpath + 'index.php/' + phpr.module + '/index/jsonMonthList/year/' + this._year +
+            '/month/' + (this._month + 1);
     },
 
-    setContainer:function() {
+    setContainer: function() {
         // Summary:
         //    Set the node to put the grid
         // Description:
@@ -68,73 +68,73 @@ dojo.declare("phpr.Timecard.Grid", phpr.Default.System.Component, {
         this._node = dijit.byId("monthView");
     },
 
-    onLoaded:function() {
+    onLoaded: function() {
         // Summary:
         //    Render the list itself
         // Description:
         //    Render the list itself
         var content = phpr.DataStore.getData({url: this.url});
-        var total   = 0;
+        var total = 0;
 
-        var dates      = new Array();
+        var dates = [];
         var totalClass = 'weekday';
         for (var i in content) {
-            var weekClass = (content[i]['week'] == 0 || content[i]['week'] == 6) ? 'weekend' : 'weekday';
+            var weekClass = (content[i].week === 0 || content[i].week === 6) ? 'weekend' : 'weekday';
             dates.push({
-                week:      phpr.date.getShortTranslateWeekDay(content[i]['week']),
+                week: phpr.date.getShortTranslateWeekDay(content[i].week),
                 weekClass: weekClass,
-                date:      content[i]['date'],
-                sum:       (content[i]['sumInHours'] != '0') ? content[i]['sumInHours'] : "-",
-                sumClass:  (content[i]['openPeriod'] == 1) ? 'open' : weekClass
+                date: content[i].date,
+                sum: (content[i].sumInHours != '0') ? content[i].sumInHours : "-",
+                sumClass: (content[i].openPeriod == 1) ? 'open' : weekClass
             });
-            if (content[i]['sumInMinutes'] != '0') {
-                total += content[i]['sumInMinutes'];
+            if (content[i].sumInMinutes != '0') {
+                total += content[i].sumInMinutes;
             }
-            if (content[i]['openPeriod'] == 1) {
+            if (content[i].openPeriod == 1) {
                 totalClass = 'open';
             }
         }
 
         this.render(["phpr.Timecard.template", "monthView.html"], this._node.domNode, {
-            monthTxt:   phpr.date.getLongTranslateMonth(this._month) + ' ' + this._year,
-            totalTxt:   phpr.nls.get('Total hours'),
-            total:      phpr.date.convertMinutesToTime(total),
+            monthTxt: phpr.date.getLongTranslateMonth(this._month) + ' ' + this._year,
+            totalTxt: phpr.nls.get('Total hours'),
+            total: phpr.date.convertMinutesToTime(total),
             totalClass: totalClass,
-            dates:      dates
+            dates: dates
         });
         dijit.byId("selectDate").set('value', new Date(this._year, this._month, this._date.getDate()));
     },
 
-    reload:function(date, forceReload) {
+    reload: function(date, forceReload) {
         // Summary:
         //    Reload the list if some value change
         // Description:
         //    Reload the list if some value change or forceReload is true
         var newMonth = date.getMonth();
-        var newYear  = date.getFullYear();
+        var newYear = date.getFullYear();
         if (forceReload || newMonth != this._month || newYear != this._year) {
             phpr.DataStore.deleteData({url: this.url});
-            this._date  = date,
+            this._date = date;
             this._month = date.getMonth();
-            this._year  = date.getFullYear();
+            this._year = date.getFullYear();
             this.setUrl();
             phpr.DataStore.addStore({url: this.url});
             phpr.DataStore.requestData({url: this.url, processData: dojo.hitch(this, "onLoaded")});
         }
     },
 
-    setExportButton:function() {
+    setExportButton: function() {
         // Summary:
         //    Set the export button
         // Description:
         //    Set the export button
         if (this._exportButton === null) {
             var params = {
-                label:     phpr.nls.get('Export to CSV'),
+                label: phpr.nls.get('Export to CSV'),
                 showLabel: true,
                 baseClass: "positive",
                 iconClass: "export",
-                disabled:  false
+                disabled: false
             };
             this._exportButton = new dijit.form.Button(params);
             phpr.viewManager.getView().buttonRow.domNode.appendChild(this._exportButton.domNode);
@@ -142,13 +142,13 @@ dojo.declare("phpr.Timecard.Grid", phpr.Default.System.Component, {
         }
     },
 
-    exportData:function() {
+    exportData: function() {
         // summary:
         //    Open a new widnows in CSV mode
         // description:
         //    Export all the bookings of the month
-        window.open(phpr.webpath + 'index.php/' + phpr.module + '/index/csvList/nodeId/1/year/'
-            + this._year + '/month/' + (this._month + 1) + '/csrfToken/' + phpr.csrfToken);
+        window.open(phpr.webpath + 'index.php/' + phpr.module + '/index/csvList/nodeId/1/year/' + this._year +
+            '/month/' + (this._month + 1) + '/csrfToken/' + phpr.csrfToken);
         return false;
     }
 });
