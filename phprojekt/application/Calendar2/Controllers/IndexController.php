8626d28c6161caeb3d9ccef2cd376ef81d21981b
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-01-04 19:50:46 +0100

    Indentation fixes

--- phprojekt/application/Calendar2/Controllers/IndexController.php
+++ phprojekt/application/Calendar2/Controllers/IndexController.php
@@ -101,8 +101,8 @@ class Calendar2_IndexController extends IndexController
         $userId = $this->getRequest()->getParam('userId', Phprojekt_Auth_Proxy::getEffectiveUserId());
 
         if (!Cleaner::validate('int', $userId)) {
-           throw new Phprojekt_PublishedException(
-               "Invalid userId '$userId'"
+            throw new Phprojekt_PublishedException(
+                "Invalid userId '$userId'"
             );
         }
 
@@ -144,8 +144,8 @@ class Calendar2_IndexController extends IndexController
         }
 
         if (!Cleaner::validate('int', $userId)) {
-           throw new Phprojekt_PublishedException(
-               "Invalid userId '$userId'"
+            throw new Phprojekt_PublishedException(
+                "Invalid userId '$userId'"
             );
         }
 
@@ -316,8 +316,8 @@ class Calendar2_IndexController extends IndexController
         }
 
         if (!Cleaner::validate('int', $userId)) {
-           throw new Phprojekt_PublishedException(
-               "Invalid userId '$userId'"
+            throw new Phprojekt_PublishedException(
+                "Invalid userId '$userId'"
             );
         }
 
@@ -479,8 +479,8 @@ class Calendar2_IndexController extends IndexController
         $id = (int) $id;
 
         if (!Cleaner::validate('int', $userId)) {
-           throw new Phprojekt_PublishedException(
-               "Invalid userId '$userId'"
+            throw new Phprojekt_PublishedException(
+                "Invalid userId '$userId'"
             );
         }
 
@@ -828,16 +828,16 @@ class Calendar2_IndexController extends IndexController
                 || !Calendar2_Models_Calendar2::isValidVisibility(
                     (int) $visibility
                 )) {
-           throw new Phprojekt_PublishedException(
-               "Invalid visibility '$visibility'"
+            throw new Phprojekt_PublishedException(
+                "Invalid visibility '$visibility'"
             );
         }
 
         $visibility = (int) $visibility;
 
         if (!Cleaner::validate('int', $params['userId'])) {
-           throw new Phprojekt_PublishedException(
-               "Invalid userId " . $params['userId']
+            throw new Phprojekt_PublishedException(
+                "Invalid userId " . $params['userId']
             );
         }
 
