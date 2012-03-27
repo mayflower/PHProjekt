3431b279a13bf18e16361921ccf91f9cbc7a513c
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-03-27 16:34:42 +0200

    Added Todo_TodoController extends Phprojekt_Restcontroller

---
+++ phprojekt/application/Todo/Controllers/TodoController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Todo REST Controller.
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Todo
+ * @copyright  Copyright (c) 2012 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1
+ * @version    Release: @package_version@
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+
+/**
+ * Todo REST Controller.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Todo
+ * @copyright  Copyright (c) 2012 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1
+ * @version    Release: @package_version@
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+class Todo_TodoController extends Phprojekt_RestController
+{
+}
