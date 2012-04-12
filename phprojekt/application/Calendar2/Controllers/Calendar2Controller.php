7b7c565f2e2e146bac2731426d816cdc50f56708
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-04-12 15:11:32 +0200

    use the new grid in the calendar

--- phprojekt/application/Calendar2/Controllers/Calendar2Controller.php
+++ phprojekt/application/Calendar2/Controllers/Calendar2Controller.php
@@ -1,4 +1,7 @@
+<?php
 /**
+ * Calendar2 Rest Controller.
+ *
  * This software is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License version 3 as published by the Free Software Foundation
@@ -16,10 +19,22 @@
  * @link       http://www.phprojekt.com
  * @since      File available since Release 6.1
  * @version    Release: @package_version@
- * @author     Gustavo Solt <solt@mayflower.de>
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
  */
 
-dojo.provide("phpr.Calendar2.Grid");
-
-dojo.declare("phpr.Calendar2.Grid", phpr.Default.Grid, {
-});
+/**
+ * Calendar2 Rest Controller.
+ *
+ * @category   PHProjekt
+ * @package    Application
+ * @subpackage Calendar2
+ * @copyright  Copyright (c) 2012 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1
+ * @version    Release: @package_version@
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+class Calendar2_Calendar2Controller extends Phprojekt_RestController
+{
+}
