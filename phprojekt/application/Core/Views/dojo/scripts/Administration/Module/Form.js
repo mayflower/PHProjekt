8d00cb32bf56199d6b32463253005a39698f73e8
Reno Reckling <reno.reckling@mayflower.de>
2012-05-18 22:43:56 +0200

    merge moduledesigner controller and modulecontroller save action
    they where only used in the module designer and they where requested in line.
    if the first request succeeded but the second one failed, the moduledesigner
    would leave database tables and files behind.

--- phprojekt/application/Core/Views/dojo/scripts/Administration/Module/Form.js
+++ phprojekt/application/Core/Views/dojo/scripts/Administration/Module/Form.js
@@ -298,18 +298,6 @@ dojo.declare("phpr.Module.Form", phpr.Core.DialogForm, {
             if (data) {
                 new phpr.handleResponse('serverFeedback', data);
                 if (data.type == 'success') {
-                    return phpr.send({
-                        url: phpr.webpath + 'index.php/Core/module/jsonSave/nodeId/1/id/' + this.id,
-                        content:   this.sendData
-                    });
-                } else {
-                    this.setSubmitInProgress(false);
-                }
-            }
-        })).then(dojo.hitch(this, function(data) {
-            if (data) {
-                new phpr.handleResponse('serverFeedback', data);
-                if (data.type == 'success') {
                     if (!this.id) {
                         phpr.loadJsFile(phpr.webpath + 'index.php/js/module/name/' + this.sendData.name +
                             '/csrfToken/' + phpr.csrfToken);
