1296b08df772ef012a8fc0f69b73046231b7431b
David Soria Parra <david.soria_parra@mayflower.de>
2012-06-15 21:23:44 +0200

    PHProjekt 6.1.2

--- phprojekt/library/Phprojekt.php
+++ phprojekt/library/Phprojekt.php
@@ -53,7 +53,7 @@ class Phprojekt
     /**
      * The extra part of the version number.
      */
-    const VERSION_EXTRA = 'dev';
+    const VERSION_EXTRA = null;
 
     /**
      * Name of the Registry for current project.
