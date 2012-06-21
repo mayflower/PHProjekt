4f127b9d90678c03c03a50222e9c1efccb437609
Reno Reckling <reno.reckling@mayflower.de>
2012-06-21 12:39:45 +0200

    delete some changes
    i deleted to much styling info in the reverted commit.
    this reapplys part of it

--- phprojekt/htdocs/min/min/groupsConfig.php
+++ phprojekt/htdocs/min/min/groupsConfig.php
@@ -25,7 +25,6 @@ return array(
         "//dojo/release/dojo/dojox/layout/resources/FloatingPane.css",
         "//dojo/release/dojo/dojox/layout/resources/ResizeHandle.css",
         "//css/themes/phprojekt/Calendar.css",
-        "//css/themes/phprojekt/Calendar_rtl.css",
         "//css/themes/phprojekt/ColorPalette.css",
         "//css/themes/phprojekt/Common.css",
         "//css/themes/phprojekt/Dialog.css",
