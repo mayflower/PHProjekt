8ffdb5f13db3e3d53aa25620795737a45aae263b
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-08-25 01:41:56 +0200

    Added Phprojekt_Exception_IOException

---
+++ phprojekt/library/Phprojekt/Exception/IOException.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category   PHProjekt
+ * @package    Phprojekt
+ * @subpackage Module
+ * @copyright  Copyright (c) 2012 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1.5
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+
+/**
+ * An IO Exception
+ *
+ * @category   PHProjekt
+ * @package    Phprojekt
+ * @subpackage Exception
+ * @copyright  Copyright (c) 2012 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.1.5
+ * @author     Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
+ */
+class Phprojekt_Exception_IOException extends Exception
+{
+}
