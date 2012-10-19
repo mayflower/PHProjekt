523a3294bebd5dd1e8b74343d201064cb31c4ffd
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-10-19 18:14:31 +0200

    Added basic structure for new Timecard
    This includes a template and a viewmixin. The template defines a borderpane
    where we'll place the grid and the form.

---
+++ phprojekt/application/Timecard/Views/dojo/scripts/Main.js
@@ -0,0 +1,42 @@
+/**
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category  PHProjekt
+ * @package   Template
+ * @copyright Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license   LGPL v3 (See LICENSE file)
+ * @link      http://www.phprojekt.com
+ * @since     File available since Release 6.0
+ * @version   Release: 6.1.0
+ * @author    Gustavo Solt <solt@mayflower.de>
+ */
+
+dojo.provide("phpr.Timecard.Main");
+
+dojo.require("dijit.ColorPalette");
+
+dojo.declare("phpr.Timecard.Main", phpr.Default.Main, {
+    constructor: function() {
+        this.module = "Timecard";
+        this.loadFunctions(this.module);
+
+        this.formWidget = phpr.Timecard.Form;
+    },
+
+    renderTemplate: function() {
+        phpr.viewManager.setView(
+            phpr.Default.System.DefaultView,
+            phpr.Timecard.ViewContentMixin
+        );
+    },
+
+    setWidgets: function() {
+    }
+});
