cc31ea3ac3cc6ee5c9ce669cda4eb1e2475472b8
Reno Reckling <reno.reckling@mayflower.de>
2012-10-20 21:15:56 +0200

    replace the old module parameter of the openform function with preset values
    we want to pass the preset values into the openform call and the module
    parameter was already unused.

--- phprojekt/application/Calendar2/Views/dojo/scripts/Main.js
+++ phprojekt/application/Calendar2/Views/dojo/scripts/Main.js
@@ -360,7 +360,7 @@ dojo.declare("phpr.Calendar2.Main", phpr.Default.Main, {
         }
     },
 
-    openForm: function(/*int*/ id, /*String*/ module, /*String*/ startDate,
+    openForm: function(/*int*/ id, presetValues, /*String*/ startDate,
                       /*String*/ startTime, /*String*/ recurrenceId) {
         // Summary:
         //    This function opens a new Detail View
