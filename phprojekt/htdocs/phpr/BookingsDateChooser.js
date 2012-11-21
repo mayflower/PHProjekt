8fc8a6fedf7973cbee2d6a792cdf7e44fd7d5523
Reno Reckling <reno.reckling@mayflower.de>
2012-11-21 14:04:17 +0100

    make the month list auto resizable

--- phprojekt/htdocs/phpr/BookingsDateChooser.js
+++ phprojekt/htdocs/phpr/BookingsDateChooser.js
@@ -1,7 +1,9 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/array',
+    'dojo/_base/lang',
     'dojo/window',
+    'dojo/on',
     'dojo/dom-style',
     'dojo/dom-geometry',
     'dijit/_Widget',
@@ -9,9 +11,10 @@ define([
     'phpr/BookingsDateChooser/MonthGroup',
     'phpr/Scrollbar',
     'dojo/text!phpr/template/bookingsDateChooser.html'
-], function(declare, array, win, style, geometry, widget, template, MonthGroup, Scrollbar, templateString) {
+], function(declare, array, lang, win, on, style, geometry, widget, template, MonthGroup, Scrollbar, templateString) {
     return declare([widget, template], {
         templateString: templateString,
+        scrollbar: null,
 
         buildRendering: function() {
             this.inherited(arguments);
@@ -24,31 +27,36 @@ define([
 
         startup: function() {
             this.inherited(arguments);
-            this.fixHeight();
+            this.resize();
             this.addScrollBar();
+            this.own(on(win.get(this.domNode.ownerDocument), 'resize', lang.hitch(this, 'resize')));
         },
 
         placeAt: function() {
             this.inherited(arguments);
-            this.fixHeight();
+            this.resize();
         },
 
         addScrollBar: function() {
-            var scrollbar = new Scrollbar({
+            this.scrollbar = new Scrollbar({
                 containingNode: this.domNode,
                 nativeNode: this.nativeNode,
                 contentNode: this.contentNode
             });
-            scrollbar.placeAt(this.domNode);
+            this.scrollbar.placeAt(this.domNode);
         },
 
-        fixHeight: function() {
+        resize: function() {
             var domNode = this.domNode;
             array.forEach([this.nativeNode, this.scrollNode], function(node) {
                 style.set(node, {
                     height: (win.getBox().h - geometry.position(domNode).y) + 'px'
                 });
             });
+
+            if (this.scrollbar && this.scrollbar.resize) {
+                this.scrollbar.resize();
+            }
         }
     });
 });
