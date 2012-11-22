890f6cc281d374a305f9141b79eebaee86eb243a
Reno Reckling <reno.reckling@mayflower.de>
2012-11-22 12:38:51 +0100

    fix bookingview styling/layout
    add resizing in javascript
    put the booking to the left and add space for the date chooser

--- phprojekt/htdocs/phpr/BookingView.js
+++ phprojekt/htdocs/phpr/BookingView.js
@@ -1,13 +1,24 @@
 define([
     'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojo/dom-style',
+    'dojo/dom-geometry',
+    'dojo/window',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!phpr/template/bookingView.html',
     'phpr/BookingsDateChooser',
     'phpr/BookingList'
-], function(declare, widget, template, widgetsInTemplate, templateString) {
+], function(declare, array, style, geometry, win, widget, template, widgetsInTemplate, templateString) {
     return declare([widget, template, widgetsInTemplate], {
-        templateString: templateString
+        templateString: templateString,
+        resize: function() {
+            var winHeight = win.getBox().h;
+            array.forEach([this.bookingListContainer.domNode, this.dateChooserContainer.domNode], function(node) {
+                var top = geometry.position(node).y;
+                style.set(node, 'min-height', (winHeight - top) + 'px');
+            });
+        }
     });
 });
