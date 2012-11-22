ff3c75a79bed006fa67c9211539d5fe588cfbf3e
Reno Reckling <reno.reckling@mayflower.de>
2012-11-22 12:39:06 +0100

    remove old date chooser
    we don't need it anymore

--- phprojekt/htdocs/phpr/BookingsDateChooser.js
+++ phprojekt/htdocs/phpr/BookingsDateChooser.js
@@ -13,50 +13,6 @@ define([
     'dojo/text!phpr/template/bookingsDateChooser.html'
 ], function(declare, array, lang, win, on, style, geometry, widget, template, MonthGroup, Scrollbar, templateString) {
     return declare([widget, template], {
-        templateString: templateString,
-        scrollbar: null,
-
-        buildRendering: function() {
-            this.inherited(arguments);
-            var month = new MonthGroup({ dateObject: new Date(2012, 9)});
-            month.placeAt(this.contentNode);
-            var month = new MonthGroup({ dateObject: new Date()});
-            month.placeAt(this.contentNode);
-            month.set('selected', true);
-        },
-
-        startup: function() {
-            this.inherited(arguments);
-            this.resize();
-            this.addScrollBar();
-            this.own(on(win.get(this.domNode.ownerDocument), 'resize', lang.hitch(this, 'resize')));
-        },
-
-        placeAt: function() {
-            this.inherited(arguments);
-            this.resize();
-        },
-
-        addScrollBar: function() {
-            this.scrollbar = new Scrollbar({
-                containingNode: this.domNode,
-                nativeNode: this.nativeNode,
-                contentNode: this.contentNode
-            });
-            this.scrollbar.placeAt(this.domNode);
-        },
-
-        resize: function() {
-            var domNode = this.domNode;
-            array.forEach([this.nativeNode, this.scrollNode], function(node) {
-                style.set(node, {
-                    height: (win.getBox().h - geometry.position(domNode).y) + 'px'
-                });
-            });
-
-            if (this.scrollbar && this.scrollbar.resize) {
-                this.scrollbar.resize();
-            }
-        }
+        templateString: templateString
     });
 });
