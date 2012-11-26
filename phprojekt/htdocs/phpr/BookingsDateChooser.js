b32f08579d999f5b2d179b0b11dc58ca8328147d
Reno Reckling <reno.reckling@mayflower.de>
2012-11-26 11:52:37 +0100

    include the calendar widget into the bookings date chooser

--- phprojekt/htdocs/phpr/BookingsDateChooser.js
+++ phprojekt/htdocs/phpr/BookingsDateChooser.js
@@ -2,17 +2,14 @@ define([
     'dojo/_base/declare',
     'dojo/_base/array',
     'dojo/_base/lang',
-    'dojo/window',
     'dojo/on',
-    'dojo/dom-style',
-    'dojo/dom-geometry',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
-    'phpr/BookingsDateChooser/MonthGroup',
-    'phpr/Scrollbar',
-    'dojo/text!phpr/template/bookingsDateChooser.html'
-], function(declare, array, lang, win, on, style, geometry, widget, template, MonthGroup, Scrollbar, templateString) {
-    return declare([widget, template], {
+    'dijit/_WidgetsInTemplateMixin',
+    'dojo/text!phpr/template/bookingsDateChooser.html',
+    'phpr/Calendar'
+], function(declare, array, lang, on, widget, template, widgetsInTemplate, templateString) {
+    return declare([widget, template, widgetsInTemplate], {
         templateString: templateString
     });
 });
