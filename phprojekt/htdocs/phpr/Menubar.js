c2684c3a8ac66d0b686ea49c19ac8ba0eba202fa
Reno Reckling <reno.reckling@mayflower.de>
2012-11-26 11:46:17 +0100

    adjust the menubar to match the template

--- phprojekt/htdocs/phpr/Menubar.js
+++ phprojekt/htdocs/phpr/Menubar.js
@@ -2,11 +2,12 @@ define([
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/on',
+    'dojo/dom-class',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dojo/topic',
     'dojo/text!phpr/template/menubar.html'
-], function(declare, lang, on, widget, template, topic, templateString) {
+], function(declare, lang, on, clazz, widget, template, topic, templateString) {
     return declare([widget, template], {
         templateString: templateString,
 
@@ -20,10 +21,12 @@ define([
 
         onStartClick: function() {
             topic.publish('phpr/showLiveBooking');
+            clazz.replace(this.domNode, 'menubarOuter start');
         },
 
         onBookingsClick: function() {
             topic.publish('phpr/showBookings');
+            clazz.replace(this.domNode, 'menubarOuter bookings');
         }
     });
 });
