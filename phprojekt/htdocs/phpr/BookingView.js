9622aeff5b8fe78890d62973c741703fd046e292
Reno Reckling <reno.reckling@mayflower.de>
2012-11-26 11:49:17 +0100

    autoresize bookingview on window resize

--- phprojekt/htdocs/phpr/BookingView.js
+++ phprojekt/htdocs/phpr/BookingView.js
@@ -1,18 +1,28 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/array',
+    'dojo/_base/lang',
     'dojo/dom-style',
     'dojo/dom-geometry',
     'dojo/window',
+    'dojo/on',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!phpr/template/bookingView.html',
     'phpr/BookingsDateChooser',
     'phpr/BookingList'
-], function(declare, array, style, geometry, win, widget, template, widgetsInTemplate, templateString) {
+], function(declare, array, lang, style, geometry, win, on, widget, template, widgetsInTemplate, templateString) {
     return declare([widget, template, widgetsInTemplate], {
         templateString: templateString,
+
+        startup: function() {
+            this.inherited(arguments);
+            var w = win.get(this.domNode.ownerDocument);
+            this.own(on(w, 'onresize', lang.hitch(this, 'resize')));
+            this.own(on(this.domNode, 'onresize', lang.hitch(this, 'resize')));
+        },
+
         resize: function() {
             var winHeight = win.getBox().h;
             array.forEach([this.bookingListContainer.domNode, this.dateChooserContainer.domNode], function(node) {
