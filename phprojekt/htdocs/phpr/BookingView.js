1279debe4cf2fdd5719340147bd04e5e731e12ac
Reno Reckling <reno.reckling@mayflower.de>
2012-11-26 11:48:56 +0100

    fix the auto resizing in firefox
    firefox needs minHeight instead of min-height because of reasons

--- phprojekt/htdocs/phpr/BookingView.js
+++ phprojekt/htdocs/phpr/BookingView.js
@@ -17,7 +17,7 @@ define([
             var winHeight = win.getBox().h;
             array.forEach([this.bookingListContainer.domNode, this.dateChooserContainer.domNode], function(node) {
                 var top = geometry.position(node).y;
-                style.set(node, 'min-height', (winHeight - top) + 'px');
+                style.set(node, 'minHeight', (winHeight - top) + 'px');
             });
         }
     });
