d05764733477c001f3ceae8dde6b80af3e1a63dc
Reno Reckling <reno.reckling@mayflower.de>
2012-11-29 21:54:39 +0100

    create timehelp
    this will containe the common time handling functions

---
+++ phprojekt/htdocs/phpr/Timehelper.js
@@ -0,0 +1,34 @@
+define([
+    'exports'
+], function(exports) {
+    var stripLeadingZero = function(s) {
+        if (s.substr(0, 1) === '0') {
+            return s.substr(1);
+        } else {
+            return s;
+        }
+    };
+
+    exports.datetimeToJsDate = function(dt) {
+        return new Date(
+            dt.substr(0, 4),
+            stripLeadingZero(dt.substr(5, 2)) - 1,
+            stripLeadingZero(dt.substr(8, 2)),
+            stripLeadingZero(dt.substr(11, 2)),
+            stripLeadingZero(dt.substr(14, 2)),
+            stripLeadingZero(dt.substr(17, 2))
+        );
+    };
+
+    exports.timeToJsDate = function(t) {
+        return new Date(
+            0,
+            0,
+            0,
+            stripLeadingZero(t.substr(0, 2)),
+            stripLeadingZero(t.substr(3, 2)),
+            stripLeadingZero(t.substr(6, 2))
+        );
+    };
+
+});
