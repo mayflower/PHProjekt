b1c7a5054f6ca4e1cff3d2983fc04e0c35a610c4
Reno Reckling <reno.reckling@mayflower.de>
2012-11-29 21:55:10 +0100

    add projectTitleForId function to the api
    this will be needed everywhere

--- phprojekt/htdocs/phpr/Api.js
+++ phprojekt/htdocs/phpr/Api.js
@@ -2,8 +2,9 @@ define([
     'exports',
     'dojo/_base/lang',
     'dojo/_base/array',
-    'dojo/request/xhr'
-], function(exports, lang, array, xhr) {
+    'dojo/request/xhr',
+    'dojo/Deferred'
+], function(exports, lang, array, xhr, Deferred) {
     var config = (function() {
         var config = {};
 
@@ -42,6 +43,40 @@ define([
 
         return false;
     };
+
+    exports.projectTitleForId = (function() {
+        var titlesById = null;
+        var def = new Deferred();
+
+        exports.getData(
+            '/index.php/Project/Project',
+            {query: {projectId: 1, recursive: true}}
+        ).then(function(projects) {
+            titlesById = {};
+            array.forEach(projects, function(p) {
+                titlesById[p.id] = p.title;
+            });
+
+            def.resolve(titlesById);
+            def = null;
+        });
+
+        return function(id) {
+            if (id == 1) {
+                var d = new Deferred();
+                d.resolve('Unassigned');
+                return d;
+            } else if (titlesById === null) {
+                return def.then(function(idMap) {
+                    return idMap[id];
+                });
+            } else {
+                var d = new Deferred();
+                d.resolve(titlesById[id]);
+                return d;
+            }
+        };
+    })();
 
     exports.getModulePermissions = function(projectId) {
         var modulePermissionsUrl = 'index.php/Default/index/jsonGetModulesPermission/nodeId/' + projectId;
