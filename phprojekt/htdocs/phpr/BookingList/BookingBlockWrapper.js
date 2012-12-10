584ecfc0b6aabbbd26c88bbb6cc58bca67a9766a
Reno Reckling <reno.reckling@mayflower.de>
2012-12-10 23:33:53 +0100

    add the bookingblockwrapper
    the wrapper switches between the booking block and the edit block

---
+++ phprojekt/htdocs/phpr/BookingList/BookingBlockWrapper.js
@@ -0,0 +1,48 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dijit/_WidgetBase',
+    'dojo/dom-style',
+    'phpr/BookingList/BookingBlock',
+    'phpr/BookingList/BookingEditor'
+], function(declare, lang, _WidgetBase, style, BookingBlock, BookingEditor) {
+    return declare([_WidgetBase], {
+        bookingBlock : null,
+        editBlock: null,
+        store: null,
+
+        postCreate: function() {
+            this.inherited(arguments);
+            this.bookingBlock = new BookingBlock(this.params);
+            this.bookingBlock.placeAt(this.domNode);
+            this.bookingBlock.on('editClick', lang.hitch(this, '_editClick'));
+            this.own(this.bookingBlock);
+            this.bookingBlock.startup();
+        },
+
+        _editClick: function() {
+            var b = this.bookingBlock.get('booking');
+
+            style.set(this.bookingBlock.domNode, 'display', 'none');
+
+            if (this.editBlock) {
+                this.editBlock.destroyRecursive();
+            }
+
+            this.editBlock = new BookingEditor({ booking: b, store: this.store });
+            this.editBlock.placeAt(this.domNode);
+            this.editBlock.on('editCancel', lang.hitch(this, '_editCancel'));
+            this.editBlock.startup();
+            this.own(this.editBlock);
+        },
+
+        _editCancel: function() {
+            if (this.editBlock) {
+                this.editBlock.destroyRecursive();
+                this.editBlock = null;
+            }
+
+            style.set(this.bookingBlock.domNode, 'display', '');
+        }
+    });
+});
