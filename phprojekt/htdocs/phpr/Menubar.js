edf44cb59a8aeb1653e9417fc5234fb3d235744f
Reno Reckling <reno.reckling@mayflower.de>
2012-12-14 00:01:34 +0100

    make the logout button hoverable

--- phprojekt/htdocs/phpr/Menubar.js
+++ phprojekt/htdocs/phpr/Menubar.js
@@ -6,11 +6,17 @@ define([
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
+    'dijit/_CssStateMixin',
     'dojo/topic',
-    'dojo/text!phpr/template/menubar.html'
-], function(declare, lang, on, clazz, widget, template, widgetsInTemplate, topic, templateString) {
-    return declare([widget, template, widgetsInTemplate], {
+    'dojo/text!phpr/template/menubar.html',
+    'phpr/Menubar/WarningIcon'
+], function(declare, lang, on, clazz, widget, template, widgetsInTemplate, cssStateMixin, topic, templateString) {
+    return declare([widget, template, widgetsInTemplate, cssStateMixin], {
         templateString: templateString,
+        baseClass: 'menuBar',
+        cssStateNodes: {
+            logoutButton: 'dijit'
+        },
 
         buildRendering: function() {
             this.inherited(arguments);
