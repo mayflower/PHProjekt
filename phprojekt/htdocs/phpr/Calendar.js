cc76d2f621c7bb1964cdb179160844f2a73f4b90
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2012-12-18 01:55:53 +0100

    Highlight special days

--- phprojekt/htdocs/phpr/Calendar.js
+++ phprojekt/htdocs/phpr/Calendar.js
@@ -1,16 +1,47 @@
 define([
     'dojo/_base/declare',
+    'dojo/_base/array',
     'dojo/dom-class',
     'dojo/date',
     'dojo/date/locale',
-    'dijit/Calendar'
-], function(declare, clazz, ddate, locale, Calendar) {
+    'dojo/when',
+    'dojo/date/stamp',
+    'dijit/Calendar',
+    'phpr/Api',
+    'phpr/Timehelper'
+], function(declare, array, clazz, ddate, locale, when, stamp, Calendar, phpr, timehelper) {
+    var specialDayCache = {};
+
+    function specialDays(month, fun) {
+        var key = stamp.toISOString(month, {selector: 'date'});
+
+        if (!specialDayCache.hasOwnProperty(key)) {
+            specialDayCache[key] = phpr.getData(
+                'index.php/Calendar2/index/jsonHolidays',
+                {
+                    query: {
+                        'start': stamp.toISOString(month, {selector: 'date'}),
+                        'end': stamp.toISOString(ddate.add(month, 'month', 1), {selector: 'date'})
+                    }
+                }
+            ).then(
+                function(data) {
+                    specialDayCache[key] = data;
+                    fun(data);
+                }, function(error) {
+                    debugger;
+                }
+            );
+        } else {
+            when(specialDayCache[month], fun);
+        }
+    }
+
     return declare([Calendar], {
         _populateGrid: function() {
             this.inherited(arguments);
             var node;
-            var month = new this.dateClassObj(this.currentFocus);
-            month.setDate(1);
+            var month = this._currentMonth();
 
             for (var timestamp in this._date2cell) {
                 if (this._date2cell.hasOwnProperty(timestamp)) {
@@ -25,6 +56,32 @@ define([
                     }
                 }
             }
+            this._highlightSpecialDays();
+        },
+
+        _highlightSpecialDays: function() {
+            specialDays(this._currentMonth(), dojo.hitch(this, function(data) {
+                var byDate = {};
+                array.forEach(data, function(specialDay) {
+                    var d = timehelper.datetimeToJsDate(specialDay.date);
+                    byDate[stamp.toISOString(d, {selector: 'date'})] = specialDay;
+                });
+
+                for (var timestamp in this._date2cell) {
+                    if (this._date2cell.hasOwnProperty(timestamp)) {
+                        var d = new Date(parseInt(timestamp, 10));
+                        if (byDate.hasOwnProperty(stamp.toISOString(d, {selector: 'date'}))) {
+                            clazz.add(this._date2cell[timestamp], 'specialDay');
+                        }
+                    }
+                }
+            }));
+        },
+
+        _currentMonth: function() {
+            var month = new this.dateClassObj(this.currentFocus);
+            month.setDate(1);
+            return month;
         }
     });
 });
