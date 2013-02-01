32e76759dbbe2e885da73fab58999d8d08849418
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2013-02-01 12:27:35 +0100

    Added a topic publication to the DateChooserCalendar
    This way, we can use it in the summary-block without connecting events somewhere
    else.

--- phprojekt/htdocs/phpr/DateChooserCalendar.js
+++ phprojekt/htdocs/phpr/DateChooserCalendar.js
@@ -1,21 +1,28 @@
 define([
     'dojo/_base/declare',
+    'dojo/topic',
     'phpr/Calendar',
     'dojo/Evented'
-], function(declare, Calendar, Evented) {
+], function(declare, topic, Calendar, Evented) {
     return declare([Calendar, Evented], {
         _startedUp: false,
 
         _onMonthSelect: function(newMonth) {
             this.inherited(arguments);
-            this.emit('change', new this.dateClassObj(this.get('currentFocus').getTime()));
+            this._publishDateChange();
         },
 
         _setCurrentFocusAttr: function(date, force) {
             this.inherited(arguments);
             if (this._startedUp === true) {
-                this.emit('change', new this.dateClassObj(date.getTime()));
+                this._publishDateChange();
             }
+        },
+
+        _publishDateChange: function() {
+            var date = new this.dateClassObj(this.get('currentFocus').getTime());
+            topic.publish('timecard/selectedDateChanged', date);
+            this.emit('change', date);
         },
 
         postCreate: function() {
