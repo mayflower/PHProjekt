cd4a06d3505fde57666f7567d7e9cdded3246f53
Reno Reckling <reno.reckling@mayflower.de>
2013-03-06 18:00:26 +0100

    add first month table version
    just a basic version of the month table

--- phprojekt/htdocs/phpr/StatisticsView.js
+++ phprojekt/htdocs/phpr/StatisticsView.js
@@ -1,13 +1,50 @@
 define([
     'dojo/_base/declare',
+    'dojo/_base/lang',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!phpr/template/statisticsView.html',
+    'dojo/on',
     'phpr/Statistics/WorktimeMonthGraph',
-], function(declare, Widget, Templated, WidgetsInTemplate, templateString) {
+    'phpr/Statistics/WorktimeMonthTable'
+], function(
+    declare,
+    lang,
+    Widget,
+    Templated,
+    WidgetsInTemplate,
+    templateString,
+    on,
+    monthGraph,
+    monthTable
+) {
     return declare([Widget, Templated, WidgetsInTemplate], {
-        templateString: templateString
+        templateString: templateString,
+        monthViewState: 'graph',
+
+        buildRendering: function() {
+            this.inherited(arguments);
+            this.own(on(this.monthViewToggle, 'click', lang.hitch(this, '_onMonthViewToggle')));
+        },
+
+        _onMonthViewToggle: function() {
+            if (this.monthViewState === 'graph') {
+                this._setMonthTable();
+            } else {
+                this._setMonthGraph();
+            }
+        },
+
+        _setMonthTable: function() {
+            this.monthContainer.set('content', new monthTable());
+            this.monthViewState = 'table';
+        },
+
+        _setMonthGraph: function() {
+            this.monthContainer.set('content', new monthGraph());
+            this.monthViewState = 'graph';
+        }
     });
 
 });
