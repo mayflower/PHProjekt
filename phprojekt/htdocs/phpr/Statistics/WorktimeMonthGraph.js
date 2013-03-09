d6989b56ec73c13fa9c32897d4dc957df08ea82d
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2013-03-09 18:34:05 +0100

    Added TimeToWorkRenderer that renders the green line
    This allows us to delete the old code. It still uses hard-coded 450minutes/day.

--- phprojekt/htdocs/phpr/Statistics/WorktimeMonthGraph.js
+++ phprojekt/htdocs/phpr/Statistics/WorktimeMonthGraph.js
@@ -118,6 +118,85 @@ define([
         }
     });
 
+    var TimeToWorkRenderer = declare(null, {
+        _svgNode: null,
+        _dayEntries: null,
+        _helper: null,
+
+        constructor: function(svgNode, dayEntries) {
+            this._svgNode = svgNode;
+            this._dayEntries = dayEntries;
+            this._helper = new GeometryHelper(svgNode, dayEntries);
+        },
+
+        render: function() {
+            var svg = d3.select(this._svgNode);
+            var svgData = svg.selectAll().data(this._dayEntries);
+
+            this._renderHorizontalLines(svgData);
+            this._renderConnectingVerticalLines(svgData);
+        },
+
+        _renderHorizontalLines: function(svgData) {
+            svgData.enter()
+                .append('svg:line')
+                    .attr('x1', lang.hitch(this, this._horizontalX1))
+                    .attr('x2', lang.hitch(this, this._horizontalX2))
+                    .attr('y1', lang.hitch(this, this._horizontalY))
+                    .attr('y2', lang.hitch(this, this._horizontalY))
+                    .attr('stroke', '#6aa700');
+        },
+
+        _horizontalX1: function(d, i) {
+            return i * (barPadding + this._helper.barWidth());
+        },
+
+        _horizontalX2: function(d, i) {
+            return (i + 1) * (barPadding + this._helper.barWidth());
+        },
+
+        _horizontalY: function(d, i) {
+            var date = timehelper.dateToJsDate(d.date);
+            if (locale.isWeekend(date)) {
+                return this._helper.heightForTimebars();
+            }
+            return this._heightForMinutesToWork();
+        },
+
+        _renderConnectingVerticalLines: function(svgData) {
+            svgData.enter()
+                .append('svg:line')
+                    .attr('x1', lang.hitch(this, this._verticalX1))
+                    .attr('x2', lang.hitch(this, this._verticalX2))
+                    .attr('y1', lang.hitch(this, this._verticalY1))
+                    .attr('y2', lang.hitch(this, this._horizontalY))
+                    .attr('stroke', '#6aa700');
+        },
+
+        _verticalX1: function(d, i) {
+            return i * (barPadding + this._helper.barWidth());
+        },
+
+        _verticalX2: function(d, i) {
+            return (i) * (barPadding + this._helper.barWidth());
+        },
+
+        _verticalY1: function(d, i) {
+            if (i === 0) {
+                return this._horizontalY(d, i);
+            }
+            return this._horizontalY(this._dayEntries[i - 1], i - 1);
+        },
+
+        _heightForMinutesToWork: function() {
+            return this._helper.heightForTimebars() - this._helper.heightPerMinute() * this._minutesToWork();
+        },
+
+        _minutesToWork: function() {
+            return 450;
+        }
+    });
+
     return declare([Widget, Templated], {
         templateString: templateString,
         baseClass: 'thisMonthDiagram',
@@ -135,13 +214,6 @@ define([
                     timecardModel.getMonthList().then(lang.hitch(this, this._renderUsingMonthList));
                 })
             );
-
-            timecardModel.getMonthList().then(lang.hitch(this, function(data) {
-                // Still needed for the toWork-line
-                this._renderDays(data.days);
-
-                this._updateUpperLeftRect();
-            }));
         },
 
         _renderUsingWorkBalance: function(data) {
@@ -154,6 +226,7 @@ define([
                 });
             }
             new MinutesBookedBlockRenderer(this.bookedTimePerDayGraph, entries).render();
+            new TimeToWorkRenderer(this.bookedTimePerDayGraph, entries).render();
 
             this._fillOvertimeLabel();
             this._renderTodayMarker(new GeometryHelper(this.bookedTimePerDayGraph, entries));
@@ -199,89 +272,6 @@ define([
                 last = new Date(thisYear, thisMonth + 1, 0, 0, 0, 0);
             this.firstDayLabel.innerHTML = locale.format(first, {selector: 'date', datePattern: 'EEE d'});
             this.lastDayLabel.innerHTML = locale.format(last, {selector: 'date', datePattern: 'EEE d'});
-        },
-
-        // Stores the data by day we got from the server for helper functions.
-        _days: null,
-
-        _renderDays: function(days) {
-            this.days = days;
-
-            var svg = d3.select(this.bookedTimePerDayGraph);
-            var svgData = svg.selectAll().data(days);
-
-            var greenBarY = lang.hitch(this, function(d, i) {
-                var date = timehelper.dateToJsDate(d.date);
-                if (locale.isWeekend(date)) {
-                    return this._heightForTimebars();
-                }
-                return this._heightForMinutesToWork();
-            });
-
-            // horizontal lines
-            svgData.enter()
-                .append('svg:line')
-                    .attr('x1', lang.hitch(this, function(d, i) {
-                        return i * (barPadding + this._barWidth());
-                    }))
-                    .attr('x2', lang.hitch(this, function(d, i) {
-                        return (i + 1) * (barPadding + this._barWidth());
-                    }))
-                    .attr('y1', greenBarY)
-                    .attr('y2', greenBarY)
-                    .attr('stroke', '#6aa700');
-
-            // vertical lines
-            svgData.enter()
-                .append('svg:line')
-                    .attr('x1', lang.hitch(this, function(d, i) {
-                        return i * (barPadding + this._barWidth());
-                    }))
-                    .attr('x2', lang.hitch(this, function(d, i) {
-                        return (i) * (barPadding + this._barWidth());
-                    }))
-                    .attr('y1', function(d, i) {
-                        if (i === 0) {
-                            return greenBarY(d, i);
-                        }
-                        return greenBarY(days[i - 1], i - 1);
-                    })
-                    .attr('y2', greenBarY)
-                    .attr('stroke', '#6aa700');
-        },
-
-        // Functions below here assume _days is set
-        _heightPerMinute: function() {
-            return this._heightForTimebars() / maxMinutes;
-        },
-
-        _heightForMinutesToWork: function() {
-            return this._heightForTimebars() - this._heightPerMinute() * this._minutesToWork();
-        },
-
-        _heightForTimebars: function() {
-            return domAttr.get(this.bookedTimePerDayGraph, 'height');
-        },
-
-        _minutesToWork: function() {
-            return 450;
-        },
-
-        _displayWidth: function() {
-            return domAttr.get(this.bookedTimePerDayGraph, 'width') - 40;
-        },
-
-        _barWidth: function() {
-            return (this._displayWidth() / this.days.length) - barPadding;
-        },
-
-        _todayX: function() {
-            return (new Date()).getDate() * (this._barWidth() + barPadding) - (barPadding / 2);
-        },
-
-        _updateUpperLeftRect: function() {
-            domAttr.set(this.upperLeftRect, 'height', this._heightForMinutesToWork());
-            domAttr.set(this.upperLeftRect, 'width', this._todayX());
         }
     });
 });
