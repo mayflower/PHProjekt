624cb0b1e4f7d912518a58f9d0c80473b53ddc61
Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
2013-04-07 16:31:11 +0200

    remove debugger;

--- phprojekt/htdocs/phpr/StatisticsView.js
+++ phprojekt/htdocs/phpr/StatisticsView.js
@@ -65,7 +65,6 @@ define([
             if (evt) {
                 evt.stopPropagation();
             }
-            debugger;
 
             if (this.exportForm.validate()) {
                 var url = 'index.php/Timecard/Timecard/index';
