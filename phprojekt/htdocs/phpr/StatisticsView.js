821bfd13f2df1813cbcf7e6305527ae19ad55c49
Reno Reckling <reno.reckling@mayflower.de>
2013-04-12 18:57:23 +0200

    make statisticsView project selector aware

--- phprojekt/htdocs/phpr/StatisticsView.js
+++ phprojekt/htdocs/phpr/StatisticsView.js
@@ -1,35 +1,94 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/lang',
+    'dojo/_base/array',
     'dojo/dom-class',
+    'dojo/dom-construct',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!phpr/template/statisticsView.html',
     'dojo/on',
+    'dojo/Deferred',
     'phpr/Statistics/WorktimeMonthGraph',
     'phpr/Statistics/WorktimeMonthTable',
-    'phpr/ProjectChooser'
+    'phpr/ProjectChooser',
+    'dijit/layout/ContentPane'
 ], function(
     declare,
     lang,
+    array,
     clazz,
+    domConstruct,
     Widget,
     Templated,
     WidgetsInTemplate,
     templateString,
     on,
+    Deferred,
     monthGraph,
-    monthTable
+    monthTable,
+    projectChooser
 ) {
     return declare([Widget, Templated, WidgetsInTemplate], {
         templateString: templateString,
-        monthViewState: 'graph',
+        activeMonthWidget: null,
+        monthState: null,
+        projectChooser: null,
 
         buildRendering: function() {
             this.inherited(arguments);
+
+            this.projectChooser = new projectChooser({
+                createOptions: function(queryResults) {
+                    var def = new Deferred();
+                    var options = [];
+
+                    var first = null;
+                    var add = function(p) {
+                        options.push({
+                            id: '' + p.id,
+                            name: '' + p.id + ' ' + p.title,
+                            label: '<span class="projectId">' + p.id + '</span> ' + p.title
+                        });
+                    };
+
+                    array.forEach(queryResults.recent, add);
+
+                    if (queryResults.recent.length > 0) {
+                        options.push({ label: '<hr/>' });
+                    }
+
+                    options.push({
+                        id: '-1',
+                        name: 'All',
+                        label: 'All'
+                    });
+
+                    add({
+                        id: '1',
+                        title: 'Unassigned'
+                    });
+
+                    for (var p in queryResults.projects) {
+                        add(queryResults.projects[p]);
+                    }
+
+                    def.resolve(options);
+
+                    return def;
+                },
+                postStoreSet: function() {
+                    this.set('value', '-1');
+                }
+            }, domConstruct.create('select'));
+            this.projectChooserContainer.set('content', this.projectChooser);
+
+            this._setMonthGraph();
+
             this.own(on(this.monthViewGraphBtn, 'click', lang.hitch(this, '_onMonthViewGraph')));
             this.own(on(this.monthViewTableBtn, 'click', lang.hitch(this, '_onMonthViewTable')));
+            this.own(on(this.projectChooser, 'change', lang.hitch(this, '_onProjectChange')));
         },
 
         _onMonthViewGraph: function() {
@@ -45,13 +104,41 @@ define([
         },
 
         _setMonthTable: function() {
-            this.monthContainer.set('content', new monthTable());
-            this.monthViewState = 'table';
+            this._setMonthWidget(new monthTable({ projects: this._getSelectedProjects() }), 'table');
         },
 
         _setMonthGraph: function() {
-            this.monthContainer.set('content', new monthGraph());
-            this.monthViewState = 'graph';
+            this._setMonthWidget(new monthGraph({ projects: this._getSelectedProjects() }), 'graph');
+        },
+
+        _setMonthWidget: function(widget, state) {
+            if (this.activeMonthWidget !== null) {
+                this.activeMonthWidget.destroyRecursive();
+                this.activeMonthWidget = null;
+            }
+            this.activeMonthWidget = widget;
+            this.monthContainer.set('content', this.activeMonthWidget);
+            this.monthState = state;
+        },
+
+        _getSelectedProjects: function() {
+            var project = this.projectChooser.get('value');
+            if (project === '-1' || project === '') {
+                return [];
+            } else {
+                return [project];
+            }
+        },
+
+        _onProjectChange: function() {
+            switch (this.monthState) {
+                case 'table':
+                    this._setMonthTable();
+                    break;
+                case 'graph':
+                    this._setMonthGraph();
+                    break;
+            }
         }
     });
 
