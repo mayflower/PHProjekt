f4dc42fa88aa076d8c695f5658ec9cbd9be6b2d3
Reno Reckling <reno.reckling@mayflower.de>
2013-05-02 14:49:26 +0200

    change frontend timecard model to support start/end dates

--- phprojekt/htdocs/phpr/models/Timecard.js
+++ phprojekt/htdocs/phpr/models/Timecard.js
@@ -2,21 +2,41 @@ define([
     'exports',
     'dojo/_base/lang',
     'dojo/promise/all',
-    'phpr/Api'
+    'phpr/Api',
+    'phpr/Timehelper'
 ], function(
     exports,
     lang,
     all,
-    api
+    api,
+    timehelper
 ) {
     function monthYearDefaultQuery(params) {
-        var now = new Date();
-        var year = now.getFullYear();
-        var month = now.getMonth() + 1;
+        var start, end;
+        params = params || {};
 
-        var opts = lang.mixin({ year: year, month: month, projects: [] }, params);
-        opts.projects = opts.projects.join(',');
-        return opts;
+        if (params.startDate && params.endDate) {
+            start = params.startDate;
+            end = params.endDate;
+            end.setDate(end.getDate() + 1);
+        } else {
+            start = new Date();
+            end = new Date();
+            start.setDate(1);
+            end.setMonth(end.getMonth() + 1);
+            end.setDate(1);
+        }
+
+        var ret = {
+            startDate: timehelper.jsDateToIsoDate(start),
+            endDate: timehelper.jsDateToIsoDate(end)
+        };
+
+        if (params.projects && params.projects.length > 0) {
+            ret.projects = params.projects.join(',');
+        }
+
+        return ret;
     }
 
     exports.getMonthStatistics = function(params) {
