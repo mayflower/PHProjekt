ba71f054acc1f82bddfa54179e0f376284465178
Reno Reckling <reno.reckling@mayflower.de>
2013-05-16 14:45:39 +0200

    add team statistics to the frontend

---
+++ phprojekt/htdocs/phpr/Statistics/ProjectBookingsTable.js
@@ -0,0 +1,82 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/_base/array',
+    'dojo/date/locale',
+    'dojo/dom-construct',
+    'dijit/_Widget',
+    'phpr/models/Timecard',
+    'phpr/Timehelper',
+    'dojox/grid/DataGrid',
+    'dojo/data/ItemFileWriteStore',
+    'phpr/models/Project'
+], function(
+    declare,
+    lang,
+    array,
+    locale,
+    domConstruct,
+    Widget,
+    Model,
+    timehelper,
+    DataGrid,
+    WriteStore,
+    projects
+) {
+    return declare([Widget], {
+        baseClass: 'teamStatisticsGrid',
+
+        buildRendering: function() {
+            this.inherited(arguments);
+            Model.getMemberBookings(this._getModelParams()).then(lang.hitch(this, '_renderData'));
+        },
+
+        _renderData: function(data) {
+            if (this._destroyed) {
+                return;
+            }
+
+            if (data.length === 0) {
+                return domConstruct.place(
+                    '<span class="info">No bookings for this Project selection.</span>',
+                    this.domNode,
+                    'replace'
+                );
+            }
+
+            var items = data.map(function(item, idx) {
+                item.id = '' + (idx + 1);
+                return item;
+            });
+
+            var d = {
+                identifier: 'id',
+                items: items
+            };
+
+            var layout = [
+                { name: 'Project', field: 'project', width: '100px' },
+                { name: 'Date', field: 'date', width: '100px'  },
+                { name: 'Start', field: 'start', width: '100px'  },
+                { name: 'End', field: 'end', width: '100px'  },
+                { name: 'Minutes', field: 'minutes', width: '100px'  },
+                { name: 'User', field: 'user', width: '150px' },
+                { name: 'Notes', field: 'notes', width: '220px' }
+            ];
+
+            var store = new WriteStore({ data: d });
+            var grid = new DataGrid({
+                store: store,
+                structure: [layout],
+                autoHeight: 20
+            });
+            this.own(grid);
+            grid.placeAt(this.domNode);
+            grid.startup();
+        },
+
+        _getModelParams: function() {
+            return { start: this.startDate, end: this.endDate, projects: this.projects };
+        }
+    });
+});
--- phprojekt/htdocs/phpr/TeamStatisticsView.js
+++ phprojekt/htdocs/phpr/TeamStatisticsView.js
@@ -2,21 +2,118 @@ define([
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/_base/array',
+    'dojo/dom-construct',
+    'dojo/promise/all',
+    'dojo/Deferred',
+    'dojo/on',
     'dijit/_Widget',
     'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
-    'dojo/text!phpr/template/teamStatisticsView.html'
+    'dojo/text!phpr/template/teamStatisticsView.html',
+    'phpr/ProjectChooser',
+    'phpr/models/Project',
+    'phpr/Statistics/ProjectBookingsTable',
+    'dijit/layout/ContentPane'
 ], function(
     declare,
     lang,
     array,
+    domConstruct,
+    all,
+    Deferred,
+    on,
     Widget,
     Templated,
     WidgetsInTemplate,
-    templateString
+    templateString,
+    projectChooser,
+    projects,
+    projectBookingsTable
 ) {
+    var TeamStatisticsProjectChooser = declare([projectChooser], {
+        createOptions: function(queryResults) {
+            var def = new Deferred();
+            var options = [];
+
+            var add = function(p) {
+                options.push({
+                    id: '' + p.id,
+                    name: '' + p.id + ' ' + p.title,
+                    label: '<span class="projectId">' + p.id + '</span> ' + p.title
+                });
+            };
+
+            this.first = null;
+            for (var p in queryResults.projects) {
+                if (this.first === null) {
+                    this.first = queryResults.projects[p];
+                }
+
+                add(queryResults.projects[p]);
+            }
+
+            def.resolve(options);
+
+            return def;
+        },
+
+        getData: function() {
+            return all({
+                projects: projects.getManagedProjects()
+            });
+        },
+
+        postStoreSet: function() {
+            if (this.first !== null) {
+                this.set('value', '' + this.first.id);
+            }
+        }
+    });
 
     return declare([Widget, Templated, WidgetsInTemplate], {
-        templateString: templateString
+        templateString: templateString,
+        _tableUpdateTimer: null,
+
+        buildRendering: function() {
+            this.inherited(arguments);
+            this.projectChooser = new TeamStatisticsProjectChooser({}, domConstruct.create('select'));
+            this.projectChooserContainer.set('content', this.projectChooser);
+
+            this.own(on(this.projectChooser, 'change', lang.hitch(this, '_updateTableWidget')));
+            this.own(on(this.startDate, 'change', lang.hitch(this, '_updateTableWidget')));
+            this.own(on(this.endDate, 'change', lang.hitch(this, '_updateTableWidget')));
+        },
+
+        _updateTableWidget: function() {
+            this._scheduleTableWidgetUpdate();
+        },
+
+        _scheduleTableWidgetUpdate: function() {
+            clearTimeout(this._tableUpdateTimer);
+            this._tableUpdateTimer = setTimeout(lang.hitch(this, function() {
+                this._setTable();
+            }), 250);
+        },
+
+        _setTable: function() {
+            this.tableContainer.set('content', new projectBookingsTable(this._getTableParams()));
+        },
+
+        _getSelectedProjects: function() {
+            var project = this.projectChooser.get('value');
+            if (project === '-1' || project === '') {
+                return [];
+            } else {
+                return [project];
+            }
+        },
+
+        _getTableParams: function() {
+            return {
+                projects: this._getSelectedProjects(),
+                startDate: this.startDate.get('value'),
+                endDate: this.endDate.get('value')
+            };
+        }
     });
 });
